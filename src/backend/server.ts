f619dfaaa4256fa055f8303f8a25680bb2217b96
eirarset <eirik.arseth@nav.no>
2023-09-07 09:53:13 +0200

    Skal bruke webpack-dev-middleware ved lokal kjøring

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,19 +1,18 @@
 import express from "express";
 import webpack from "webpack";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import developmentConfig from "./webpack/webpack.development.js";
+import developmentConfig from "./webpack/webpack.development";
 
 import webpackDevMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
 
 import path from "path";
+import { miljø } from "./miljø";
 
 const app = express();
 
 const BASE_PATH = "";
-const buildPath = path.resolve(process.cwd(), "../../app/build");
+const buildPath = path.resolve(process.cwd(), miljø.builldPath);
 const PORT = 3000;
 
 app.get(`${BASE_PATH}/internal/isAlive|isReady`, (req, res) =>
@@ -21,9 +20,12 @@ app.get(`${BASE_PATH}/internal/isAlive|isReady`, (req, res) =>
 );
 
 if (process.env.NODE_ENV === "development") {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const compiler = webpack(developmentConfig);
 
   const devMiddleware = webpackDevMiddleware(compiler, {
+    writeToDisk: true,
     publicPath: developmentConfig.output.publicPath,
   });
 
@@ -31,7 +33,9 @@ if (process.env.NODE_ENV === "development") {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get("*", (_req, res) => {
+app.use(BASE_PATH, express.static(buildPath, { index: false }));
+
+app.get(/^(?!.*\/(internal|static|api)\/).*$/, (_req, res) => {
   res.sendFile("index.html", { root: buildPath });
 });
 
