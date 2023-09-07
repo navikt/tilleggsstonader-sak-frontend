c8af6c89bea8c85ee51817abcb9763c383c6b478
eirarset <eirik.arseth@nav.no>
2023-09-07 10:40:04 +0200

    Skal kunne bruke css og aksel

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { InternalHeader, Spacer } from "@navikt/ds-react";
 
 const App: React.FC = () => {
-  return <div>Tilleggsstønader</div>;
+  return (
+    <InternalHeader>
+      <InternalHeader.Title as="h1">Tilleggsstønader</InternalHeader.Title>
+      <Spacer />
+      <InternalHeader.User name="Ola Normann" />
+    </InternalHeader>
+  );
 };
 
 export default App;
--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "@navikt/ds-css";
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement!);
