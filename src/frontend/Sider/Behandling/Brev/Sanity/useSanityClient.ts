80d16e7ec275ac41e7a7daeaf78b4a5be9b23975
eirarset <eirik.arseth@nav.no>
2023-09-27 10:33:46 +0200

    Legg til sanityClient

---
+++ src/frontend/Sider/Behandling/Brev/Sanity/useSanityClient.ts
@@ -0,0 +1,14 @@
+import { createClient, type ClientConfig } from '@sanity/client';
+import { format } from 'date-fns';
+
+import { erProd } from '../../../../utils/miljø';
+export const useSanityClient = () => {
+    const dataset = 'production';
+    const config: ClientConfig = {
+        projectId: 's0grjk9v',
+        dataset: dataset,
+        useCdn: erProd(),
+        apiVersion: format(new Date(), 'yyyy-MM-dd'), // Nyeste api
+    };
+    return createClient(config);
+};
