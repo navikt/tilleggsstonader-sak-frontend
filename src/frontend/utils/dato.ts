bf42826f00cb19e5bac14d1745383526bf5da1c8
eirarset <eirik.arseth@nav.no>
2023-10-02 11:27:00 +0200

    Skal kunne formatere dagens dato

--- src/frontend/utils/dato.ts
+++ src/frontend/utils/dato.ts
@@ -7,3 +7,11 @@ export const formaterIsoDatoTid = (dato: string): string => {
 export const formaterNullableIsoDatoTid = (dato?: string): string | undefined => {
     return dato && formaterIsoDatoTid(dato);
 };
+
+export const dagensDatoFormatert = (): string => {
+    return new Date().toLocaleDateString('no-NO', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+};
