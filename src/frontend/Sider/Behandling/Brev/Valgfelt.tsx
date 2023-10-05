50fbcbe9260f1c7af2ebff0079480ef8a2d06322
eirarset <eirik.arseth@nav.no>
2023-10-05 13:35:01 +0200

    Oppdater state for hele delmalen når valgfelt velges

--- src/frontend/Sider/Behandling/Brev/Valgfelt.tsx
+++ src/frontend/Sider/Behandling/Brev/Valgfelt.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React, { SetStateAction, useState } from 'react';
 
 import { Select, TextField } from '@navikt/ds-react';
 
 import Fritekst from './Fritekst';
-import { Tekst, Valgfelt } from './typer';
+import { Tekst, Valg, Valgfelt } from './typer';
 
 interface Props {
     valgfelt: Valgfelt;
+    settValgfelt: React.Dispatch<SetStateAction<Record<string, Valg>>>;
 }
 
 const Tekst: React.FC<{ tekst: Tekst }> = ({ tekst }) => {
     return (
         <>
             {tekst.variabler.map((variabel) => (
-                <div>
+                <div key={variabel._id}>
                     <TextField label={variabel.visningsnavn} key={variabel._id} />
                 </div>
             ))}
@@ -21,21 +22,33 @@ const Tekst: React.FC<{ tekst: Tekst }> = ({ tekst }) => {
     );
 };
 
-const Valgfelt: React.FC<Props> = ({ valgfelt }) => {
+const Valgfelt: React.FC<Props> = ({ valgfelt, settValgfelt }) => {
     const [valgt, settValgt] = useState<string>();
 
-    const valgtBlock = valgfelt.valg.find(
-        (valg) =>
-            (valg._type === 'tekst' && valg._id === valgt) ||
-            (valg._type === 'fritekst' && valgt === 'fritekst')
-    );
+    const finnValgtBlock = (id: string | undefined) =>
+        valgfelt.valg.find(
+            (valg) =>
+                (valg._type === 'tekst' && valg._id === id) ||
+                (valg._type === 'fritekst' && id === 'fritekst')
+        );
+
+    const valgtBlock = finnValgtBlock(valgt);
 
     return (
         <>
             <Select
                 label={valgfelt.visningsnavn}
                 value={valgt || ''}
-                onChange={(e) => settValgt(e.target.value)}
+                onChange={(e) => {
+                    settValgt(e.target.value);
+                    settValgfelt((prevState) => {
+                        const valgtBlock = finnValgtBlock(e.target.value);
+
+                        return valgtBlock
+                            ? { ...prevState, [valgfelt._id]: valgtBlock }
+                            : prevState;
+                    });
+                }}
             >
                 <option value={''}>Velg</option>
                 {valgfelt.valg.map((valg, index) =>
