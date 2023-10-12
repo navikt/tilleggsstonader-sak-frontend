4c90fdb3814fb3694de9569265ef5693673dfe34
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-12 10:04:58 +0200

    Skal ikke sende videre til login automatisk, det håndteres av løsningen

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -42,13 +42,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use(bodyParser.json({ limit: '200mb' }));
 app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
 
-app.get(
-    /^(?!.*\/(internal|static|api|oauth2|favicon\.ico)\/).*$/,
-    validateToken(true),
-    (_req, res) => {
-        res.sendFile('index.html', { root: buildPath });
-    }
-);
+app.get(/^(?!.*\/(internal|static|api|oauth2)\/).*$/, validateToken(true), (_req, res) => {
+    res.sendFile('index.html', { root: buildPath });
+});
 
 app.use('/api/profile', addRequestInfo(), validateToken(), getProfile());
 
