df3a3d0547eca222980d4291523abe2968db25fd
sarahjelle <sara.hjelle@bekk.no>
2023-10-18 13:22:19 +0200

    Tilpass formstate for å håndtere recordstates

---
+++ src/frontend/hooks/felles/useRecordState.ts
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+export interface RecordState<T> {
+    value: Record<string, T>;
+    setValue: Dispatch<SetStateAction<Record<string, T>>>;
+    push(key: string, value: T): void;
+    remove(key: string): void;
+    update(key: string, value: T): void;
+}
+
+export default function useRecordState<T>(initialState: Record<string, T>): RecordState<T> {
+    const [value, setValue] = useState(initialState);
+
+    const remove = (key: string) => {
+        const prevValue = { ...value };
+        delete prevValue[key];
+        setValue(prevValue);
+    };
+
+    const update = (key: string, value: T) =>
+        setValue((prevState) => ({
+            ...prevState,
+            [key]: value,
+        }));
+
+    return {
+        value,
+        setValue,
+        push,
+        remove,
+        update,
+    };
+}
