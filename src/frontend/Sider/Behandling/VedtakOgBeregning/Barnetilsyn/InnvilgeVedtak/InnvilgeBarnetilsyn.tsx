552bdd52fed0f24b36cddd702013af8880b19569
sarahjelle <sara.hjelle@bekk.no>
2023-10-20 11:36:08 +0200

    Refaktor. Slett utkommentert kode

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/InnvilgeBarnetilsyn.tsx
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/InnvilgeBarnetilsyn.tsx
@@ -63,12 +63,6 @@ export const InnvilgeBarnetilsyn: React.FC<Props> = ({ lagretVedtak }) => {
     const { behandlingErRedigerbar, behandling } = useBehandling();
     // TODO: Prøve å slippe denne castingen
     const lagretInnvilgetVedtak = lagretVedtak as InnvilgeVedtakForBarnetilsyn;
-    // const [laster, settLaster] = useState<boolean>(false);
-    // const [feilmelding, settFeilmelding] = useState('');
-
-    // const { axiosRequest, nullstillIkkePersisterteKomponenter, settIkkePersistertKomponent } =
-    //     useApp();
-    // const { utførRedirect } = useRedirectEtterLagring(`/behandling/${behandling.id}/simulering`);
 
     const formState = useFormState<InnvilgeVedtakForm>(
         initFormState(lagretInnvilgetVedtak, barnIBehandling),
@@ -99,36 +93,7 @@ export const InnvilgeBarnetilsyn: React.FC<Props> = ({ lagretVedtak }) => {
             vedtaksRequest
             // eslint-disable-next-line no-console
         ).then((res) => console.log('response: ', res));
-
-        // settLaster(true);
-        // nullstillIkkePersisterteKomponenter();
-        // axiosRequest<string, InnvilgeVedtakForBarnetilsyn>({
-        //     method: 'POST',
-        //     url: `/familie-ef-sak/api/vedtak/${behandling.id}/lagre-vedtak`,
-        //     data: vedtaksRequest,
-        // })
-        //     .then(håndterVedtaksresultat())
-        //     .finally(() => {
-        //         settLaster(false);
-        //     });
     };
-
-    // const håndterVedtaksresultat = () => {
-    //     return (res: Ressurs<string>) => {
-    //         switch (res.status) {
-    //             case RessursStatus.SUKSESS:
-    //                 utførRedirect();
-    //                 hentBehandling.rerun();
-    //                 break;
-    //             case RessursStatus.HENTER:
-    //             case RessursStatus.IKKE_HENTET:
-    //                 break;
-    //             default:
-    //                 // settIkkePersistertKomponent(uuidv4());
-    //                 settFeilmelding(res.frontendFeilmelding);
-    //         }
-    //     };
-    // };
 
     const handleSubmit = (form: FormState<InnvilgeVedtakForm>) => {
         const vedtaksRequest: InnvilgeVedtakForBarnetilsyn = {
@@ -140,52 +105,6 @@ export const InnvilgeBarnetilsyn: React.FC<Props> = ({ lagretVedtak }) => {
         lagreVedtak(vedtaksRequest);
         return form;
     };
-
-    // const beregnBarnetilsyn = () => {
-    //     if (formState.customValidate(validerPerioder)) {
-    //         axiosRequest<IBeregningsperiodeBarnetilsyn[], IBeregningsrequestBarnetilsyn>({
-    //             method: 'POST',
-    //             url: `/familie-ef-sak/api/beregning/barnetilsyn`,
-    //             data: {
-    //                 utgiftsperioder: utgiftsperiodeState.value,
-    //                 kontantstøtteperioder:
-    //                     kontantstøtteState.value === ERadioValg.JA
-    //                         ? kontantstøttePeriodeState.value
-    //                         : [],
-    //                 tilleggsstønadsperioder:
-    //                     tilleggsstønadState.value === ERadioValg.JA &&
-    //                     stønadsreduksjonState.value === ERadioValg.JA
-    //                         ? tilleggsstønadsperiodeState.value
-    //                         : [],
-    //             },
-    //         }).then((res: Ressurs<IBeregningsperiodeBarnetilsyn[]>) => settBeregningsresultat(res));
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     if (!behandlingErRedigerbar) {
-    //         axiosRequest<IBeregningsperiodeBarnetilsyn[], null>({
-    //             method: 'GET',
-    //             url: `/familie-ef-sak/api/beregning/barnetilsyn/${behandling.id}`,
-    //         }).then((res: Ressurs<IBeregningsperiodeBarnetilsyn[]>) => settBeregningsresultat(res));
-    //     }
-    //     // eslint-disable-next-line
-    // }, [behandlingErRedigerbar]);
-
-    // useEffect(() => {
-    //     if (beregningsresultat.status === RessursStatus.SUKSESS) {
-    //         const kontantstøttebeløpOverstigerUtgiftsbeløpForAllePerioder =
-    //             blirNullUtbetalingPgaOverstigendeKontantstøtte(beregningsresultat.data);
-    //         settNullUtbetalingPgaKontantstøtte(
-    //             kontantstøttebeløpOverstigerUtgiftsbeløpForAllePerioder
-    //         );
-    //         if (kontantstøttebeløpOverstigerUtgiftsbeløpForAllePerioder) {
-    //             settResultatType(EBehandlingResultat.INNVILGE_UTEN_UTBETALING);
-    //         } else {
-    //             settResultatType(EBehandlingResultat.INNVILGE);
-    //         }
-    //     }
-    // }, [beregningsresultat, settResultatType]);
 
     return (
         <Form onSubmit={formState.onSubmit(handleSubmit)}>
@@ -204,44 +123,5 @@ export const InnvilgeBarnetilsyn: React.FC<Props> = ({ lagretVedtak }) => {
                 </Button>
             )}
         </Form>
-        // begrunnelseState={begrunnelseState}
-        // errorState={formState.errors}
-        // settValideringsFeil={formState.setErrors}
-        // barn={barn}
-        // låsFraDatoFørsteRad={låsFraDatoFørsteRad}
-        // />
-        /* <KontantstøtteValg
-                erLesevisning={!behandlingErRedigerbar}
-                kontantstøtte={kontantstøtteState}
-                kontantstøttePerioder={kontantstøttePeriodeState}
-                settValideringsFeil={formState.setErrors}
-                valideringsfeil={formState.errors}
-                finnesKontantstøtteUtbetaling={finnesKontantstøtteUtbetaling}
-    );
-            <TilleggsstønadValg
-                erLesevisning={!behandlingErRedigerbar}
-                settValideringsfeil={formState.setErrors}
-                stønadsreduksjon={stønadsreduksjonState}
-                tilleggsstønad={tilleggsstønadState}
-                tilleggsstønadBegrunnelse={tilleggsstønadBegrunnelseState}
-                tilleggsstønadPerioder={tilleggsstønadsperiodeState}
-                valideringsfeil={formState.errors}
-            /> */
-        /* {behandlingErRedigerbar && (
-                <div>
-                    <Knapp
-                        variant={'secondary'}
-                        onClick={beregnBarnetilsyn}
-                        type={'button'}
-                        icon={<CalculatorIcon title={'beregn'} />}
-                        iconPosition={'right'}
-                    >
-                        Beregn
-                    </Knapp>
-                    {feilmelding && <AlertError>{feilmelding}</AlertError>}
-                </div>
-            )} */
-        /* <Utregningstabell beregningsresultat={beregningsresultat} />
-            {behandlingErRedigerbar && <HovedKnapp disabled={laster} knappetekst="Lagre vedtak" />} */
     );
 };
