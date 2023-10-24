dbd125632fbab46a7ee6110614e6686910dbf91f
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-24 07:58:50 +0200

    Visningskomponenter for tekster

---
+++ src/frontend/komponenter/Visningskomponenter/BreakWordBodyLongSmall.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { BodyLongSmall } from './Tekster';
+
+export const BreakWordBodyLongSmall = styled(BodyLongSmall)`
+    white-space: pre-wrap;
+    word-wrap: break-word;
+`;
