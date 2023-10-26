d20eb0d9eb1240312b457f6b26e5dcb466e99170
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-26 10:27:47 +0200

    Høyremeny for å kunne vise totrinnskontroll (#80)

--- src/frontend/Sider/Behandling/BehandlingInnhold.tsx
+++ src/frontend/Sider/Behandling/BehandlingInnhold.tsx
@@ -3,15 +3,35 @@ import React from 'react';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { ABorderDefault } from '@navikt/ds-tokens/dist/tokens';
+
 import Fanemeny from './Fanemeny/Fanemeny';
 import { behandlingFaner } from './Fanemeny/faner';
+import Høyremeny from './Høyremeny/Høyremeny';
 import { BehandlingProvider } from '../../context/BehandlingContext';
 import { VilkårProvider } from '../../context/VilkårContext';
 import { RerrunnableEffect } from '../../hooks/useRerunnableEffect';
 import { Behandling } from '../../typer/behandling/behandling';
 
+const BehandlingContainer = styled.div`
+    display: flex;
+`;
+
 const InnholdWrapper = styled.div`
-    padding: 1rem;
+    flex-grow: 1;
+
+    max-width: calc(100% - 20rem);
+`;
+
+const HøyreMenyWrapper = styled.div`
+    border-left: 2px solid ${ABorderDefault};
+    background-color: white;
+
+    width: 20rem;
+    min-width: 20rem;
+
+    // Når skjermen blir for liten  så blir høyremenyn liggendes ovenfor venstredelen
+    z-index: 10;
 `;
 
 const BehandlingInnhold: React.FC<{
@@ -24,24 +44,29 @@ const BehandlingInnhold: React.FC<{
 
     return (
         <BehandlingProvider behandling={behandling} hentBehandling={hentBehandling}>
-            <Fanemeny behandlingId={behandling.id} aktivFane={path} />
-            <VilkårProvider behandling={behandling}>
-                <InnholdWrapper>
-                    <Routes>
-                        {behandlingFaner.map((tab) => (
+            <BehandlingContainer>
+                <VilkårProvider behandling={behandling}>
+                    <InnholdWrapper>
+                        <Fanemeny behandlingId={behandling.id} aktivFane={path} />
+                        <Routes>
+                            {behandlingFaner.map((tab) => (
+                                <Route
+                                    key={tab.path}
+                                    path={`/${tab.path}`}
+                                    element={tab.komponent(behandling.id)}
+                                />
+                            ))}
                             <Route
-                                key={tab.path}
-                                path={`/${tab.path}`}
-                                element={tab.komponent(behandling.id)}
+                                path="*"
+                                element={<Navigate to={behandlingFaner[0].path} replace={true} />}
                             />
-                        ))}
-                        <Route
-                            path="*"
-                            element={<Navigate to={behandlingFaner[0].path} replace={true} />}
-                        />
-                    </Routes>
-                </InnholdWrapper>
-            </VilkårProvider>
+                        </Routes>
+                    </InnholdWrapper>
+                </VilkårProvider>
+                <HøyreMenyWrapper>
+                    <Høyremeny />
+                </HøyreMenyWrapper>
+            </BehandlingContainer>
         </BehandlingProvider>
     );
 };
---
+++ src/frontend/Sider/Behandling/Høyremeny/Høyremeny.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Høyremeny = () => {
+    return <></>;
+};
+
+export default Høyremeny;
