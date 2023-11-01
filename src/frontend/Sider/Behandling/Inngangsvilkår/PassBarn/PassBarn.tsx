ac17eecddb8206d862db8660151a08caad4c5595
sarahjelle <sara.hjelle@bekk.no>
2023-11-01 10:26:24 +0100

    Legg til vilkårpanel for pass barn

---
+++ src/frontend/Sider/Behandling/Inngangsvilkår/PassBarn/PassBarn.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Vilkårsregler } from '../../../../typer/regel';
+import { Inngangsvilkårtype, Vilkårsresultat, Vilkårsvurdering } from '../../vilkår';
+import { Vilkårpanel } from '../../Vilkårspanel/Vilkårpanel';
+import { VilkårpanelInnhold } from '../../Vilkårspanel/VilkårpanelInnhold';
+import VisEllerEndreVurdering from '../../Vilkårvurdering/VisEllerEndreVurdering';
+
+interface Props {
+    vilkårsregler: Vilkårsregler<Inngangsvilkårtype.PASSBARN>;
+    vilkårsvurdering: Vilkårsvurdering;
+}
+
+const PassBarn: React.FC<Props> = ({ vilkårsregler, vilkårsvurdering }) => {
+    const vilkårsett = vilkårsvurdering.vilkårsett.filter(
+        (v) => v.vilkårType === Inngangsvilkårtype.PASSBARN
+    );
+
+    if (vilkårsett.length === 0) {
+        return <div>Mangler vurderinger for pass av barn</div>;
+    }
+
+    return vilkårsett.map((vilkår) => (
+        <Vilkårpanel
+            tittel={'Pass av barn'}
+            vilkårsresultat={Vilkårsresultat.IKKE_TATT_STILLING_TIL}
+        >
+            <VilkårpanelInnhold>
+                {{
+                    høyre: <VisEllerEndreVurdering vilkår={vilkår} regler={vilkårsregler.regler} />,
+                }}
+            </VilkårpanelInnhold>
+        </Vilkårpanel>
+    ));
+};
+
+export default PassBarn;
