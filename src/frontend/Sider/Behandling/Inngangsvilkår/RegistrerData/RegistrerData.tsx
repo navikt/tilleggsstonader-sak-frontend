37039f5df5e9d7fb337fc69a785b30940dba2fbb
sarahjelle <sara.hjelle@bekk.no>
2023-11-30 11:13:03 +0100

    Legg til målgruppeperiodevalg i registrer data

--- src/frontend/Sider/Behandling/Inngangsvilkår/RegistrerData/RegistrerData.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/RegistrerData/RegistrerData.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import MålgruppePeriodeValg from './MålgruppeValg';
 import { validerRegistrerDataForm } from './registrerDataValidering';
-import { RegistrerDataForm } from './typer';
+import { MålgruppePeriode, RegistrerDataForm } from './typer';
 import { tomMålgruppeRad } from './utils';
 import useFormState from '../../../../hooks/felles/useFormState';
+import { ListState } from '../../../../hooks/felles/useListState';
 import { Vilkårsresultat } from '../../vilkår';
 import { Vilkårpanel } from '../../Vilkårspanel/Vilkårpanel';
 
@@ -14,12 +16,19 @@ const initFormState = () => ({
 const RegistrerData = () => {
     const vilkårsresultat = Vilkårsresultat.OPPFYLT; // TODO
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const formState = useFormState<RegistrerDataForm>(initFormState(), validerRegistrerDataForm);
+
+    const stønadsperioderState = formState.getProps(
+        'målgruppePerioder'
+    ) as ListState<MålgruppePeriode>;
 
     return (
         <Vilkårpanel tittel="Registrer data" vilkårsresultat={vilkårsresultat}>
-            Hei
+            <MålgruppePeriodeValg
+                målgruppePerioderState={stønadsperioderState}
+                errorState={formState.errors.målgruppePerioder}
+                settValideringsFeil={formState.setErrors}
+            />
         </Vilkårpanel>
     );
 };
