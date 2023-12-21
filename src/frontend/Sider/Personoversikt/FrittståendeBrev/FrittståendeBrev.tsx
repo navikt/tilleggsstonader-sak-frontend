43cfe226da8b26ab50a60ffa750045faaaf0c07f
sarahjelle <sara.hjelle@bekk.no>
2023-12-21 12:35:52 +0100

    Legg til knapp for å sende brev

--- src/frontend/Sider/Personoversikt/FrittståendeBrev/FrittståendeBrev.tsx
+++ src/frontend/Sider/Personoversikt/FrittståendeBrev/FrittståendeBrev.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { Button } from '@navikt/ds-react';
+
+import { useApp } from '../../../context/AppContext';
 import DataViewer from '../../../komponenter/DataViewer';
 import { Stønadstype } from '../../../typer/behandling/behandlingTema';
+import { RessursStatus } from '../../../typer/ressurs';
 import Brevmeny from '../../Behandling/Brev/Brevmeny';
 import useBrev from '../../Behandling/Brev/useBrev';
 import VelgBrevmal from '../../Behandling/Brev/VelgBrevmal';
@@ -10,10 +14,28 @@ const FrittståendeBrev: React.FC<{ valgtStønadstype: Stønadstype; fagsakId: s
     valgtStønadstype,
     fagsakId,
 }) => {
+    const { request } = useApp();
+
     const { brevmaler, brevmal, settBrevmal, malStruktur, fil, settFil } = useBrev(
         valgtStønadstype,
         'FRITTSTAENDE'
     );
+
+    const sendBrev = () => {
+        if (fil.status === RessursStatus.SUKSESS && brevmal) {
+            request<null, { pdf: string; tittel: string }>(
+                `/api/sak/frittstaende-brev/send/${fagsakId}`,
+                'POST',
+                {
+                    pdf: fil.data,
+                    tittel: brevmal,
+                }
+            ).then((res) => {
+                // eslint-disable-next-line no-console
+                console.log(res);
+            });
+        }
+    };
 
     return (
         <DataViewer response={{ brevmaler }}>
@@ -35,6 +57,7 @@ const FrittståendeBrev: React.FC<{ valgtStønadstype: Stønadstype; fagsakId: s
                             />
                         )}
                     </DataViewer>
+                    <Button onClick={sendBrev}>Send brev</Button>
                 </>
             )}
         </DataViewer>
