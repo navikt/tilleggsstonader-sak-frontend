f22ba9dfb15012085b1b0e23f2bb3cd209e310d5
sarahjelle <sara.hjelle@bekk.no>
2024-01-02 11:12:39 +0100

    Legg til toast for vedtak underkjent

--- src/frontend/typer/toast.ts
+++ src/frontend/typer/toast.ts
@@ -1,7 +1,9 @@
 export enum Toast {
     BREV_SENDT = 'BREV_SENDT',
+    VEDTAK_UNDERKJENT = 'VEDTAK_UNDERKJENT',
 }
 
 export const toastTilTekst: Record<Toast, string> = {
     BREV_SENDT: 'Brevet er nå sendt',
+    VEDTAK_UNDERKJENT: 'Vedtak underkjent',
 };
