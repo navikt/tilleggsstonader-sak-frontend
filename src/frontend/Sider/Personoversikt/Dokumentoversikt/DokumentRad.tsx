3603cacb5db7cccd5d82983a0f9a94b84edcb58e
sarahjelle <sara.hjelle@bekk.no>
2024-01-10 16:16:46 +0100

    Legg til lenke som åpner dokument i ny fane

--- src/frontend/Sider/Personoversikt/Dokumentoversikt/DokumentRad.tsx
+++ src/frontend/Sider/Personoversikt/Dokumentoversikt/DokumentRad.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Table } from '@navikt/ds-react';
+import { Link, Table } from '@navikt/ds-react';
 
 import { DokumentInfo, journalstatuserTilTekst } from '../../../typer/dokument';
 import { formaterIsoDatoTid } from '../../../utils/dato';
@@ -10,7 +10,14 @@ const DokumentRad: React.FC<{ dokument: DokumentInfo }> = ({ dokument }) => {
         <Table.Row>
             <Table.DataCell>{formaterIsoDatoTid(dokument.dato)}</Table.DataCell>
             <Table.DataCell>{dokument.journalposttype}</Table.DataCell>
-            <Table.DataCell>{dokument.tittel}</Table.DataCell>
+            <Table.DataCell>
+                <Link
+                    target="_blank"
+                    href={`/dokument/journalpost/${dokument.journalpostId}/dokument-pdf/${dokument.dokumentinfoId}`}
+                >
+                    {dokument.tittel}
+                </Link>
+            </Table.DataCell>
             <Table.DataCell>{dokument.avsenderMottaker?.navn}</Table.DataCell>
             <Table.DataCell>{dokument.tema}</Table.DataCell>
             <Table.DataCell>{journalstatuserTilTekst[dokument.journalstatus]}</Table.DataCell>
