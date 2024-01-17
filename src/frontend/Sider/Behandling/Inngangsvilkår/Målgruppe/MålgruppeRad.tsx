c2eb99da889c0ad35dfb02ee68343a112731db31
sarahjelle <sara.hjelle@bekk.no>
2024-01-17 09:32:55 +0100

    Legg til knapp for å redigere og fjern innhold i expanded row

--- src/frontend/Sider/Behandling/Inngangsvilkår/Målgruppe/MålgruppeRad.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Målgruppe/MålgruppeRad.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Table } from '@navikt/ds-react';
+import { PencilIcon } from '@navikt/aksel-icons';
+import { Button, Table } from '@navikt/ds-react';
 
 import { VilkårsresultatIkon } from '../../../../komponenter/Ikoner/Vilkårsresultat/VilkårsresultatIkon';
 import { formaterIsoDato } from '../../../../utils/dato';
@@ -8,33 +9,25 @@ import { Målgruppe } from '../typer/målgruppe';
 
 const MålgruppeRad: React.FC<{ målgruppe: Målgruppe }> = ({ målgruppe }) => {
     return (
-        <Table.ExpandableRow
-            key={målgruppe.id}
-            togglePlacement={'right'}
-            // expansionDisabled={målgruppe.vilkår.delvilkårsett.length === 0}
-            content={
-                <>
-                    {/* <Heading size={'xsmall'}>Vilkårsvurdering</Heading>
-                                    <EndreVurderingComponent
-                                        vilkårType={målgruppe.vilkår.vilkårType}
-                                        regler={regler[målgruppe.vilkår.vilkårType].regler}
-                                        vilkår={målgruppe.vilkår}
-                                        oppdaterVilkår={oppdaterMålgruppeVilkårState}
-                                    />
-                                    <Feilmelding>
-                                        {vilkårFeilmeldinger[målgruppe.vilkår.id]}
-                                    </Feilmelding> */}
-                </>
-            }
-        >
+        <Table.Row key={målgruppe.id}>
             <Table.DataCell width="max-content">
                 <VilkårsresultatIkon vilkårsresultat={målgruppe.resultat} />
             </Table.DataCell>
             <Table.DataCell>{målgruppe.type}</Table.DataCell>
             <Table.DataCell>{formaterIsoDato(målgruppe.fom)}</Table.DataCell>
             <Table.DataCell>{formaterIsoDato(målgruppe.tom)}</Table.DataCell>
-            <Table.DataCell>Kilde</Table.DataCell>
-        </Table.ExpandableRow>
+            <Table.DataCell>{målgruppe.kilde}</Table.DataCell>
+            <Table.DataCell>
+                <Button
+                    // TODO: OnClick sett redigeringsmodus
+                    variant="secondary"
+                    size="small"
+                    icon={<PencilIcon />}
+                >
+                    Endre
+                </Button>
+            </Table.DataCell>
+        </Table.Row>
     );
 };
 
