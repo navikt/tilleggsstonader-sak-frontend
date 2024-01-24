caeebfc117456fb8507400c29f174413e2bb7668
Eirik Årseth <eirik.arseth@nav.no>
2024-01-24 22:01:10 +0100

    Visning av slettede vilkårperioder

--- src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/VilkårperiodeRad.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/VilkårperiodeRad.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import { PencilIcon } from '@navikt/aksel-icons';
+import { styled } from 'styled-components';
+
+import { PencilIcon, TrashIcon } from '@navikt/aksel-icons';
 import { Button, Table } from '@navikt/ds-react';
+import { ABgSubtle } from '@navikt/ds-tokens/dist/tokens';
 
 import { VilkårsresultatIkon } from '../../../../komponenter/Ikoner/Vilkårsresultat/VilkårsresultatIkon';
 import { formaterIsoDato } from '../../../../utils/dato';
 import { AktivitetType } from '../typer/aktivitet';
 import { MålgruppeType } from '../typer/målgruppe';
-import { VilkårPeriode } from '../typer/vilkårperiode';
+import { VilkårPeriode, VilkårPeriodeResultat } from '../typer/vilkårperiode';
+
+const TabellRad = styled(Table.Row)<{ disabled?: boolean }>`
+    background: ${(props) => (props.disabled ? ABgSubtle : '')};
+`;
 
 const VilkårperiodeRad: React.FC<{
     vilkårperiode: VilkårPeriode;
@@ -15,7 +22,10 @@ const VilkårperiodeRad: React.FC<{
     startRedigering: () => void;
 }> = ({ vilkårperiode, type, startRedigering }) => {
     return (
-        <Table.Row key={vilkårperiode.id}>
+        <TabellRad
+            key={vilkårperiode.id}
+            disabled={vilkårperiode.resultat === VilkårPeriodeResultat.SLETTET}
+        >
             <Table.DataCell width="max-content">
                 <VilkårsresultatIkon vilkårsresultat={vilkårperiode.resultat} />
             </Table.DataCell>
@@ -33,7 +43,7 @@ const VilkårperiodeRad: React.FC<{
                     Endre
                 </Button>
             </Table.DataCell>
-        </Table.Row>
+        </TabellRad>
     );
 };
 
