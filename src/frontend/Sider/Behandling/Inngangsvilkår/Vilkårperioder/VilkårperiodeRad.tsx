f7288e86f09fddfbe8a322a5755a77b743251765
Eirik Årseth <eirik.arseth@nav.no>
2024-01-26 12:53:09 +0100

    Skal vise begrunnelse for vilkårperiode i lesevisning

--- src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/VilkårperiodeRad.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/VilkårperiodeRad.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { styled } from 'styled-components';
 
-import { PencilIcon, TrashIcon } from '@navikt/aksel-icons';
-import { Button, Table } from '@navikt/ds-react';
+import { ChatIcon, PencilIcon, TrashIcon } from '@navikt/aksel-icons';
+import { Button, Detail, HStack, Popover, Spacer, Table } from '@navikt/ds-react';
 import { ABgSubtle } from '@navikt/ds-tokens/dist/tokens';
 
 import SlettVilkRperiodeModal from './SlettVilkårperiodeModal';
 import { useBehandling } from '../../../../context/BehandlingContext';
 import { VilkårsresultatIkon } from '../../../../komponenter/Ikoner/Vilkårsresultat/VilkårsresultatIkon';
-import { formaterIsoDato } from '../../../../utils/dato';
+import Lesefelt from '../../../../komponenter/Skjema/Lesefelt';
+import { formaterIsoDato, formaterIsoDatoTidMedSekunder } from '../../../../utils/dato';
 import { AktivitetType } from '../typer/aktivitet';
 import { MålgruppeType } from '../typer/målgruppe';
 import { VilkårPeriode, VilkårPeriodeResultat } from '../typer/vilkårperiode';
 
 const TabellRad = styled(Table.Row)<{ disabled?: boolean }>`
     background: ${(props) => (props.disabled ? ABgSubtle : '')};
+`;
+
+const FlexDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    max-width: 48rem;
 `;
 
 const VilkårperiodeRad: React.FC<{
@@ -29,6 +37,9 @@ const VilkårperiodeRad: React.FC<{
     const visRedigerKnapper =
         vilkårperiode.resultat != VilkårPeriodeResultat.SLETTET && behandlingErRedigerbar;
 
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const [visBeskrivelse, settVisBeskrivelse] = useState(false);
+
     return (
         <TabellRad
             key={vilkårperiode.id}
@@ -37,7 +48,50 @@ const VilkårperiodeRad: React.FC<{
             <Table.DataCell width="max-content">
                 <VilkårsresultatIkon vilkårsresultat={vilkårperiode.resultat} />
             </Table.DataCell>
-            <Table.DataCell>{type}</Table.DataCell>
+            <Table.DataCell>
+                <HStack>
+                    {type}
+                    {vilkårperiode.begrunnelse && (
+                        <>
+                            <Spacer />
+                            <Button
+                                ref={buttonRef}
+                                icon={<ChatIcon />}
+                                size={'small'}
+                                variant={'tertiary'}
+                                onClick={() => settVisBeskrivelse(!visBeskrivelse)}
+                            />
+                            <Popover
+                                anchorEl={buttonRef.current}
+                                open={visBeskrivelse}
+                                onClose={() => settVisBeskrivelse(false)}
+                            >
+                                <Popover.Content>
+                                    <FlexDiv>
+                                        <Detail>
+                                            Sist endret{' '}
+                                            {formaterIsoDatoTidMedSekunder(
+                                                vilkårperiode.sistEndret
+                                            )}
+                                        </Detail>
+                                        {vilkårperiode.resultat ===
+                                            VilkårPeriodeResultat.SLETTET && (
+                                            <Lesefelt
+                                                verdi={vilkårperiode.slettetKommentar}
+                                                label={'Begrunnelse for sletting'}
+                                            />
+                                        )}
+                                        <Lesefelt
+                                            verdi={vilkårperiode.begrunnelse}
+                                            label={'Begrunnelse periode'}
+                                        />
+                                    </FlexDiv>
+                                </Popover.Content>
+                            </Popover>
+                        </>
+                    )}
+                </HStack>
+            </Table.DataCell>
             <Table.DataCell>{formaterIsoDato(vilkårperiode.fom)}</Table.DataCell>
             <Table.DataCell>{formaterIsoDato(vilkårperiode.tom)}</Table.DataCell>
             <Table.DataCell>{vilkårperiode.kilde}</Table.DataCell>
