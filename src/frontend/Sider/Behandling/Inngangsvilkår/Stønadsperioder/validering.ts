ecbb71fe549d6428093a2459d6c7dbb494e217f9
Eirik Årseth <eirik.arseth@nav.no>
2024-02-06 16:02:56 +0100

    Backend skal være ansvarlig for validering på tvers av vilkårsperioder og stønadsperioder

--- src/frontend/Sider/Behandling/Inngangsvilkår/Stønadsperioder/validering.ts
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Stønadsperioder/validering.ts
@@ -1,14 +1,9 @@
 import { FormErrors } from '../../../../hooks/felles/useFormState';
-import { erPeriodeInnenforAnnenPeriode, validerPeriode } from '../../../../utils/periode';
-import { Aktivitet } from '../typer/aktivitet';
-import { Målgruppe } from '../typer/målgruppe';
+import { validerPeriode } from '../../../../utils/periode';
 import { Stønadsperiode } from '../typer/stønadsperiode';
-import { VilkårPeriodeResultat } from '../typer/vilkårperiode';
 
 export const validerStønadsperioder = (
-    stønadsperioder: Stønadsperiode[],
-    målgrupper: Målgruppe[],
-    aktiviteter: Aktivitet[]
+    stønadsperioder: Stønadsperiode[]
 ): FormErrors<Stønadsperiode[]> => {
     const feilIStønadsperioder = stønadsperioder.map((periode) => {
         const stønadsperiodeFeil: FormErrors<Stønadsperiode> = {
@@ -31,56 +26,6 @@ export const validerStønadsperioder = (
             return {
                 ...stønadsperiodeFeil,
                 ...periodeValidering,
-            };
-        }
-
-        // TODO: Flytte validering under til backend?
-        // TODO valider at målgruppe og aktivitet sammen er gyldige
-        const relevanteMålgrupper = målgrupper.filter(
-            (målgruppe) =>
-                periode.målgruppe === målgruppe.type &&
-                målgruppe.resultat === VilkårPeriodeResultat.OPPFYLT
-        );
-
-        if (relevanteMålgrupper.length === 0) {
-            return {
-                ...stønadsperiodeFeil,
-                målgruppe: 'Finnes ingen periode for målgruppe hvor vilkår er oppfylt',
-            };
-        }
-
-        const målgrupperInnenfor = relevanteMålgrupper.some((målgruppe) =>
-            erPeriodeInnenforAnnenPeriode(periode, målgruppe)
-        );
-
-        if (!målgrupperInnenfor) {
-            return {
-                ...stønadsperiodeFeil,
-                målgruppe: 'Ingen relevante målgruppeperioder rommer stønadsperioden',
-            };
-        }
-
-        const relevanteAktiviteter = aktiviteter.filter(
-            (aktivitet) =>
-                periode.aktivitet === aktivitet.type &&
-                aktivitet.resultat === VilkårPeriodeResultat.OPPFYLT
-        );
-
-        if (relevanteAktiviteter.length === 0) {
-            return {
-                ...stønadsperiodeFeil,
-                aktivitet: 'Finnes ingen periode for aktivitet hvor vilkår er oppfylt',
-            };
-        }
-
-        const aktivitetInnenfor = relevanteAktiviteter.some((aktivitet) =>
-            erPeriodeInnenforAnnenPeriode(periode, aktivitet)
-        );
-
-        if (!aktivitetInnenfor) {
-            return {
-                ...stønadsperiodeFeil,
-                aktivitet: 'Ingen relevante aktivitetperioder rommer stønadsperioden',
             };
         }
 
