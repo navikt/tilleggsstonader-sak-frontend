1b4b64908f580b2dbef73ebb480468dd4db48fc2
sarahjelle <sara.hjelle@bekk.no>
2024-02-07 14:41:58 +0100

    Styling. Container margin

--- src/frontend/Sider/Oppgavebenk/Oppgavebenk.tsx
+++ src/frontend/Sider/Oppgavebenk/Oppgavebenk.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 import styled from 'styled-components';
 
-import { Alert } from '@navikt/ds-react';
+import { Alert, VStack } from '@navikt/ds-react';
 
 import { Oppgavefiltrering } from './filter/Oppgavefiltrering';
 import Oppgavetabell from './Oppgavetabell';
@@ -13,7 +13,11 @@ import DataViewer from '../../komponenter/DataViewer';
 import { Feilmelding } from '../../komponenter/Feil/Feilmelding';
 import { erLokalt } from '../../utils/miljø';
 
-const Container = styled.div`
+const Container = styled(VStack).attrs({ gap: '8' })`
+    margin: 2rem;
+`;
+
+const AlertContainer = styled.div`
     margin: 2rem;
     width: fit-content;
 `;
@@ -21,13 +25,13 @@ const Container = styled.div`
 const OppgavebenkContainer = () => {
     const { feilmelding, oppgaveRessurs } = useOppgave();
     return (
-        <div>
+        <Container>
             <Oppgavefiltrering />
             <DataViewer response={{ oppgaver: oppgaveRessurs }}>
                 {({ oppgaver }) => <Oppgavetabell oppgaver={oppgaver.oppgaver} />}
             </DataViewer>
             <Feilmelding>{feilmelding}</Feilmelding>
-        </div>
+        </Container>
     );
 };
 
@@ -40,12 +44,12 @@ const Oppgavebenk: React.FC = () => {
 
     if (!erSaksbehandler) {
         return (
-            <Container>
+            <AlertContainer>
                 <Alert variant={'info'}>
                     Oppgavebenken er ikke tilgjengelig for veiledere. Benytt fødselsnummer i
                     søkefelt for å finne informasjon om en person
                 </Alert>
-            </Container>
+            </AlertContainer>
         );
     }
 
