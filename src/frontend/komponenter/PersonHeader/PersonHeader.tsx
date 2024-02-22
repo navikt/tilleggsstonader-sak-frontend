5dc24887f790cd8d4cc1c8346335ec06f15c0e40
Eirik Årseth <eirik.arseth@nav.no>
2024-02-22 13:34:38 +0100

    Skal lenke til personoversikt når man trykker på ident i headeren i behandling

--- src/frontend/komponenter/PersonHeader/PersonHeader.tsx
+++ src/frontend/komponenter/PersonHeader/PersonHeader.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import styled from 'styled-components';
 
-import { BodyShort, CopyButton, HStack, Heading } from '@navikt/ds-react';
+import { BodyShort, CopyButton, HStack, Heading, Link } from '@navikt/ds-react';
 import { ABorderStrong, ASpacing2, ASpacing4 } from '@navikt/ds-tokens/dist/tokens';
 
+import { useApp } from '../../context/AppContext';
 import { usePersonopplysninger } from '../../context/PersonopplysningerContext';
+import { RessursStatus } from '../../typer/ressurs';
+import { Søkeresultat } from '../PersonSøk';
 import { Sticky } from '../Visningskomponenter/Sticky';
 
 const Container = styled(Sticky)`
@@ -21,13 +24,31 @@ const Container = styled(Sticky)`
 
 const PersonHeader: React.FC = () => {
     const { personopplysninger } = usePersonopplysninger();
+    const { request } = useApp();
+    const [fagsakPersonId, settFagsakPersonId] = useState<string>();
+
+    const søkPerson = useCallback(() => {
+        request<Søkeresultat, { personIdent: string }>(`/api/sak/sok/person`, 'POST', {
+            personIdent: personopplysninger.personIdent,
+        }).then((resultat) => {
+            if (resultat.status === RessursStatus.SUKSESS) {
+                settFagsakPersonId(resultat.data.fagsakPersonId);
+            }
+        });
+    }, [personopplysninger.personIdent, request]);
+
+    useEffect(søkPerson, [søkPerson]);
 
     return (
         <Container>
             <Heading size="xsmall">{personopplysninger.navn.visningsnavn}</Heading>
             <BodyShort>|</BodyShort>
             <HStack gap="2" align="center">
-                {personopplysninger.personIdent}
+                {fagsakPersonId ? (
+                    <Link href={`/person/${fagsakPersonId}`}>{personopplysninger.personIdent}</Link>
+                ) : (
+                    <BodyShort>{personopplysninger.personIdent}</BodyShort>
+                )}
                 <CopyButton copyText={personopplysninger.personIdent} size="small" />
             </HStack>
         </Container>
