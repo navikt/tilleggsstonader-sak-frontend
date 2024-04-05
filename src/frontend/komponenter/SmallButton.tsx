37ef20b6c428afc181c260bab157206493177406
Runar Skagestad <runska@gmail.com>
2024-04-05 09:04:01 +0200

    Bytt til arrow component

--- src/frontend/komponenter/SmallButton.tsx
+++ src/frontend/komponenter/SmallButton.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Button, ButtonProps } from '@navikt/ds-react';
 
-export default function SmallButton(props: ButtonProps) {
+const SmallButton = (props: ButtonProps) => {
     return (
         <Button size="small" style={{ maxWidth: 'fit-content' }} {...props}>
             {props.children}
         </Button>
     );
-}
+};
+
+export default SmallButton;
--- src/frontend/komponenter/SmallWarningTag.tsx
+++ src/frontend/komponenter/SmallWarningTag.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Tag } from '@navikt/ds-react';
 
-export default function SmallWarningTag(props: { children: React.ReactNode }) {
-    return (
-        <Tag size="small" variant={'warning'} style={{ maxWidth: 'fit-content' }}>
-            {props.children}
-        </Tag>
-    );
-}
+const SmallWarningTag = (props: { children: React.ReactNode }) => (
+    <Tag size="small" variant={'warning'} style={{ maxWidth: 'fit-content' }}>
+        {props.children}
+    </Tag>
+);
+
+export default SmallWarningTag;
