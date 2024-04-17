2350f3eb9c99fbff1823f0ac466adeff9086b405
sarahjelle <sara.hjelle@bekk.no>
2024-04-17 15:09:52 +0200

    Leservisning av select som brukes for vilkårperiodetype skal ha readonly som leservisning

--- src/frontend/komponenter/Skjema/SelectMedOptions.tsx
+++ src/frontend/komponenter/Skjema/SelectMedOptions.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from 'react';
 
-import Select, { SelectProps } from './Select';
+import { Select, SelectProps } from '@navikt/ds-react';
 
 export interface SelectOption {
     value: string;
     label: string;
 }
 
-interface Props extends Omit<SelectProps, 'children'> {
+interface Props extends Omit<SelectProps, 'children' | 'readOnly'> {
     valg: SelectOption[];
+    erLesevisning: boolean;
 }
 
-const SelectMedOptions: FC<Props> = ({ valg, ...props }) => {
+const SelectMedOptions: FC<Props> = ({ valg, erLesevisning, ...props }) => {
     return (
-        <Select {...props}>
+        <Select {...props} readOnly={erLesevisning}>
             <option value="">Velg</option>
             {valg.map((v) => (
                 <option key={v.value} value={v.value}>
