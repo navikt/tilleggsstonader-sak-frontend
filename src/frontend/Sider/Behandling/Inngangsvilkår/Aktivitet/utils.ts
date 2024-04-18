d1e144bb14b4cb705ec418d2537eb570420fda09
Johan Blomgren <blommish@users.noreply.github.com>
2024-04-18 19:24:49 +0200

    Når man endrer type så må man rydde opp statet for delvilkår, hvis ikke så risikerer man at man sender vurdering av eks lønnet når den ikke skal være med

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/utils.ts
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/utils.ts
@@ -1,4 +1,5 @@
 import { EndreAktivitetForm } from './EndreAktivitetRad';
+import { AktivitetType, DelvilkårAktivitet } from '../typer/aktivitet';
 
 export const nyAktivitet = (behandlingId: string): EndreAktivitetForm => {
     return {
@@ -10,3 +11,14 @@ export const nyAktivitet = (behandlingId: string): EndreAktivitetForm => {
         delvilkår: { '@type': 'AKTIVITET' },
     };
 };
+
+export const skalVurdereLønnet = (type: AktivitetType | '') => type === AktivitetType.TILTAK;
+
+export const resetDelvilkår = (
+    type: AktivitetType,
+    delvilkår: DelvilkårAktivitet
+): DelvilkårAktivitet => ({
+    ...delvilkår,
+    lønnet: skalVurdereLønnet(type) ? delvilkår.lønnet : undefined,
+    // fjerner ikke mottarSykepenger då den alltid skal vurderes
+});
