3499798119037df53f1e24da278f5636c3828073
Runar Skagestad <runska@gmail.com>
2024-05-08 17:24:16 +0200

    Bytt erProd-sjekk til å gå mot prod som default, men dev dersom hostname inneholder "dev.nav.no"

--- src/frontend/utils/miljø.ts
+++ src/frontend/utils/miljø.ts
@@ -1,4 +1,4 @@
-export const erProd = (): boolean => window.location.host === 'tilleggsstonader.intern.nav.no';
+export const erProd = (): boolean => !window.location.host.includes('dev.nav.no');
 
 export const erLokalt = (): boolean =>
     window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
