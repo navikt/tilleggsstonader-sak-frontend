1a14a440b7b0f8ccf7af36cfa26d4186be09e38b
Runar Skagestad <runska@gmail.com>
2024-05-08 21:54:58 +0200

    Endre erPRod-sjekk så vi ikke får true når vi kjører lokalt

--- src/frontend/utils/miljø.ts
+++ src/frontend/utils/miljø.ts
@@ -1,4 +1,6 @@
-export const erProd = (): boolean => !window.location.host.includes('dev.nav.no');
+export const erProd = (): boolean => !(erLokalt() || erDev());
+
+export const erDev = (): boolean => window.location.host.includes('dev.nav.no');
 
 export const erLokalt = (): boolean =>
     window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
