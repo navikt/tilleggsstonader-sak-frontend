af6725fef542793a87aed30496c5b12b930c3441
sarahjelle <sara.hjelle@bekk.no>
2024-05-14 11:31:21 +0200

    Legg behandlingFakta inn i behandlingcontext

--- src/frontend/Sider/Behandling/BehandlingContainer.tsx
+++ src/frontend/Sider/Behandling/BehandlingContainer.tsx
@@ -7,6 +7,7 @@ import { useApp } from '../../context/AppContext';
 import { useRerunnableEffect } from '../../hooks/useRerunnableEffect';
 import DataViewer from '../../komponenter/DataViewer';
 import { Behandling } from '../../typer/behandling/behandling';
+import { BehandlingFakta } from '../../typer/behandling/behandlingFakta/behandlingFakta';
 import { Personopplysninger } from '../../typer/personopplysninger';
 import { byggTomRessurs, Ressurs } from '../../typer/ressurs';
 
@@ -15,6 +16,8 @@ const BehandlingContainer = () => {
     const [behandling, settBehandling] = useState<Ressurs<Behandling>>(byggTomRessurs());
     const [personopplysninger, settPersonopplysninger] =
         useState<Ressurs<Personopplysninger>>(byggTomRessurs());
+    const [behandlingFakta, settBehandlingFakta] =
+        useState<Ressurs<BehandlingFakta>>(byggTomRessurs());
 
     const behandlingId = useParams<{
         behandlingId: string;
@@ -36,13 +39,22 @@ const BehandlingContainer = () => {
         document.title = 'Behandling';
     }, []);
 
+    const hentBehandlingFaktaCallback = useCallback(() => {
+        request<BehandlingFakta, null>(`/api/sak/behandling/${behandlingId}/fakta`).then(
+            settBehandlingFakta
+        );
+    }, [request, behandlingId]);
+
+    useEffect(hentBehandlingFaktaCallback, [hentBehandlingFaktaCallback]);
+
     return (
-        <DataViewer response={{ behandling, personopplysninger }}>
-            {({ behandling, personopplysninger }) => (
+        <DataViewer response={{ behandling, personopplysninger, behandlingFakta }}>
+            {({ behandling, personopplysninger, behandlingFakta }) => (
                 <BehandlingInnhold
                     behandling={behandling}
                     hentBehandling={hentBehandling}
                     personopplysninger={personopplysninger}
+                    behandlingFakta={behandlingFakta}
                 />
             )}
         </DataViewer>
--- src/frontend/Sider/Behandling/Venstremeny/Oppsummering/Oppsummering.tsx
+++ src/frontend/Sider/Behandling/Venstremeny/Oppsummering/Oppsummering.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
 import { VStack } from '@navikt/ds-react';
 
@@ -7,64 +7,42 @@ import ArbeidOgOpphold from './ArbeidOgOpphold';
 import BarnDetaljer from './BarnDetaljer';
 import Vedlegg from './Vedlegg';
 import { Informasjonskilde, Informasjonsrad, InfoSeksjon } from './Visningskomponenter';
-import { useApp } from '../../../../context/AppContext';
 import { useBehandling } from '../../../../context/BehandlingContext';
-import DataViewer from '../../../../komponenter/DataViewer';
-import { BehandlingFakta } from '../../../../typer/behandling/behandlingFakta/behandlingFakta';
 import { hovedytelseTilTekst } from '../../../../typer/behandling/behandlingFakta/faktaHovedytelse';
-import { byggTomRessurs, Ressurs } from '../../../../typer/ressurs';
 import { tekstEllerKode } from '../../../../utils/tekstformatering';
 
 const Oppsummering: React.FC = () => {
-    const { request } = useApp();
-    const { behandling } = useBehandling();
-
-    const [behandlingFakta, settBehandlingFakta] =
-        useState<Ressurs<BehandlingFakta>>(byggTomRessurs());
-
-    const hentBehandlingFaktaCallback = useCallback(() => {
-        request<BehandlingFakta, null>(`/api/sak/behandling/${behandling.id}/fakta`).then(
-            settBehandlingFakta
-        );
-    }, [request, behandling.id]);
-
-    useEffect(hentBehandlingFaktaCallback, [hentBehandlingFaktaCallback]);
+    const { behandlingFakta } = useBehandling();
 
     return (
-        <DataViewer response={{ behandlingFakta }}>
-            {({ behandlingFakta }) => (
-                <VStack gap="8">
-                    <InfoSeksjon label="Ytelse/situasjon">
-                        <Informasjonsrad
-                            kilde={Informasjonskilde.SØKNAD}
-                            verdi={behandlingFakta.hovedytelse.søknadsgrunnlag?.hovedytelse
-                                ?.map((hovedytelse) =>
-                                    tekstEllerKode(hovedytelseTilTekst, hovedytelse)
-                                )
-                                ?.join(', ')}
-                        />
-                    </InfoSeksjon>
-                    {behandlingFakta.hovedytelse.søknadsgrunnlag?.arbeidOgOpphold && (
-                        <InfoSeksjon label={'Arbeid og opphold'}>
-                            <ArbeidOgOpphold
-                                fakta={behandlingFakta.hovedytelse.søknadsgrunnlag.arbeidOgOpphold}
-                            />
-                        </InfoSeksjon>
-                    )}
-
-                    <InfoSeksjon label="Aktivitet">
-                        {/* TODO: Legg inn info om aktiviteter*/}
-                        <Aktivitet aktivitet={behandlingFakta.aktivitet}></Aktivitet>
-                    </InfoSeksjon>
-
-                    {behandlingFakta.barn.map((barn) => (
-                        <BarnDetaljer barn={barn} key={barn.barnId} />
-                    ))}
-
-                    <Vedlegg fakta={behandlingFakta.dokumentasjon} />
-                </VStack>
+        <VStack gap="8">
+            <InfoSeksjon label="Ytelse/situasjon">
+                <Informasjonsrad
+                    kilde={Informasjonskilde.SØKNAD}
+                    verdi={behandlingFakta.hovedytelse.søknadsgrunnlag?.hovedytelse
+                        ?.map((hovedytelse) => tekstEllerKode(hovedytelseTilTekst, hovedytelse))
+                        ?.join(', ')}
+                />
+            </InfoSeksjon>
+            {behandlingFakta.hovedytelse.søknadsgrunnlag?.arbeidOgOpphold && (
+                <InfoSeksjon label={'Arbeid og opphold'}>
+                    <ArbeidOgOpphold
+                        fakta={behandlingFakta.hovedytelse.søknadsgrunnlag.arbeidOgOpphold}
+                    />
+                </InfoSeksjon>
             )}
-        </DataViewer>
+
+            <InfoSeksjon label="Aktivitet">
+                {/* TODO: Legg inn info om aktiviteter*/}
+                <Aktivitet aktivitet={behandlingFakta.aktivitet}></Aktivitet>
+            </InfoSeksjon>
+
+            {behandlingFakta.barn.map((barn) => (
+                <BarnDetaljer barn={barn} key={barn.barnId} />
+            ))}
+
+            <Vedlegg fakta={behandlingFakta.dokumentasjon} />
+        </VStack>
     );
 };
 
