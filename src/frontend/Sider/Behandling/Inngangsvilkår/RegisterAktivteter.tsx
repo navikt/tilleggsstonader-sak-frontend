a24f9aebb9d8eed223188082fcdcf91d28055d04
Eirik Årseth <eirik.arseth@nav.no>
2024-05-28 10:41:10 +0200

    Ikke vis tabell for aktiviteter dersom ingen aktiviteter finnes

--- src/frontend/Sider/Behandling/Inngangsvilkår/RegisterAktivteter.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/RegisterAktivteter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { styled } from 'styled-components';
 
-import { Button, Heading, Table } from '@navikt/ds-react';
+import { Alert, Button, Heading, Table } from '@navikt/ds-react';
 
 import { RegisterAktivteter } from './typer/vilkårperiode';
 import { useInngangsvilkår } from '../../../context/InngangsvilkårContext';
@@ -19,43 +19,49 @@ const RegisterAktiviteter: React.FC<{ aktivitetGrunnlag: RegisterAktivteter }> =
     return (
         <div>
             <Heading size="small">Brukers registrerte aktiviteter</Heading>
-            <Tabell size={'small'}>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell scope="col">Type</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Status</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Startdato</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Sluttdato</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Aktivitetsdager</Table.HeaderCell>
-                        <Table.HeaderCell scope="col"></Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {aktivitetGrunnlag.aktiviteter.map((aktivitet) => {
-                        return (
-                            <Table.Row key={aktivitet.id}>
-                                <Table.DataCell>{aktivitet.type}</Table.DataCell>
-                                <Table.DataCell>{aktivitet.typeNavn}</Table.DataCell>
-                                <Table.DataCell>
-                                    {formaterNullableIsoDato(aktivitet.fom)}
-                                </Table.DataCell>
-                                <Table.DataCell>
-                                    {formaterNullableIsoDato(aktivitet.tom)}
-                                </Table.DataCell>
-                                <Table.DataCell>{aktivitet.antallDagerPerUke}</Table.DataCell>
-                                <Table.DataCell>
-                                    <Button
-                                        size={'small'}
-                                        onClick={() => leggTilAktivitetFraRegister(aktivitet)}
-                                    >
-                                        Legg til
-                                    </Button>
-                                </Table.DataCell>
-                            </Table.Row>
-                        );
-                    })}
-                </Table.Body>
-            </Tabell>
+            {aktivitetGrunnlag.aktiviteter.length > 0 ? (
+                <Tabell size={'small'}>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell scope="col">Type</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Status</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Startdato</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Sluttdato</Table.HeaderCell>
+                            <Table.HeaderCell scope="col">Aktivitetsdager</Table.HeaderCell>
+                            <Table.HeaderCell scope="col"></Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {aktivitetGrunnlag.aktiviteter.map((aktivitet) => {
+                            return (
+                                <Table.Row key={aktivitet.id}>
+                                    <Table.DataCell>{aktivitet.type}</Table.DataCell>
+                                    <Table.DataCell>{aktivitet.typeNavn}</Table.DataCell>
+                                    <Table.DataCell>
+                                        {formaterNullableIsoDato(aktivitet.fom)}
+                                    </Table.DataCell>
+                                    <Table.DataCell>
+                                        {formaterNullableIsoDato(aktivitet.tom)}
+                                    </Table.DataCell>
+                                    <Table.DataCell>{aktivitet.antallDagerPerUke}</Table.DataCell>
+                                    <Table.DataCell>
+                                        <Button
+                                            size={'small'}
+                                            onClick={() => leggTilAktivitetFraRegister(aktivitet)}
+                                        >
+                                            Legg til
+                                        </Button>
+                                    </Table.DataCell>
+                                </Table.Row>
+                            );
+                        })}
+                    </Table.Body>
+                </Tabell>
+            ) : (
+                <Alert variant={'info'} inline>
+                    Bruker har ingen registrerte aktiviteter
+                </Alert>
+            )}
         </div>
     );
 };
