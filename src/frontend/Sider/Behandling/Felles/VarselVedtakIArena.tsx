4300d18f600915c1460ba0f91f31fecd157b89be
Johan Blomgren <blommish@users.noreply.github.com>
2024-06-04 07:45:25 +0200

    Flytter VarselVedtakIArena kun til å vises i Inngangsvilkår.tsx

--- src/frontend/Sider/Behandling/Felles/VarselVedtakIArena.tsx
+++ src/frontend/Sider/Behandling/Felles/VarselVedtakIArena.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
 import { Alert, Heading } from '@navikt/ds-react';
 
 import { useBehandling } from '../../../context/BehandlingContext';
 import { formaterTilTekstligDato } from '../../../utils/dato';
-
-const Container = styled.div`
-    margin: 1rem 2rem 0 2rem;
-`;
 
 export const VarselVedtakIArena = () => {
     const { behandlingFakta } = useBehandling();
@@ -18,16 +12,15 @@ export const VarselVedtakIArena = () => {
     if (!vedtakTom) {
         return null;
     }
+
     return (
-        <Container>
-            <Alert variant={'warning'} size={'small'}>
-                <Heading size={'xsmall'} level="3">
-                    Søker har vedtak i Arena på tilsyn barn innvilget til og med{' '}
-                    {formaterTilTekstligDato(vedtakTom)}
-                </Heading>
-                Skal du innvilge tilbake i tid? Gå til Arena for å sjekke at det ikke blir
-                overlappende utbetalinger.
-            </Alert>
-        </Container>
+        <Alert variant={'warning'} size={'small'}>
+            <Heading size={'xsmall'} level="3">
+                Søker har vedtak i Arena på tilsyn barn innvilget til og med{' '}
+                {formaterTilTekstligDato(vedtakTom)}
+            </Heading>
+            Skal du innvilge tilbake i tid? Gå til Arena for å sjekke at det ikke blir overlappende
+            utbetalinger.
+        </Alert>
     );
 };
