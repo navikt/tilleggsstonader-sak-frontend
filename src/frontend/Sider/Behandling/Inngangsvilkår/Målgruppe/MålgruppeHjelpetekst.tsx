6a748ffc887c64b1d413192c4acbf2957407b358
Johan Blomgren <blommish@users.noreply.github.com>
2024-06-05 15:14:19 +0200

    Endret til BodyLong med spacing

--- src/frontend/Sider/Behandling/Inngangsvilkår/Målgruppe/MålgruppeHjelpetekst.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Målgruppe/MålgruppeHjelpetekst.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { ReadMore } from '@navikt/ds-react';
+import { BodyLong, ReadMore } from '@navikt/ds-react';
 
 export const MålgruppeHjelpetekst = () => (
     <ReadMore header={'Slik gjør du vurderingen'}>
-        <p>
+        <BodyLong size={'small'} spacing>
             Dersom søker oppgir å ha <strong>overgangsstønad</strong>, men ikke mottar stønaden, må
             du be NAY enslig forsørger (kontornummer 4489) vurdere om søker har rett til
             overgangsstønad. Dersom søker har rett, settes målgruppe overgangsstønad til oppfylt.
-        </p>
-        <p>
+        </BodyLong>
+        <BodyLong size={'small'} spacing>
             Dersom søker oppgir å ha <strong>omstillingsstønad</strong>, men ikke mottar stønaden,
             må du be NAV Familie- og Pensjonsytelser vurdere om søker har rett til
             omstillingsstønad. Dersom søker har rett, settes målgruppe omstillingsstønad til
@@ -18,17 +18,17 @@ export const MålgruppeHjelpetekst = () => (
             målgruppe omstillingsstønad til oppfylt. Dersom søker heller ikke har
             gjenlevendepensjon, må du be NAV Familie- og Pensjonsytelser vurdere om inngangsvilkår
             for tilleggsstønad etter kapittel 17 slik det lød før 01.01.24, er oppfylt.
-        </p>
-        <p>
+        </BodyLong>
+        <BodyLong size={'small'} spacing>
             Dersom søker oppgir <strong>nedsatt arbeidsevne</strong>, men ikke mottar AAP eller
             uføretrygd, må du be NAV- kontoret vurdere om vilkårene for nedsatt arbeidsevne i § 11
             A-3 er oppfylt.
-        </p>
-        <p>
+        </BodyLong>
+        <BodyLong size={'small'}>
             <strong>100% sykepenger</strong> gjelder hvis søker er sykemeldt fra 100%
             fulltidsstilling eller fra fast deltidsstilling hvor søker ikke har annen inntekt eller
             ytelse fra NAV. F.eks. hvis søker er i 70% stilling og mottar 100% sykepenger for denne
             stillingen.
-        </p>
+        </BodyLong>
     </ReadMore>
 );
