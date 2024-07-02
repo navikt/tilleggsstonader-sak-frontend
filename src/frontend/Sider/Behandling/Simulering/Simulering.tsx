10b5f2866c2ad433b273a2ab0ee62da560d974b9
Eirik Årseth <eirik.arseth@nav.no>
2024-07-02 16:44:05 +0200

    Hent simuleringsresultat

--- src/frontend/Sider/Behandling/Simulering/Simulering.tsx
+++ src/frontend/Sider/Behandling/Simulering/Simulering.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { SimuleringResponse } from './simuleringTyper';
+import { useApp } from '../../../context/AppContext';
+import { useBehandling } from '../../../context/BehandlingContext';
+import { byggTomRessurs, Ressurs } from '../../../typer/ressurs';
 
 const Simulering: React.FC = () => {
+    const { behandling } = useBehandling();
+    const { request } = useApp();
+
+    const [simuleringsresultat, settSimuleringsresultat] =
+        useState<Ressurs<SimuleringResponse>>(byggTomRessurs());
+
+    useEffect(() => {
+        request<SimuleringResponse, null>(`/api/sak/simulering/${behandling.id}`).then(
+            settSimuleringsresultat
+        );
+    }, [request, settSimuleringsresultat, behandling.id]);
+
     return <div>Simulering</div>;
 };
 
