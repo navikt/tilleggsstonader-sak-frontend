03a56fd085fc3a5423801ba5ef48bbf7fe30a487
Eirik Årseth <eirik.arseth@nav.no>
2024-07-05 15:01:50 +0200

    Vis info om simulering ved avslag

--- src/frontend/Sider/Behandling/Simulering/Simulering.tsx
+++ src/frontend/Sider/Behandling/Simulering/Simulering.tsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
 
-import Oppsumering from './Oppsumering';
-import SimuleringTabell from './SimuleringTabell';
+import { Alert } from '@navikt/ds-react';
+
+import SimuleringResultatWrapper from './SimuleringResultatWrapper';
 import { SimuleringResponse } from './simuleringTyper';
 import { harVedtaksresultatMedTilkjentYtelse } from './simuleringUtils';
 import { useApp } from '../../../context/AppContext';
@@ -11,6 +12,7 @@ import { useBehandling } from '../../../context/BehandlingContext';
 import { useVedtak } from '../../../hooks/useVedtak';
 import DataViewer from '../../../komponenter/DataViewer';
 import { byggTomRessurs, Ressurs, RessursStatus } from '../../../typer/ressurs';
+import { typeVedtakTilTekst } from '../../../typer/vedtak';
 
 const Container = styled.div`
     margin: 2rem;
@@ -41,11 +43,17 @@ const Simulering: React.FC = () => {
 
     return (
         <Container>
-            <DataViewer response={{ simuleringsresultat }}>
-                {({ simuleringsresultat }) => (
+            <DataViewer response={{ vedtak }}>
+                {({ vedtak }) => (
                     <>
-                        <Oppsumering oppsumering={simuleringsresultat.oppsummering} />
-                        <SimuleringTabell perioder={simuleringsresultat.perioder} />
+                        {harVedtaksresultatMedTilkjentYtelse(vedtak.type) ? (
+                            <SimuleringResultatWrapper />
+                        ) : (
+                            <Alert variant={'info'} inline>
+                                Ingen simulering for vedtaksresultat{' '}
+                                {typeVedtakTilTekst[vedtak.type].toLowerCase()}
+                            </Alert>
+                        )}
                     </>
                 )}
             </DataViewer>
