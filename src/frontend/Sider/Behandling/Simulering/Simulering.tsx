7df9c82a0760fd719dce8baf2aabb3ad0a2f01f9
Eirik Årseth <eirik.arseth@nav.no>
2024-07-05 14:47:30 +0200

    Skal ikke simulere ved avslag

--- src/frontend/Sider/Behandling/Simulering/Simulering.tsx
+++ src/frontend/Sider/Behandling/Simulering/Simulering.tsx
@@ -5,10 +5,12 @@ import styled from 'styled-components';
 import Oppsumering from './Oppsumering';
 import SimuleringTabell from './SimuleringTabell';
 import { SimuleringResponse } from './simuleringTyper';
+import { harVedtaksresultatMedTilkjentYtelse } from './simuleringUtils';
 import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/BehandlingContext';
+import { useVedtak } from '../../../hooks/useVedtak';
 import DataViewer from '../../../komponenter/DataViewer';
-import { byggTomRessurs, Ressurs } from '../../../typer/ressurs';
+import { byggTomRessurs, Ressurs, RessursStatus } from '../../../typer/ressurs';
 
 const Container = styled.div`
     margin: 2rem;
@@ -21,15 +23,21 @@ const Container = styled.div`
 const Simulering: React.FC = () => {
     const { behandling } = useBehandling();
     const { request } = useApp();
+    const { vedtak } = useVedtak();
 
     const [simuleringsresultat, settSimuleringsresultat] =
         useState<Ressurs<SimuleringResponse>>(byggTomRessurs());
 
     useEffect(() => {
-        request<SimuleringResponse, null>(`/api/sak/simulering/${behandling.id}`).then(
-            settSimuleringsresultat
-        );
-    }, [request, settSimuleringsresultat, behandling.id]);
+        if (
+            vedtak.status === RessursStatus.SUKSESS &&
+            harVedtaksresultatMedTilkjentYtelse(vedtak.data.type)
+        ) {
+            request<SimuleringResponse, null>(`/api/sak/simulering/${behandling.id}`).then(
+                settSimuleringsresultat
+            );
+        }
+    }, [request, settSimuleringsresultat, behandling.id, vedtak]);
 
     return (
         <Container>
