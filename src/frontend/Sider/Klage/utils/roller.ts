3a6e5206f16ef2140030e22a60e0ab428666f37e
Runar Skagestad <runar.skagestad@nav.no>
2024-07-05 11:32:18 +0200

    Klagemodulen 🧼: Fjern roller.ts

--- src/frontend/Sider/Klage/utils/roller.ts
+++
@@ -1,9 +0,0 @@
-export type SaksbehadlerRolle = 'veileder' | 'saksbehandler' | 'beslutter';
-export type Rolle = SaksbehadlerRolle | 'kode6' | 'kode7';
-
-/**
- * Mapper rolle til gruppe
- */
-export type Roller = {
-    [key in Rolle]: string;
-};
