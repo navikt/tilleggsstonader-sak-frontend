975b0347e40b5ea6eb81c6034e7a4c57abae5204
eirikvilpponen <eirik.vilpponen@nav.no>
2024-07-10 13:04:20 +0200

    Refaktorering: Trekker ut mappingfunksjoner til utils-fil

--- src/frontend/Sider/Personoversikt/Behandlingsoversikt/BehandlingOversikt.tsx
+++ src/frontend/Sider/Personoversikt/Behandlingsoversikt/BehandlingOversikt.tsx
@@ -2,17 +2,17 @@ import React, { useEffect } from 'react';
 
 import { Alert } from '@navikt/ds-react';
 
-import { TabellBehandling } from './BehandlingTabell';
 import { FagsakOversikt } from './FagsakOversikt';
 import { useHentFagsakPersonUtvidet } from '../../../hooks/useFagsakPerson';
 import { useHentKlagebehandlinger } from '../../../hooks/useKlagebehandlinger';
 import DataViewer from '../../../komponenter/DataViewer';
-import { Behandling } from '../../../typer/behandling/behandling';
 import { Stønadstype } from '../../../typer/behandling/behandlingTema';
-import { BehandlingType } from '../../../typer/behandling/behandlingType';
-import { KlageBehandling } from '../../../typer/klage';
 import { erFeilressurs, pakkUtHvisSuksess } from '../../../typer/ressurs';
-import { sorterBehandlinger } from '../../../utils/behandlingutil';
+import {
+    mapFagsakPersonRessursTilTabellBehandling,
+    mapKlagesakRessursTilTabellBehandling,
+    sorterBehandlinger,
+} from '../../../utils/behandlingutil';
 
 const Behandlingsoversikt: React.FC<{ fagsakPersonId: string }> = ({ fagsakPersonId }) => {
     const { hentFagsakPerson, fagsakPerson } = useHentFagsakPersonUtvidet();
@@ -25,40 +25,6 @@ const Behandlingsoversikt: React.FC<{ fagsakPersonId: string }> = ({ fagsakPerso
 
     const rekjørHentKlagebehandlinger = () => hentKlagebehandlinger(fagsakPersonId);
     const rekjørHentBehandlinger = () => hentFagsakPerson(fagsakPersonId);
-
-    const mapFagsakPersonRessursTilTabellBehandling = (
-        behandlinger: Behandling[] | undefined
-    ): TabellBehandling[] => {
-        const tabellBehandlinger = behandlinger?.map((behandling) => {
-            return {
-                id: behandling.id,
-                opprettet: behandling.opprettet,
-                type: behandling.type,
-                behandlingsårsak: behandling.behandlingsårsak,
-                status: behandling.status,
-                vedtaksdato: behandling.vedtaksdato,
-                resultat: behandling.resultat,
-            };
-        });
-        return tabellBehandlinger ?? [];
-    };
-
-    const mapKlagesakRessursTilTabellBehandling = (
-        klageBehandlinger: KlageBehandling[] | undefined
-    ): TabellBehandling[] => {
-        const tabellBehandlinger = klageBehandlinger?.map((klageBehandling) => {
-            return {
-                id: klageBehandling.id,
-                opprettet: klageBehandling.opprettet,
-                type: BehandlingType.KLAGE,
-                behandlingsårsak: klageBehandling.årsak,
-                status: klageBehandling.status,
-                vedtaksdato: klageBehandling.vedtaksdato,
-                resultat: klageBehandling.resultat,
-            };
-        });
-        return tabellBehandlinger ? tabellBehandlinger : [];
-    };
 
     const tabellBehandlinger = [
         ...mapFagsakPersonRessursTilTabellBehandling(
--- src/frontend/utils/behandlingutil.ts
+++ src/frontend/utils/behandlingutil.ts
@@ -1,5 +1,10 @@
 import { compareDesc } from 'date-fns';
 
+import { TabellBehandling } from '../Sider/Personoversikt/Behandlingsoversikt/BehandlingTabell';
+import { Behandling } from '../typer/behandling/behandling';
+import { BehandlingType } from '../typer/behandling/behandlingType';
+import { KlageBehandling } from '../typer/klage';
+
 /**
  * Sorterer behandlinger etter vedtaksdato
  * Hvis vedtaksdato ikke finnes på noen av behandlingene, sorteres de etter opprettet
@@ -16,4 +21,38 @@ export const sorterBehandlinger = <T extends { vedtaksdato?: string; opprettet: 
         return compareDesc(new Date(a.opprettet), new Date(b.opprettet));
     }
     return a.vedtaksdato ? 1 : -1;
+};
+
+export const mapFagsakPersonRessursTilTabellBehandling = (
+    behandlinger: Behandling[] | undefined
+): TabellBehandling[] => {
+    const tabellBehandlinger = behandlinger?.map((behandling) => {
+        return {
+            id: behandling.id,
+            opprettet: behandling.opprettet,
+            type: behandling.type,
+            behandlingsårsak: behandling.behandlingsårsak,
+            status: behandling.status,
+            vedtaksdato: behandling.vedtaksdato,
+            resultat: behandling.resultat,
+        };
+    });
+    return tabellBehandlinger ?? [];
+};
+
+export const mapKlagesakRessursTilTabellBehandling = (
+    klageBehandlinger: KlageBehandling[] | undefined
+): TabellBehandling[] => {
+    const tabellBehandlinger = klageBehandlinger?.map((klageBehandling) => {
+        return {
+            id: klageBehandling.id,
+            opprettet: klageBehandling.opprettet,
+            type: BehandlingType.KLAGE,
+            behandlingsårsak: klageBehandling.årsak,
+            status: klageBehandling.status,
+            vedtaksdato: klageBehandling.vedtaksdato,
+            resultat: klageBehandling.resultat,
+        };
+    });
+    return tabellBehandlinger ?? [];
 };
