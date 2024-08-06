5e56de9035d3aa7e57069e5f41d68a24618d8279
sarahjelle <sara.hjelle@bekk.no>
2024-08-06 16:15:54 +0200

    Flytt hjelpefunksjoner ut i egen fil for gjenbruk

--- src/frontend/Sider/Personoversikt/Dokumentoversikt/DokumentTabell.tsx
+++ src/frontend/Sider/Personoversikt/Dokumentoversikt/DokumentTabell.tsx
@@ -4,27 +4,13 @@ import { Table } from '@navikt/ds-react';
 
 import DokumentRad from './DokumentRad';
 import { Underrad } from './Underrad';
+import { grupperDokumenterPåJournalpost, sorterJournalpostPåTid } from './utils';
 import { DokumentInfo } from '../../../typer/dokument';
-import { groupBy } from '../../../utils/utils';
 
 export const DokumentTabell: React.FC<{ dokumenter: DokumentInfo[] }> = ({ dokumenter }) => {
-    const dokumentterGrupperPåJournalpost = groupBy(
-        dokumenter,
-        (dokument) => dokument.journalpostId
-    );
+    const dokumenterGrupperPåJournalpost = grupperDokumenterPåJournalpost(dokumenter);
+    const journalposterSortertPåTid = sorterJournalpostPåTid(dokumenterGrupperPåJournalpost);
 
-    const journalposterSortertPåTid = Object.keys(dokumentterGrupperPåJournalpost).sort(
-        function (a, b) {
-            const datoA = dokumentterGrupperPåJournalpost[a][0].dato;
-            const datoB = dokumentterGrupperPåJournalpost[b][0].dato;
-            if (!datoA) {
-                return -1;
-            } else if (!datoB) {
-                return 1;
-            }
-            return datoA > datoB ? -1 : 1;
-        }
-    );
     return (
         <Table size="small">
             <Table.Header>
@@ -39,7 +25,7 @@ export const DokumentTabell: React.FC<{ dokumenter: DokumentInfo[] }> = ({ dokum
             </Table.Header>
             <Table.Body>
                 {journalposterSortertPåTid.map((journalpost) =>
-                    dokumentterGrupperPåJournalpost[journalpost].map((dokument, indeks) =>
+                    dokumenterGrupperPåJournalpost[journalpost].map((dokument, indeks) =>
                         indeks === 0 ? (
                             <DokumentRad key={dokument.dokumentInfoId} dokument={dokument} />
                         ) : (
