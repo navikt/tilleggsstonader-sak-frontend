41c97a7b45d98aa112785cfa2ac6640d1c13a81d
Johan Blomgren <blommish@users.noreply.github.com>
2024-08-22 07:19:41 +0200

    Fjerner toggling av henting av arena-oppgaver

--- src/frontend/utils/toggles.ts
+++ src/frontend/utils/toggles.ts
@@ -5,5 +5,4 @@ export enum Toggle {
     VIS_SIMULERING = 'sak.simulering',
     KAN_OPPRETTE_BEHANDLING_FRA_JOURNALPOST = 'sak.kan-opprette-behandling-fra-journalpost',
     KAN_OPPDATERE_GRUNNLAG_VILKÅRPERIODE = 'sak.kan-oppdatere-grunnlag-vilkarperiode',
-    ARENA_OPPGAVER = 'sak.frontend.arena-oppgaver',
 }
