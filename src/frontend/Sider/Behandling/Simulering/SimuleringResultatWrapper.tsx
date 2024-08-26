7f37353a7d92f8b3c6cb26de48cb500f59810a9c
Eirik Årseth <eirik.arseth@nav.no>
2024-08-26 15:47:08 +0200

    Saksbehandler skal se hvorfor det ikke kommer simuleringsresultat på simuleringsfanen når det er en førstegangsbehandling

--- src/frontend/Sider/Behandling/Simulering/SimuleringResultatWrapper.tsx
+++ src/frontend/Sider/Behandling/Simulering/SimuleringResultatWrapper.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
+import { Alert } from '@navikt/ds-react';
+
 import SimuleringTabell from './SimuleringTabell';
 import { SimuleringResponse } from './simuleringTyper';
 import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/BehandlingContext';
 import DataViewer from '../../../komponenter/DataViewer';
+import { BehandlingType } from '../../../typer/behandling/behandlingType';
 import { byggTomRessurs, Ressurs } from '../../../typer/ressurs';
 
 const SimuleringResultatWrapper: React.FC = () => {
@@ -20,13 +23,21 @@ const SimuleringResultatWrapper: React.FC = () => {
         );
     }, [request, settSimuleringsresultat, behandling.id]);
 
+    const erFørstegangsbehandling = behandling.type === BehandlingType.FØRSTEGANGSBEHANDLING;
+
     return (
         <DataViewer response={{ simuleringsresultat }}>
             {({ simuleringsresultat }) => (
                 <>
                     {simuleringsresultat ? (
                         <SimuleringTabell perioder={simuleringsresultat.perioder} />
-                    ) : null}
+                    ) : (
+                        <Alert variant={'info'} inline>
+                            {erFørstegangsbehandling
+                                ? 'Ingen simulering for førstegangsbehandling'
+                                : 'Ingen simulering lagret for behandling'}
+                        </Alert>
+                    )}
                 </>
             )}
         </DataViewer>
