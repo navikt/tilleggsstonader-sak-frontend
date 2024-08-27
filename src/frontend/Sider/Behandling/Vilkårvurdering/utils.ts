12e3e21aba07df3d4c96de1c37bf75ae33b3736b
Runar Skagestad <runar.skagestad@nav.no>
2024-08-27 15:20:27 +0200

    Lag funksjon for å opprette ny delvilkårsvurdering. Den er nødvendig for å slippe å måtte spørre backend om denne informasjonen.

--- src/frontend/Sider/Behandling/Vilkårvurdering/utils.ts
+++ src/frontend/Sider/Behandling/Vilkårvurdering/utils.ts
@@ -7,7 +7,7 @@ import {
     Svaralternativ,
 } from '../../../typer/regel';
 import { harIkkeVerdi, harVerdi } from '../../../utils/utils';
-import { Vurdering } from '../vilkår';
+import { Delvilkår, Vilkårsresultat, Vurdering } from '../vilkår';
 
 export function begrunnelseErPåkrevdOgUtfyllt(
     svarsalternativ: Svaralternativ,
@@ -117,3 +117,18 @@ export const kopierBegrunnelse = (
         return nyeVurderinger;
     }
 };
+
+export function lagTomtDelvilkårsett(regler: Regler): Delvilkår[] {
+    return Object.keys(regler).map((regelId) => {
+        return {
+            resultat: Vilkårsresultat.IKKE_TATT_STILLING_TIL,
+            vurderinger: [
+                {
+                    regelId: regelId,
+                    svar: undefined,
+                    begrunnelse: undefined,
+                },
+            ],
+        };
+    });
+}
