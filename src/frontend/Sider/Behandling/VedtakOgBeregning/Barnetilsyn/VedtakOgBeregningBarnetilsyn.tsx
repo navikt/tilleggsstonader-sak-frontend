3e089c40c2e3a5d165e5fcbb8a07080651a4a7ce
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-11 13:05:10 +0200

    Fjerner visning av vilkarsoppsummering som ikke lengre brukes i vedtak

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
@@ -1,22 +1,16 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
-import { useFlag } from '@unleash/proxy-client-react';
 import styled from 'styled-components';
 
 import { HStack } from '@navikt/ds-react';
 
 import AvslåVedtak from './AvslåVedtak/AvslåVedtak';
 import { InnvilgeBarnetilsyn } from './InnvilgeVedtak/InnvilgeBarnetilsyn';
-import OppsummeringVilkår from './OppsummeringVilkår';
-import { useApp } from '../../../../context/AppContext';
-import { useBehandling } from '../../../../context/BehandlingContext';
 import { useVedtak } from '../../../../hooks/useVedtak';
 import DataViewer from '../../../../komponenter/DataViewer';
 import Panel from '../../../../komponenter/Panel/Panel';
-import { Ressurs, RessursStatus, byggTomRessurs } from '../../../../typer/ressurs';
+import { RessursStatus } from '../../../../typer/ressurs';
 import { AvslagBarnetilsyn, InnvilgelseBarnetilsyn, TypeVedtak } from '../../../../typer/vedtak';
-import { Vilkårsoppsummering } from '../../../../typer/vilkårsoppsummering';
-import { Toggle } from '../../../../utils/toggles';
 import VelgVedtakResultat from '../Felles/VelgVedtakResultat';
 
 const Container = styled.div`
@@ -26,29 +20,10 @@ const Container = styled.div`
     gap: 1.5rem;
 `;
 
-const Skillelinje = styled.div`
-    border-left: 1px solid white;
-`;
-
 const VedtakOgBeregningBarnetilsyn: FC = () => {
-    const { request } = useApp();
-    const { behandling } = useBehandling();
     const { vedtak } = useVedtak();
-    const periodiserteVilkårIsEnabled = useFlag(Toggle.VILKÅR_PERIODISERING);
 
-    const [vilkårsoppsummering, settVilkårsoppsummering] =
-        useState<Ressurs<Vilkårsoppsummering>>(byggTomRessurs());
     const [typeVedtak, settTypeVedtak] = useState<TypeVedtak | undefined>();
-
-    const hentVilkåroppsummering = useCallback(() => {
-        request<Vilkårsoppsummering, null>(`/api/sak/vilkarsoppsummering/${behandling.id}`).then(
-            settVilkårsoppsummering
-        );
-    }, [behandling.id, request]);
-
-    useEffect(() => {
-        hentVilkåroppsummering();
-    }, [hentVilkåroppsummering]);
 
     useEffect(() => {
         if (vedtak.status === RessursStatus.SUKSESS) {
@@ -58,28 +33,18 @@ const VedtakOgBeregningBarnetilsyn: FC = () => {
 
     return (
         <>
-            <DataViewer response={{ vedtak, vilkårsoppsummering }}>
-                {({ vedtak, vilkårsoppsummering }) => (
+            <DataViewer response={{ vedtak }}>
+                {({ vedtak }) => (
                     <Container>
                         <Panel tittel="Vedtak">
-                            <HStack gap="10">
-                                {!periodiserteVilkårIsEnabled && (
-                                    <>
-                                        <OppsummeringVilkår
-                                            vilkårsoppsummering={vilkårsoppsummering}
-                                        />
-                                        <Skillelinje />
-                                    </>
+                            <HStack gap="16">
+                                <VelgVedtakResultat
+                                    typeVedtak={typeVedtak}
+                                    settTypeVedtak={settTypeVedtak}
+                                />
+                                {typeVedtak === TypeVedtak.AVSLAG && (
+                                    <AvslåVedtak vedtak={vedtak as AvslagBarnetilsyn} />
                                 )}
-                                <HStack gap="16">
-                                    <VelgVedtakResultat
-                                        typeVedtak={typeVedtak}
-                                        settTypeVedtak={settTypeVedtak}
-                                    />
-                                    {typeVedtak === TypeVedtak.AVSLAG && (
-                                        <AvslåVedtak vedtak={vedtak as AvslagBarnetilsyn} />
-                                    )}
-                                </HStack>
                             </HStack>
                         </Panel>
 
--- src/frontend/typer/vilkårsoppsummering.tsx
+++ src/frontend/typer/vilkårsoppsummering.tsx
@@ -3,19 +3,4 @@ import { Stønadsperiode } from '../Sider/Behandling/Inngangsvilkår/typer/støn
 export interface Vilkårsoppsummering {
     stønadsperioder: Stønadsperiode[];
     visVarselKontantstøtte: boolean;
-
-    // Brukes disse feltene?
-    aktivitet: boolean;
-    målgruppe: boolean;
-    stønadsperiode: boolean;
-    passBarn: BarnOppsummering[];
-}
-
-export interface BarnOppsummering {
-    barnId: string;
-    ident: string;
-    navn: string;
-    alder?: number;
-    alderNårStønadsperiodeBegynner?: number;
-    oppfyllerAlleVilkår: boolean;
 }
