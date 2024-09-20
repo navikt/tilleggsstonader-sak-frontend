cb4297889ed41e527a19c0279aab4204f6d4b15a
Runar Skagestad <runar.skagestad@nav.no>
2024-09-20 08:25:00 +0200

    Legg til oppdateringstidspunkt på aktiviteter i personoversikten

--- src/frontend/Sider/Personoversikt/Aktivitetsoversikt/Aktivitetsoversikt.tsx
+++ src/frontend/Sider/Personoversikt/Aktivitetsoversikt/Aktivitetsoversikt.tsx
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { Heading } from '@navikt/ds-react';
+import { Detail, Heading } from '@navikt/ds-react';
 
 import AktiviteterTabell from './Aktiviteter';
 import { AktiviteterDto } from './AktiviteterDto';
 import { useApp } from '../../../context/AppContext';
 import DataViewer from '../../../komponenter/DataViewer';
 import { byggHenterRessurs, Ressurs } from '../../../typer/ressurs';
-import { formaterTilTekstligDato } from '../../../utils/dato';
+import { formaterDatoMedTidspunkt, formaterTilTekstligDato } from '../../../utils/dato';
 
 const Aktivitetsoversikt: React.FC<{ fagsakPersonId: string }> = ({ fagsakPersonId }) => {
     const { request } = useApp();
 
     const [aktiviteter, settAktiviteter] = useState<Ressurs<AktiviteterDto>>(byggHenterRessurs());
 
-    useEffect(() => {
-        request<AktiviteterDto, null>(`/api/sak/aktivitet/temp/${fagsakPersonId}`, 'GET').then(
-            settAktiviteter
+    const [oppdatertTidspunkt, settOppdatertTidspunkt] = useState<Date | undefined>();
+
+    const hentAktiviter = useCallback(async () => {
+        const response = request<AktiviteterDto, null>(
+            `/api/sak/aktivitet/temp/${fagsakPersonId}`,
+            'GET'
         );
-    }, [request, fagsakPersonId]);
+        settAktiviteter(await response);
+    }, [fagsakPersonId, request]);
+
+    useEffect(() => {
+        hentAktiviter().then(() => settOppdatertTidspunkt(new Date()));
+    }, [fagsakPersonId, hentAktiviter]);
 
     return (
         <DataViewer response={{ aktiviteter }}>
@@ -29,6 +37,7 @@ const Aktivitetsoversikt: React.FC<{ fagsakPersonId: string }> = ({ fagsakPerson
                         {formaterTilTekstligDato(aktiviteter.periodeHentetFra)}
                     </Heading>
                     <AktiviteterTabell aktiviteter={aktiviteter.aktiviteter} />
+                    <Detail>Oppdatert: {formaterDatoMedTidspunkt(oppdatertTidspunkt)}</Detail>
                 </>
             )}
         </DataViewer>
