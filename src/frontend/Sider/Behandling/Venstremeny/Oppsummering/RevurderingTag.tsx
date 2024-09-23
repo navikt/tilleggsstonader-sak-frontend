22b1220659ef6543245ff1f6a870effd18ed0132
Runar Skagestad <runar.skagestad@nav.no>
2024-09-23 14:08:48 +0200

    Legg til årsak til revurdering i revurdering-t

--- src/frontend/Sider/Behandling/Venstremeny/Oppsummering/RevurderingTag.tsx
+++ src/frontend/Sider/Behandling/Venstremeny/Oppsummering/RevurderingTag.tsx
@@ -3,20 +3,23 @@ import React from 'react';
 import { ArrowsSquarepathIcon } from '@navikt/aksel-icons';
 import { Tag } from '@navikt/ds-react';
 
+import { Behandling } from '../../../../typer/behandling/behandling';
 import { BehandlingType } from '../../../../typer/behandling/behandlingType';
+import { formaterEnumVerdi } from '../../../../utils/tekstformatering';
 
-export const RevurderingTag = ({ type }: { type: BehandlingType }) => {
-    if (type == BehandlingType.REVURDERING) {
-        return (
-            <Tag
-                size="small"
-                variant="warning"
-                icon={<ArrowsSquarepathIcon />}
-                style={{ maxWidth: 'fit-content' }}
-            >
-                Revurdering
-            </Tag>
-        );
+export function RevurderingTag({ behandling }: { behandling: Behandling }) {
+    if (behandling.type != BehandlingType.REVURDERING) {
+        return null;
     }
-    return null;
-};
+
+    return (
+        <Tag
+            size="small"
+            variant="warning"
+            icon={<ArrowsSquarepathIcon />}
+            style={{ maxWidth: 'fit-content' }}
+        >
+            Revurdering - {formaterEnumVerdi(behandling.behandlingsårsak)}
+        </Tag>
+    );
+}
