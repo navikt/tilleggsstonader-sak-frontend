b4229d3b3ae6d202905c98d8eaa78aa94cd1d372
Runar Skagestad <runar.skagestad@nav.no>
2024-09-23 11:30:27 +0200

    Legg inn datofelt i Revurderes fra-komponenten
    Co-authored-by: Sara Hjelle <sara.hjelle@nav.no>

--- src/frontend/Sider/Behandling/RevurderFra/RevurderFra.tsx
+++ src/frontend/Sider/Behandling/RevurderFra/RevurderFra.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { styled } from 'styled-components';
+
+import { useBehandling } from '../../../context/BehandlingContext';
+import DateInputMedLeservisning from '../../../komponenter/Skjema/DateInputMedLeservisning';
+
+const Container = styled.div`
+    margin: 2rem;
+`;
 
 export function RevurderFra() {
-    return <p>hei</p>;
+    const { behandlingErRedigerbar } = useBehandling();
+
+    const [revurderFraDato, settRevurderFraDato] = useState<string>();
+
+    return (
+        <Container>
+            <DateInputMedLeservisning
+                label="Revurderes fra"
+                value={revurderFraDato}
+                onChange={settRevurderFraDato}
+                erLesevisning={!behandlingErRedigerbar}
+                size="small"
+            />
+        </Container>
+    );
 }
