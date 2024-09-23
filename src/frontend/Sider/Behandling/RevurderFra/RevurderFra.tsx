5eb005f2853f217cd94d9c6a0da4274bd4133b38
Runar Skagestad <runar.skagestad@nav.no>
2024-09-23 16:27:41 +0200

    Legg inn knapp for å lagre "revurder fra"-datoen

--- src/frontend/Sider/Behandling/RevurderFra/RevurderFra.tsx
+++ src/frontend/Sider/Behandling/RevurderFra/RevurderFra.tsx
@@ -1,28 +1,61 @@
 import React, { useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
+import { VStack } from '@navikt/ds-react';
+
+import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/BehandlingContext';
+import { Feilmelding } from '../../../komponenter/Feil/Feilmelding';
+import SmallButton from '../../../komponenter/Knapper/SmallButton';
 import DateInputMedLeservisning from '../../../komponenter/Skjema/DateInputMedLeservisning';
+import { Behandling } from '../../../typer/behandling/behandling';
+import { RessursStatus } from '../../../typer/ressurs';
+import { FanePath } from '../faner';
 
 const Container = styled.div`
     margin: 2rem;
 `;
 
 export function RevurderFra() {
-    const { behandlingErRedigerbar } = useBehandling();
+    const { behandlingErRedigerbar, behandling, hentBehandling } = useBehandling();
+    const { request } = useApp();
+    const navigate = useNavigate();
 
-    const [revurderFraDato, settRevurderFraDato] = useState<string>();
+    const [feilVedLagring, settFeilVedLagring] = useState<string>();
+
+    const [revurderFraDato, settRevurderFraDato] = useState<string | undefined>(
+        behandling.revurderFra
+    );
+
+    async function endreRevurderFraDato() {
+        const response = await request<Behandling, null>(
+            `/api/sak/behandling/${behandling.id}/revurder-fra/${revurderFraDato}`,
+            'POST'
+        );
+        if (response.status === RessursStatus.SUKSESS) {
+            settFeilVedLagring(undefined);
+            hentBehandling.rerun();
+            navigate(`/behandling/${behandling.id}/${FanePath.INNGANGSVILKÅR}`);
+        } else {
+            settFeilVedLagring(response.frontendFeilmelding);
+        }
+    }
 
     return (
         <Container>
-            <DateInputMedLeservisning
-                label="Revurderes fra"
-                value={revurderFraDato}
-                onChange={settRevurderFraDato}
-                erLesevisning={!behandlingErRedigerbar}
-                size="small"
-            />
+            <VStack gap="2">
+                <DateInputMedLeservisning
+                    label="Revurderes fra"
+                    value={revurderFraDato}
+                    onChange={settRevurderFraDato}
+                    erLesevisning={!behandlingErRedigerbar}
+                    size="small"
+                />
+                <SmallButton onClick={endreRevurderFraDato}>Lagre og gå videre</SmallButton>
+                {feilVedLagring && <Feilmelding>{feilVedLagring}</Feilmelding>}
+            </VStack>
         </Container>
     );
 }
