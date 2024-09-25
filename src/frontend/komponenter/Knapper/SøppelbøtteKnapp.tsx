07b837f04cb5d14ba4525e9e9b2e33cc2969a843
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-25 09:20:43 +0200

    Legger til forwardRef på knapp då den får med ref fra Tooltip i Fritekst, som tidligere logget error

--- src/frontend/komponenter/Knapper/SøppelbøtteKnapp.tsx
+++ src/frontend/komponenter/Knapper/SøppelbøtteKnapp.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { TrashIcon } from '@navikt/aksel-icons';
 import { Button, ButtonProps } from '@navikt/ds-react';
 
-export const SøppelbøtteKnapp = (props: ButtonProps) => {
-    return (
-        <Button
-            icon={<TrashIcon title="Fjern" />}
-            size={props.size ?? 'small'}
-            variant={props.variant ?? 'tertiary'}
-            {...props}
-        >
-            {props.children}
-        </Button>
-    );
-};
+export const SøppelbøtteKnapp = React.forwardRef<HTMLButtonElement, Omit<ButtonProps, 'icon'>>(
+    ({ children, size = 'small', variant = 'tertiary', ...restProps }, ref) => {
+        return (
+            <Button
+                icon={<TrashIcon title="Fjern" />}
+                size={size}
+                variant={variant}
+                ref={ref}
+                {...restProps}
+            >
+                {children}
+            </Button>
+        );
+    }
+);
+
+// Set displayName for easier debugging
+SøppelbøtteKnapp.displayName = 'SøppelbøtteKnapp';
