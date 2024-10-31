ad0211d21ea3ef5e7b0ceac33112f30d730f1445
eirikvilpponen <eirik.vilpponen@nav.no>
2024-10-31 12:19:03 +0100

    Fjerner oppgaveTypeTilVisningstekstSomTarHensynTilKlage
    Co-authored-by: nibedita.pahari <nibedita.pahari @nav.no>

--- src/frontend/Sider/Oppgavebenk/typer/oppgavetema.ts
+++ src/frontend/Sider/Oppgavebenk/typer/oppgavetema.ts
@@ -1,5 +1,3 @@
-import { OppgaveBehandlingstype } from './oppgave';
-
 export type Oppgavetype =
     | 'BEH_SAK'
     | 'JFR'
@@ -43,16 +41,6 @@ export const oppgaveTypeTilTekst: Record<Oppgavetype, string> = {
     VUR_KONS_YTE: 'Vurder konsekvens for ytelse',
     VURD_LIVS: 'Vurder livshendelse',
     VUR_SVAR: 'Vurder svar',
-};
-
-export const oppgaveTypeTilVisningstekstSomTarHensynTilKlage = (
-    oppgavetype: Oppgavetype,
-    oppgaveBehandlingstype?: OppgaveBehandlingstype
-): string => {
-    if (oppgavetype === 'BEH_SAK' && oppgaveBehandlingstype === OppgaveBehandlingstype.Klage) {
-        return 'Behandle sak (klage)';
-    }
-    return oppgaveTypeTilTekst[oppgavetype];
 };
 
 export const oppgaverTyperSomSkalVisesFørst: Oppgavetype[] = [
