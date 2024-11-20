195bbe5e01f3f6359f9c32bda7c96f4b254df023
sarahjelle <sara.hjelle@bekk.no>
2024-11-20 14:12:19 +0100

    Legg til begrunnelsegrunn for læremidler

--- src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/Begrunnelse/utils.ts
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Vilkårperioder/Begrunnelse/utils.ts
@@ -1,11 +1,19 @@
 export enum BegrunnelseGrunner {
+    //Målgruppe
     MEDLEMSKAP = 'MEDLEMSKAP',
     DEKKET_AV_ANNET_REGELVERK = 'DEKKET_AV_ANNET_REGELVERK',
-    NEDSATT_ARBEIDSEVNE = 'NEDSATT_ARBEIDSEVNE',
-    LØNNET = 'LØNNET',
-    INGEN_AKTIVITET = 'INGEN_AKTIVITET',
     SYKEPENGER_100_PROSENT = 'SYKEPENGER_100_PROSENT',
+    NEDSATT_ARBEIDSEVNE = 'NEDSATT_ARBEIDSEVNE',
     INGEN_MÅLGRUPPE = 'INGEN_MÅLGRUPPE',
+
+    // Aktivitet felles
+    INGEN_AKTIVITET = 'INGEN_AKTIVITET',
+
+    // Barnetilsyn
+    LØNNET = 'LØNNET',
+
+    // Læremidler
+    HAR_UTGIFTER = 'HAR_UTGIFTER',
 }
 
 export const begrunnelseTilTekst: Record<BegrunnelseGrunner, string> = {
@@ -16,4 +24,5 @@ export const begrunnelseTilTekst: Record<BegrunnelseGrunner, string> = {
     INGEN_AKTIVITET: 'Ingen aktivitet',
     SYKEPENGER_100_PROSENT: '100% sykepenger',
     INGEN_MÅLGRUPPE: 'Ingen målgruppe',
+    HAR_UTGIFTER: 'Hvorfor bruker ikke har utgifter',
 };
