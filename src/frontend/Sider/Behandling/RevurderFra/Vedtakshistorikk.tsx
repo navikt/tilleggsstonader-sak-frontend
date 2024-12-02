936d53d4ebfc30415fdd97020584987912e7cd2e
eirikvilpponen <eirik.vilpponen@nav.no>
2024-12-02 12:37:28 +0100

    Legger til DataViewer for å håndtere feilmeldinger.Dummy-data

--- src/frontend/Sider/Behandling/RevurderFra/Vedtakshistorikk.tsx
+++ src/frontend/Sider/Behandling/RevurderFra/Vedtakshistorikk.tsx
@@ -4,18 +4,40 @@ import styled from 'styled-components';
 
 import { Heading, VStack } from '@navikt/ds-react';
 
-import VedtakshistorikkTabellVisning from './VedtakshistorikkTabellVisning';
+import VedtakshistorikkTabellVisning, {
+    historiskeDataForEnkeltVedtak,
+} from './VedtakshistorikkTabellVisning';
+import DataViewer from '../../../komponenter/DataViewer';
+import { RessursStatus, RessursSuksess } from '../../../typer/ressurs';
 
 const InnholdWrapper = styled.div`
     max-width: 40rem;
 `;
+
+const dummyDataVedtakshistorikkRessurs: RessursSuksess<historiskeDataForEnkeltVedtak[]> = {
+    status: RessursStatus.SUKSESS,
+    data: [
+        {
+            fom: '2021-04-28T19:12:14.358Z',
+            tom: '2021-06-28T19:12:14.358Z',
+            aktivitet: 'Arbeidstrening',
+            målgruppe: 'AAP',
+            antallBarn: '2',
+            utgift: '6000',
+        },
+    ],
+};
 
 export const Vedtakshistorikk = () => {
     return (
         <InnholdWrapper>
             <VStack gap="4">
                 <Heading size="xsmall">Vedtakshistorikk tilsyn barn TS-Sak </Heading>
-                <VedtakshistorikkTabellVisning />
+                <DataViewer response={{ dataVedtakshistorikk: dummyDataVedtakshistorikkRessurs }}>
+                    {({ dataVedtakshistorikk }) => (
+                        <VedtakshistorikkTabellVisning data={dataVedtakshistorikk} />
+                    )}
+                </DataViewer>
             </VStack>
         </InnholdWrapper>
     );
