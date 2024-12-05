25071a6a26f6419f64b650876d7ef4fa42bca5ac
Johan Blomgren <blommish@users.noreply.github.com>
2024-12-05 06:50:38 +0100

    Grupperer toggles, de som gjelder permission kan ligge nederst

--- src/frontend/utils/toggles.ts
+++ src/frontend/utils/toggles.ts
@@ -1,13 +1,15 @@
 export enum Toggle {
     KAN_OPPRETTE_REVURDERING = 'sak.kan-opprette-revurdering',
 
-    BEHANDLING_ÅRSAK_UTEN_BREV = 'sak.behandling-arsak-uten-brev',
-
-    ADMIN_KAN_OPPRETTE_BEHANDLING = 'sak.admin-kan-opprette-behandling',
-
     KAN_SAKSBEHANDLE_LÆREMIDLER = 'sak.frontend.kan-saksbehandle.laremidler',
     KAN_REVURDERE_LÆREMIDLER = 'sak.frontend.kan-revurdere.laremidler',
 
     FORESLÅ_STØNADSPERIODER = 'sak.foresla-stonadsperioder',
     OPPHØR = 'sak.opphor',
+
+    /**
+     * Permission-toggles
+     */
+    BEHANDLING_ÅRSAK_UTEN_BREV = 'sak.behandling-arsak-uten-brev',
+    ADMIN_KAN_OPPRETTE_BEHANDLING = 'sak.admin-kan-opprette-behandling',
 }
