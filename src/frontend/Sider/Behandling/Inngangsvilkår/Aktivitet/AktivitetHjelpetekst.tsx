87244a7f6fa7e163e3a0a177187a1ac65f3634f8
eirikvilpponen <eirik.vilpponen@nav.no>
2024-12-18 10:03:30 +0100

    Bruker BodyLong med spacing istedenfor line-breaks. Dette er tilsvarende som for MålgruppeHjelpetekst

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
-import { ReadMore } from '@navikt/ds-react';
+import { BodyLong, ReadMore } from '@navikt/ds-react';
 
 export const AktivitetHjelpetekst = () => (
     <ReadMore header="Slik gjør du vurderingen" size="small">
-        Du skal bare trykke &#34;Bruk&#34; på aktiviteter som er utdanning eller opplæring. For
-        målgruppe nedsatt arbeidsevne, vil dette si tiltak etter tiltaksforskriften kapittel 7. Hvis
-        du er i tvil om tiltaket er et opplæringstiltak, må du sjekke vedtaket om tildeling av
-        arbeidsmarkedstiltak.
-        <br />
-        <br />
-        Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak som ikke er av type
-        opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;.
-        <br />
-        <br />
-        Merk at enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt som
-        arbeidsmarkedstiltak etter tiltaksforskriften kapittel 7 ikke har rett til stønad til
-        læremidler. Du skal derfor også velge &#34;Ingen utdanning/opplæringstiltak&#34; for enslige
-        som deltar på opplæringstiltak.
+        <BodyLong size={'small'} spacing>
+            Du skal bare trykke &#34;Bruk&#34; på aktiviteter som er utdanning eller opplæring. For
+            målgruppe nedsatt arbeidsevne, vil dette si tiltak etter tiltaksforskriften kapittel 7.
+            Hvis du er i tvil om tiltaket er et opplæringstiltak, må du sjekke vedtaket om tildeling
+            av arbeidsmarkedstiltak.
+        </BodyLong>
+        <BodyLong size={'small'} spacing>
+            Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak som ikke er av
+            type opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;.
+        </BodyLong>
+        <BodyLong size={'small'}>
+            Merk at enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt som
+            arbeidsmarkedstiltak etter tiltaksforskriften kapittel 7 ikke har rett til stønad til
+            læremidler. Du skal derfor også velge &#34;Ingen utdanning/opplæringstiltak&#34; for
+            enslige som deltar på opplæringstiltak.
+        </BodyLong>
     </ReadMore>
 );
