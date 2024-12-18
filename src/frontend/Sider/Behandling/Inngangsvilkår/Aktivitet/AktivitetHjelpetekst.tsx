4b7dfa993d370679873af037c1522746937969bb
eirikvilpponen <eirik.vilpponen@nav.no>
2024-12-18 09:46:14 +0100

    Legger til linjeskift i hjelpeteksten

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
@@ -7,9 +7,14 @@ export const AktivitetHjelpetekst = () => (
         Du skal bare trykke &#34;Bruk&#34; på aktiviteter som er utdanning eller opplæring. For
         målgruppe nedsatt arbeidsevne, vil dette si tiltak etter tiltaksforskriften kapittel 7. Hvis
         du er i tvil om tiltaket er et opplæringstiltak, må du sjekke vedtaket om tildeling av
-        arbeidsmarkedstiltak. Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak
-        som ikke er av type opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;. Merk at
-        enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt som
+        arbeidsmarkedstiltak.
+        <br />
+        <br />
+        Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak som ikke er av type
+        opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;.
+        <br />
+        <br />
+        Merk at enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt som
         arbeidsmarkedstiltak etter tiltaksforskriften kapittel 7 ikke har rett til stønad til
         læremidler. Du skal derfor også velge &#34;Ingen utdanning/opplæringstiltak&#34; for enslige
         som deltar på opplæringstiltak.
