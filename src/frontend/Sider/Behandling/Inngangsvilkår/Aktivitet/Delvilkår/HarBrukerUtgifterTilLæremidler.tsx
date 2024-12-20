a7c2274e501a289aac67095cdbe958ed1e609c04
Runar Skagestad <runar.skagestad@nav.no>
2024-12-20 12:32:49 +0100

    ✨ Viser ulike hjelpetekst for "har utgifter" basert på om det gjelder utdanning eller tiltak

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/Delvilkår/HarBrukerUtgifterTilLæremidler.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/Delvilkår/HarBrukerUtgifterTilLæremidler.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
 
 import { JaNeiVurdering } from '../../../Vilkårvurdering/JaNeiVurdering';
+import { AktivitetType } from '../../typer/vilkårperiode/aktivitet';
 import { SvarJaNei } from '../../typer/vilkårperiode/vilkårperiode';
 import { EndreAktivitetFormLæremidler } from '../EndreAktivitetLæremidler';
 import { erUtdanningEllerTiltak } from '../utilsLæremidler';
-
-const hjelpetekstUtgifter = (
-    <BodyShort>
-        Vi vurderer det slik at søker har utgifter til læremidler hvis de deltar på et
-        opplæringstiltak eller en utdanning godkjent av Nav.
-        <br />
-        <br />
-        Unntaket er AMO-kurs hvor det må undersøkes om læremidler er dekket gjennom kursavgiften.
-    </BodyShort>
-);
 
 export const HarBrukerUtgifterTilLæremidler: React.FC<{
     aktivitetForm: EndreAktivitetFormLæremidler;
@@ -44,8 +35,43 @@ export const HarBrukerUtgifterTilLæremidler: React.FC<{
                     resettHarRettTilUtstyrsstipendSvar();
                 }
             }}
-            hjelpetekst={hjelpetekstUtgifter}
+            hjelpetekst={lagHjelpetekst(aktivitetForm.type)}
             hjelpetekstHeader={'Slik vurderer du om søker har utgifter'}
         />
     );
+};
+
+function lagHjelpetekst(aktivitetType: AktivitetType) {
+    if (!erUtdanningEllerTiltak(aktivitetType)) {
+        return null;
+    }
+
+    return hjelpetekst[aktivitetType];
+}
+
+const hjelpetekst: Record<AktivitetType.TILTAK | AktivitetType.UTDANNING, ReactNode> = {
+    [AktivitetType.UTDANNING]: (
+        <>
+            <BodyShort spacing>
+                Vi vurderer det slik at søker har utgifter til læremidler hvis de deltar på en
+                utdanning godkjent av Nav.
+            </BodyShort>
+            <BodyShort spacing>
+                Unntaket er hvis utdanningen er på grunnskolenivå da skolen skal dekke alle
+                utgifter.
+            </BodyShort>
+        </>
+    ),
+    [AktivitetType.TILTAK]: (
+        <>
+            <BodyShort spacing>
+                Vi vurderer det slik at søker har utgifter til læremidler hvis de deltar på et
+                opplæringstiltak eller en utdanning godkjent av Nav.
+            </BodyShort>
+            <BodyShort spacing>
+                Unntakene er AMO-kurs hvor det må undersøkes om læremidler er dekket gjenom
+                kursavgiften.
+            </BodyShort>
+        </>
+    ),
 };
