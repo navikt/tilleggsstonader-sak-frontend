055f7ee91c0fdbf97f27daf5d1cf606b1f38677c
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-08 14:50:54 +0100

    Skal ikke vise hjelpeteksten koblet til læremidler for tilsyn barn

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/AktivitetHjelpetekst.tsx
@@ -2,23 +2,41 @@ import React from 'react';
 
 import { BodyLong, ReadMore } from '@navikt/ds-react';
 
-export const AktivitetHjelpetekst = () => (
-    <ReadMore header="Slik gjør du vurderingen" size="small">
-        <BodyLong size={'small'} spacing>
-            Du skal bare trykke &#34;Bruk&#34; på aktiviteter som er utdanning eller opplæring. For
-            målgruppe nedsatt arbeidsevne, vil dette si tiltak etter tiltaksforskriften kapittel 7.
-            Hvis du er i tvil om tiltaket er et opplæringstiltak, må du sjekke vedtaket om tildeling
-            av arbeidsmarkedstiltak.
-        </BodyLong>
-        <BodyLong size={'small'} spacing>
-            Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak som ikke er av
-            type opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;.
-        </BodyLong>
-        <BodyLong size={'small'}>
-            Merk at enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt som
-            arbeidsmarkedstiltak etter tiltaksforskriften kapittel 7 ikke har rett til stønad til
-            læremidler. Du skal derfor også velge &#34;Ingen utdanning/opplæringstiltak&#34; for
-            enslige som deltar på opplæringstiltak.
-        </BodyLong>
-    </ReadMore>
+import { useBehandling } from '../../../../context/BehandlingContext';
+import { Stønadstype } from '../../../../typer/behandling/behandlingTema';
+
+export function AktivitetHjelpetekst() {
+    const { behandling } = useBehandling();
+
+    switch (behandling.stønadstype) {
+        case Stønadstype.BARNETILSYN:
+            return null;
+        case Stønadstype.LÆREMIDLER:
+            return hjelpetekstLæremidler;
+        default:
+            return null;
+    }
+}
+
+const hjelpetekstLæremidler = (
+    <>
+        <ReadMore header="Slik gjør du vurderingen" size="small">
+            <BodyLong size={'small'} spacing>
+                Du skal bare trykke &#34;Bruk&#34; på aktiviteter som er utdanning eller opplæring.
+                For målgruppe nedsatt arbeidsevne, vil dette si tiltak etter tiltaksforskriften
+                kapittel 7. Hvis du er i tvil om tiltaket er et opplæringstiltak, må du sjekke
+                vedtaket om tildeling av arbeidsmarkedstiltak.
+            </BodyLong>
+            <BodyLong size={'small'} spacing>
+                Hvis søker ikke har noe arbeidsrettet aktivitet eller deltar i tiltak som ikke er av
+                type opplæring, velger du &#34;Ingen utdanning/opplæringstiltak&#34;.
+            </BodyLong>
+            <BodyLong size={'small'}>
+                Merk at enslige mor eller far som gjennomfører utdanning/opplæring som er tildelt
+                som arbeidsmarkedstiltak etter tiltaksforskriften kapittel 7 ikke har rett til
+                stønad til læremidler. Du skal derfor også velge &#34;Ingen
+                utdanning/opplæringstiltak&#34; for enslige som deltar på opplæringstiltak.
+            </BodyLong>
+        </ReadMore>
+    </>
 );
