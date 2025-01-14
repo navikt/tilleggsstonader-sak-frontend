0c203883e826ad18493c1dd13cd087225eaeadf8
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-14 09:11:02 +0100

    Skal vise tekst koblet til stønadstype for vedtak i arena

--- src/frontend/Sider/Behandling/Felles/VarselVedtakIArena.tsx
+++ src/frontend/Sider/Behandling/Felles/VarselVedtakIArena.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { Alert, Heading } from '@navikt/ds-react';
 
 import { useBehandling } from '../../../context/BehandlingContext';
+import { stønadstypeTilTekst } from '../../../typer/behandling/behandlingTema';
 import { formaterTilTekstligDato } from '../../../utils/dato';
 
 export const VarselVedtakIArena = () => {
-    const { behandlingFakta } = useBehandling();
+    const { behandling, behandlingFakta } = useBehandling();
 
     const vedtakTom = behandlingFakta.arena?.vedtakTom;
     if (!vedtakTom) {
@@ -16,7 +17,8 @@ export const VarselVedtakIArena = () => {
     return (
         <Alert variant={'warning'} size={'small'}>
             <Heading size={'xsmall'} level="3">
-                Søker har vedtak i Arena på tilsyn barn innvilget til og med{' '}
+                Søker har vedtak i Arena for{' '}
+                {stønadstypeTilTekst[behandling.stønadstype].toLowerCase()} innvilget til og med{' '}
                 {formaterTilTekstligDato(vedtakTom)}
             </Heading>
             Skal du innvilge tilbake i tid? Gå til Arena for å sjekke at det ikke blir overlappende
