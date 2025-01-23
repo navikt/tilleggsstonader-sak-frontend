d8ea53a43fd58080be7e80d91f8c5095cfb202ab
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-23 16:39:05 +0100

    Returnerer om det finnes mangler eller ikke i stedet for void

--- src/frontend/context/BrevFeilContext.ts
+++ src/frontend/context/BrevFeilContext.ts
@@ -47,10 +47,10 @@ export const [BrevFeilContextProvider, useBrevFeilContext] = constate(() => {
         inkluderteDelmaler: Record<string, boolean>,
         valgfelt: Partial<Record<string, Record<Valgfelt['_id'], Valg>>>,
         variabler: Partial<Record<string, string>>
-    ) => {
-        return settManglendeBrevVariabler(
-            finnManglendeBrevVariabler(mal, inkluderteDelmaler, valgfelt, variabler)
-        );
+    ): 'HAR_MANGEL' | 'HAR_IKKE_MANGEL' => {
+        const mangler = finnManglendeBrevVariabler(mal, inkluderteDelmaler, valgfelt, variabler);
+        settManglendeBrevVariabler(mangler);
+        return mangler.length > 0 ? 'HAR_MANGEL' : 'HAR_IKKE_MANGEL';
     };
 
     const brevMalManglerVariabler = manglendeBrevVariabler.length !== 0;
