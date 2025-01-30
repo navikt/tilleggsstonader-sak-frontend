c3d565e025513006279c81a21790e295da82e5a2
sarahjelle <sara.hjelle@bekk.no>
2025-01-30 12:45:16 +0100

    ♻️ Flytt switch til egen fil for å gjøre det mer oversiktlig

--- src/frontend/Sider/Behandling/Totrinnskontroll/Totrinnskontroll.tsx
+++ src/frontend/Sider/Behandling/Totrinnskontroll/Totrinnskontroll.tsx
@@ -2,64 +2,11 @@ import * as React from 'react';
 import { FC, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
 
-import { ABorderSubtle } from '@navikt/ds-tokens/dist/tokens';
-
-import FatteVedtak from './FatteVedtak';
-import SendtTilBeslutter from './SendtTilBeslutter';
-import TotrinnskontrollUnderkjent from './TotrinnskontrollUnderkjent';
-import { TotrinnskontrollResponse, TotrinnskontrollStatus } from './typer';
+import { TotrinnskontrollSwitch } from './TotrinnskontrollSwitch';
 import { useTotrinnskontroll } from '../../../context/TotrinnskontrollContext';
 import DataViewer from '../../../komponenter/DataViewer';
 import { ModalWrapper } from '../../../komponenter/Modal/ModalWrapper';
-import { Ressurs } from '../../../typer/ressurs';
-
-const BorderBox = styled.div`
-    border: 1px solid ${ABorderSubtle};
-    padding: 0.5rem 1rem;
-    margin: 1rem 0.5rem;
-    border-radius: 0.125rem;
-`;
-
-const TotrinnskontrollSwitch: FC<{
-    totrinnskontroll: TotrinnskontrollResponse;
-    settVisModalGodkjent: (vis: boolean) => void;
-    settTotrinnskontroll: React.Dispatch<React.SetStateAction<Ressurs<TotrinnskontrollResponse>>>;
-}> = ({ totrinnskontroll, settVisModalGodkjent, settTotrinnskontroll }) => {
-    switch (totrinnskontroll.status) {
-        case TotrinnskontrollStatus.UAKTUELT:
-            return null;
-        case TotrinnskontrollStatus.KAN_FATTE_VEDTAK:
-            return (
-                <BorderBox>
-                    <FatteVedtak
-                        settVisGodkjentModal={settVisModalGodkjent}
-                        settTotrinnskontroll={settTotrinnskontroll}
-                    />
-                </BorderBox>
-            );
-        case TotrinnskontrollStatus.TOTRINNSKONTROLL_UNDERKJENT:
-            return (
-                <BorderBox>
-                    <TotrinnskontrollUnderkjent
-                        totrinnskontroll={totrinnskontroll.totrinnskontroll}
-                    />
-                </BorderBox>
-            );
-        case TotrinnskontrollStatus.IKKE_AUTORISERT:
-            return (
-                <BorderBox>
-                    <SendtTilBeslutter
-                        totrinnskontroll={totrinnskontroll.totrinnskontroll}
-                        settTotrinnskontroll={settTotrinnskontroll}
-                    />
-                </BorderBox>
-            );
-        default:
-            return null;
-    }
-};
 
 const Totrinnskontroll: FC = () => {
     const navigate = useNavigate();
