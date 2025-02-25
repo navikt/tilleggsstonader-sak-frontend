e615e74dfcfa8e04e162ca6467565fcccb312280
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-25 08:56:30 +0100

    Update src/frontend/utils/dato.ts
    Signed-off-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/frontend/utils/dato.ts
+++ src/frontend/utils/dato.ts
@@ -165,7 +165,7 @@ export const formaterNullableÅrMåned = (dato: string | undefined): string | un
 export const formaterÅrMåned = (dato: string): string =>
     format(parseISO(dato), 'MMM yyyy', { locale: nb });
 
-// Eksempel: formaterÅrMåned('2023-09-18') -> 'sep 2023'
+// Eksempel: formaterÅrMåned('2023-09-18') -> 'september 2023'
 export const formaterÅrFullMåned = (dato: string): string =>
     format(parseISO(dato), 'MMMM yyyy', { locale: nb });
 
