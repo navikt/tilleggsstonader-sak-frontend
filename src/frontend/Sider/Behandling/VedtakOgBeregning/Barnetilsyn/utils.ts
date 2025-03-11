5c4e07e4fbe5f94450f7509d8b78c70a4e6736c6
Sigmund Berbom <sberbom@gmail.com>
2025-03-11 11:10:39 +0100

    🔥 Fjern ubrukt kode

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/utils.ts
+++
@@ -1,6 +0,0 @@
-import { TypeVedtak } from '../../../../typer/vedtak/vedtak';
-import { InnvilgeBarnetilsynRequest } from '../../../../typer/vedtak/vedtakTilsynBarn';
-
-export const lagVedtakRequest = (): InnvilgeBarnetilsynRequest => {
-    return { type: TypeVedtak.INNVILGELSE };
-};
