c86ecbb2f2a441e1861b1d4bf2b6161d34f1fe5d
Sigmund Berbom <sberbom@gmail.com>
2025-03-14 09:38:10 +0100

    ♻️Ta i bruk `stønadstypeTilTekst` på stønadstype tag

--- src/frontend/Sider/Behandling/Venstremeny/Oppsummering/StønadstypeTag.tsx
+++ src/frontend/Sider/Behandling/Venstremeny/Oppsummering/StønadstypeTag.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 
 import { Tag } from '@navikt/ds-react';
 
-import { Stønadstype } from '../../../../typer/behandling/behandlingTema';
-
-const stønadstypeTagTittel: Record<Stønadstype, string> = {
-    BARNETILSYN: 'Barnetilsyn',
-    LÆREMIDLER: 'Læremidler',
-    BOUTGIFTER: 'Bolig/overnatting',
-};
+import { Stønadstype, stønadstypeTilTekst } from '../../../../typer/behandling/behandlingTema';
 
 export function StønadstypeTag({ stønadstype }: { stønadstype: Stønadstype }) {
     return (
         <Tag size="small" variant="neutral">
-            {stønadstypeTagTittel[stønadstype]}
+            {stønadstypeTilTekst[stønadstype]}
         </Tag>
     );
 }
