e1f4caa1303ff1552b1be3f5f58a6debd7d69e4a
Sigmund Berbom <sberbom@gmail.com>
2025-03-17 14:58:18 +0100

    💬 Oppdater tekster i "read more" om revurder-fra

--- src/frontend/Sider/Behandling/RevurderFra/RevurderFraReadMore.tsx
+++ src/frontend/Sider/Behandling/RevurderFra/RevurderFraReadMore.tsx
@@ -5,7 +5,7 @@ import { BodyLong, List, ReadMore } from '@navikt/ds-react';
 const RevurderFraReadMore = () => (
     <ReadMore header={'Slik setter du revurder fra-dato'} size={'small'} defaultOpen={true}>
         <BodyLong size={'small'}>
-            Revurder fra datoen skal som hovedregel være den første datoen det har skjedd en endring
+            Revurder-fra datoen skal som hovedregel være den første datoen det har skjedd en endring
             i situasjon for brukeren, for eksempel
         </BodyLong>
         <List as="ul" size={'small'}>
@@ -15,7 +15,7 @@ const RevurderFraReadMore = () => (
         </List>
         <BodyLong size={'small'}>
             Unntaket er hvis du skal legge til en ny utgift til pass av barn, da må du sette den 1.
-            i den aktuelle måneden, fordi utgifter til pass barn alltid skal legges inn for hele
+            i den aktuelle måneden, fordi utgifter til pass av barn alltid skal legges inn for hele
             måneder
         </BodyLong>
     </ReadMore>
