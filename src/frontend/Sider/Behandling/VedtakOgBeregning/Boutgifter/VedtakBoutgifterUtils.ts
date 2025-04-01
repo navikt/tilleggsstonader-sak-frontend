cf2b80867388ab036ba123414072a068375e58a5
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-01 08:34:22 +0200

    Skal bruke riktig vedtaksperiode for boutgifter

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Boutgifter/VedtakBoutgifterUtils.ts
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/Boutgifter/VedtakBoutgifterUtils.ts
@@ -1,11 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import { VedtaksperiodeBoutgifter } from '../../../../typer/vedtak/vedtakBoutgifter';
-import { Vedtaksperiode } from '../../../../typer/vedtak/vedtakLæremidler';
 
 export const initialiserVedtaksperioder = (
     vedtaksperioder: VedtaksperiodeBoutgifter[] | undefined
-): Vedtaksperiode[] => {
+): VedtaksperiodeBoutgifter[] => {
     return vedtaksperioder ?? [tomVedtaksperiode()];
 };
 
