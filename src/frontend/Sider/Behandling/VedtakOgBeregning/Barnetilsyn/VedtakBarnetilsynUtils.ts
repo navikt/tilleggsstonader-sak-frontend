a30ef19ddebf8b92b84e52b3ea332e318cc1c54f
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-01 08:35:19 +0200

    Skal bruke riktig vedtaksperiode for tilsyn barn

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakBarnetilsynUtils.ts
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakBarnetilsynUtils.ts
@@ -1,11 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { Vedtaksperiode } from '../../../../typer/vedtak/vedtakLæremidler';
 import { VedtaksperiodeTilsynBarn } from '../../../../typer/vedtak/vedtakTilsynBarn';
 
 export const initialiserVedtaksperioder = (
     vedtaksperioder: VedtaksperiodeTilsynBarn[] | undefined
-): Vedtaksperiode[] => {
+): VedtaksperiodeTilsynBarn[] => {
     return vedtaksperioder ?? [tomVedtaksperiode()];
 };
 
