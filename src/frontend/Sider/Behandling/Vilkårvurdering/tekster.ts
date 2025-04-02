bd6968798dc9741a95c2c0162eabf15ade9c1906
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-04-02 13:57:56 +0200

    Lagt til nye spørsmålene i tekster

--- src/frontend/Sider/Behandling/Vilkårvurdering/tekster.ts
+++ src/frontend/Sider/Behandling/Vilkårvurdering/tekster.ts
@@ -45,14 +45,14 @@ export const regelIdTilSpørsmålKortversjon: Record<RegelId, string> = {
     ANNEN_FORELDER_MOTTAR_STØTTE: 'Mottar annen forelder støtte?',
     HAR_FULLFØRT_FJERDEKLASSE: 'Ferdig med 4. skoleår?',
     NØDVENDIGE_MERUTGIFTER: 'Har nødvendige merutgifter?',
-    DOKUMENTERT_UTGIFTER_OVERNATTING: `Har søker dokumentert utgifter til overnatting tilfredsstillende?`,
+    DOKUMENTERT_UTGIFTER_OVERNATTING: `Dokumentert utgift tilfredsstillende?`,
     UNNTAK_ALDER: 'Unntak fra aldersregelen?',
     HØYERE_BOUTGIFTER_SAMMENLIGNET_MED_TIDLIGERE: 'Dokumentert høyere utgift?',
     NØDVENDIG_Å_BO_NÆRMERE_AKTIVITET: 'Nødvendig å bo nærmere aktivitetssted?',
     RETT_TIL_BOSTØTTE: 'Rett til bostøtte?',
     HØYERE_UTGIFTER_HELSEMESSIG_ÅRSAKER: 'Høyere utgift grunnet helsemessige årsaker?',
     DOKUMENTERT_UTGIFTER_BOLIG: 'Dokumentert utgifter?',
-    SØKER_DELTA_PÅ: `Har søker dokumentert at de har vært/skal de delta på samling/eksamen/opptaksprøve?`,
+    SØKER_DELTA_PÅ: `Dokumentert samling e.l.?`,
 };
 
 export const hjelpetekster: Record<RegelId, string[]> = {
