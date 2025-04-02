6679439a30f3d205157a2e721d7ee4a24102c0d5
Sigmund Berbom <sberbom@gmail.com>
2025-04-02 14:01:22 +0200

    💄 Oppdater lesevisning av vilkår slik at delvilkår spørsmål og svar ikke knekker før siden blir veldig smal

--- src/frontend/Sider/Behandling/Vilkårvurdering/Vurderingsrad.tsx
+++ src/frontend/Sider/Behandling/Vilkårvurdering/Vurderingsrad.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BodyShort, HGrid, HStack, VStack } from '@navikt/ds-react';
+import { BodyShort, HStack, VStack } from '@navikt/ds-react';
 
 import { Delvilkår } from '../vilkår';
 import { regelIdTilSpørsmålKortversjon, svarIdTilTekstKorversjon } from './tekster';
@@ -10,25 +10,23 @@ export function Vurderingsrad(props: { delvilkår: Delvilkår }) {
     const begrunnelse = props.delvilkår.vurderinger.at(-1)?.begrunnelse;
     return (
         <>
-            <HGrid gap={{ md: '4', lg: '8' }} columns="minmax(100px, 250px) auto">
-                <VStack>
-                    {props.delvilkår.vurderinger.map((vurdering, index) => (
-                        <HStack gap="2" key={vurdering.regelId}>
-                            {index === 0 && (
-                                <BodyShort weight="semibold" size="small">
-                                    {regelIdTilSpørsmålKortversjon[vurdering.regelId]}
-                                </BodyShort>
-                            )}
-                            {vurdering.svar && (
-                                <BodyShort size="small">
-                                    {svarIdTilTekstKorversjon[vurdering.svar]}
-                                </BodyShort>
-                            )}
-                        </HStack>
-                    ))}
-                </VStack>
-                <BodyShort size="small">{begrunnelse}</BodyShort>
-            </HGrid>
+            <VStack>
+                {props.delvilkår.vurderinger.map((vurdering, index) => (
+                    <HStack gap="2" key={vurdering.regelId}>
+                        {index === 0 && (
+                            <BodyShort weight="semibold" size="small">
+                                {regelIdTilSpørsmålKortversjon[vurdering.regelId]}
+                            </BodyShort>
+                        )}
+                        {vurdering.svar && (
+                            <BodyShort size="small">
+                                {svarIdTilTekstKorversjon[vurdering.svar]}
+                            </BodyShort>
+                        )}
+                    </HStack>
+                ))}
+            </VStack>
+            <BodyShort size="small">{begrunnelse}</BodyShort>
         </>
     );
 }
