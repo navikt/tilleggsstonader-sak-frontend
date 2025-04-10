7a6bcd2fab0a36964d105e3b6ea5c2814f903ed4
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-10 22:54:36 +0200

    Trigger henting av behandlingshistorikken for klage ved sett på eller ta av vent

--- src/frontend/context/SettPåVentContext.ts
+++ src/frontend/context/SettPåVentContext.ts
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+
 import constate from 'constate';
 
+import { usePrevious } from '../hooks/usePrevious';
 import { RerrunnableEffect } from '../hooks/useRerunnableEffect';
 import { SettPåVentContext } from '../komponenter/SettPåVent/typer';
 
@@ -16,7 +19,23 @@ export const [SettPåVentProvider, useSettPåVent] = constate(
         behandlingErSattPåVent: boolean;
         hentBehandling: RerrunnableEffect;
         hentBehandlingshistorikk: RerrunnableEffect;
+        hentHistorikkPåNyttNårSattPåVent?: boolean;
     }) => {
+        const prevBehandlingErSattPåVent = usePrevious(behandlingErSattPåVent);
+        /**
+         * Klage:
+         * Hack for å trigge henting av behandlingshistorikk på nytt når man setter på/av vent
+         * I vanlig saksbehandling finnes en vanlig trigger for henting av behandlingshistorikk på nytt når status endrer seg
+         */
+        useEffect(() => {
+            if (
+                context === 'klage' &&
+                (prevBehandlingErSattPåVent === true || prevBehandlingErSattPåVent === false) &&
+                prevBehandlingErSattPåVent !== behandlingErSattPåVent
+            ) {
+                hentBehandlingshistorikk.rerun();
+            }
+        }, [context, hentBehandlingshistorikk, behandlingErSattPåVent, prevBehandlingErSattPåVent]);
         return {
             context,
             behandlingId,
