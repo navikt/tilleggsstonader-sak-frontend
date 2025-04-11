f424d252d87a4b601bffb2b8c02a7f2551547b47
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-11 11:13:52 +0200

    Erstatter useParams med useKlagebehandling for å enklere skjønne hvilken context man er i

--- src/frontend/Sider/Klage/hooks/useHentDokumenter.ts
+++ src/frontend/Sider/Klage/hooks/useHentDokumenter.ts
@@ -1,22 +1,21 @@
 import { useEffect, useState } from 'react';
 
-import { useParams } from 'react-router-dom';
-
 import { useApp } from '../../../context/AppContext';
 import { byggTomRessurs, Ressurs, RessursStatus } from '../../../typer/ressurs';
+import { useKlagebehandling } from '../context/KlagebehandlingContext';
 import { DokumentProps } from '../familie-felles-frontend/familie-dokumentliste';
 
 export const useHentDokumenter = (): Ressurs<DokumentProps[]> => {
     const { request } = useApp();
 
-    const { behandlingId } = useParams<{ behandlingId: string }>();
+    const { behandling } = useKlagebehandling();
 
     const [dokumenter, settDokumenter] = useState<Ressurs<DokumentProps[]>>(byggTomRessurs);
 
     useEffect(() => {
         settDokumenter({ status: RessursStatus.HENTER });
-        request<DokumentProps[], null>(`/api/klage/vedlegg/${behandlingId}`).then(settDokumenter);
-    }, [behandlingId, request]);
+        request<DokumentProps[], null>(`/api/klage/vedlegg/${behandling.id}`).then(settDokumenter);
+    }, [request, behandling.id]);
 
     return dokumenter;
 };
