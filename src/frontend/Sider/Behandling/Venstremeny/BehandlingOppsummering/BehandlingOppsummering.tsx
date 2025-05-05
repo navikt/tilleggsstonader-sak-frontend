f0d1ce3661bb30228e484d537121e29c057258c4
sarahjelle <sara.hjelle@bekk.no>
2025-05-05 10:12:30 +0200

    Styling. Bakgrunn og border for å skille oppsummeringen ut i venstremeny

--- src/frontend/Sider/Behandling/Venstremeny/BehandlingOppsummering/BehandlingOppsummering.tsx
+++ src/frontend/Sider/Behandling/Venstremeny/BehandlingOppsummering/BehandlingOppsummering.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { BodyShort, ExpansionCard, VStack } from '@navikt/ds-react';
+import { ABorderDefault, AGray50 } from '@navikt/ds-tokens/dist/tokens';
 
 import { OppsummeringVilkår } from './OppsummeringVilkår';
 import { VedtakOppsummering } from './VedtakOppsummering';
@@ -12,6 +13,12 @@ import { useBehandlingOppsummering } from '../../../../hooks/useBehandlingOppsum
 import DataViewer from '../../../../komponenter/DataViewer';
 import { RessursStatus } from '../../../../typer/ressurs';
 import { formaterDato } from '../../../../utils/dato';
+
+const Container = styled.div`
+    background-color: ${AGray50};
+    padding: 1rem 0;
+    border-bottom: 1px solid ${ABorderDefault};
+`;
 
 const StyledExpansionCard = styled(ExpansionCard)`
     margin: 0.5rem;
@@ -35,33 +42,35 @@ export const BehandlingOppsummering = () => {
     return (
         <DataViewer response={{ behandlingOppsummering }}>
             {({ behandlingOppsummering }) => (
-                <StyledExpansionCard
-                    aria-label="Oppsummering av vurderinger"
-                    defaultOpen
-                    size="small"
-                >
-                    <ExpansionCard.Header>Oppsummering av vurderinger</ExpansionCard.Header>
-                    <ExpansionCard.Content>
-                        <VStack gap="6">
-                            {behandling.revurderFra && (
-                                <BodyShort size="small">
-                                    <b>Revurder fra:</b> {formaterDato(behandling.revurderFra)}
-                                </BodyShort>
-                            )}
-                            <OppsummeringAktiviteter
-                                aktiviteter={behandlingOppsummering.aktiviteter}
-                            />
-                            <OppsummeringMålgrupper
-                                målgrupper={behandlingOppsummering.målgrupper}
-                            />
-                            <OppsummeringVilkår
-                                vilkår={behandlingOppsummering.vilkår}
-                                stønadstype={behandling.stønadstype}
-                            />
-                            <VedtakOppsummering vedtak={behandlingOppsummering.vedtak} />
-                        </VStack>
-                    </ExpansionCard.Content>
-                </StyledExpansionCard>
+                <Container>
+                    <StyledExpansionCard
+                        aria-label="Oppsummering av vurderinger"
+                        defaultOpen
+                        size="small"
+                    >
+                        <ExpansionCard.Header>Oppsummering av vurderinger</ExpansionCard.Header>
+                        <ExpansionCard.Content>
+                            <VStack gap="6">
+                                {behandling.revurderFra && (
+                                    <BodyShort size="small">
+                                        <b>Revurder fra:</b> {formaterDato(behandling.revurderFra)}
+                                    </BodyShort>
+                                )}
+                                <OppsummeringAktiviteter
+                                    aktiviteter={behandlingOppsummering.aktiviteter}
+                                />
+                                <OppsummeringMålgrupper
+                                    målgrupper={behandlingOppsummering.målgrupper}
+                                />
+                                <OppsummeringVilkår
+                                    vilkår={behandlingOppsummering.vilkår}
+                                    stønadstype={behandling.stønadstype}
+                                />
+                                <VedtakOppsummering vedtak={behandlingOppsummering.vedtak} />
+                            </VStack>
+                        </ExpansionCard.Content>
+                    </StyledExpansionCard>
+                </Container>
             )}
         </DataViewer>
     );
