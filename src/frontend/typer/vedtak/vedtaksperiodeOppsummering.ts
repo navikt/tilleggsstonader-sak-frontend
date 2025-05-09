03011ae57a84221c9246babf40b8505059ee1aea
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-05-09 15:24:43 +0200

    Legger til vedtaksperioder for læremidler

--- src/frontend/typer/vedtak/vedtaksperiodeOppsummering.ts
+++ src/frontend/typer/vedtak/vedtaksperiodeOppsummering.ts
@@ -1,8 +1,10 @@
 import { FaktiskMålgruppe } from '../../Sider/Behandling/Felles/faktiskMålgruppe';
 import { AktivitetType } from '../../Sider/Behandling/Inngangsvilkår/typer/vilkårperiode/aktivitet';
+import { Studienivå } from '../../Sider/Behandling/Inngangsvilkår/typer/vilkårperiode/aktivitetLæremidler';
 
 export interface VedtakperioderOversiktResponse {
     tilsynBarn: DetaljertVedtaksperiodeTilsynBarn[];
+    læremidler: DetaljertVedtaksperiodeLæremidler[];
 }
 
 export type DetaljertVedtaksperiodeTilsynBarn = {
@@ -12,4 +14,15 @@ export type DetaljertVedtaksperiodeTilsynBarn = {
     målgruppe: FaktiskMålgruppe;
     antallBarn: number;
     totalMånedsUtgift: number;
+};
+
+export type DetaljertVedtaksperiodeLæremidler = {
+    fom: string;
+    tom: string;
+    antallMåneder: number;
+    aktivitet: AktivitetType;
+    målgruppe: FaktiskMålgruppe;
+    studienivå: Studienivå;
+    studieprosent: number;
+    månedsbeløp: number;
 };
