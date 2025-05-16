9aa5ef79f9b299617a6a06014e0880a61b369adb
Johan Blomgren <blommish@users.noreply.github.com>
2025-05-16 10:26:26 +0200

    Vis 0 hvis tallet er null når man formaterer tall

--- src/frontend/utils/fomatering.ts
+++ src/frontend/utils/fomatering.ts
@@ -6,9 +6,7 @@ export const formaterTallMedTusenSkille = (verdi?: number): string =>
     harTallverdi(verdi) ? Number(verdi).toLocaleString('no-NO', { currency: 'NOK' }) : '';
 
 export const formaterTallMedTusenSkilleEllerStrek = (verdi?: number): string =>
-    harTallverdi(verdi) && verdi !== 0
-        ? Number(verdi).toLocaleString('no-NO', { currency: 'NOK' })
-        : '-';
+    harTallverdi(verdi) ? Number(verdi).toLocaleString('no-NO', { currency: 'NOK' }) : '-';
 
 export const tilLitenSkriftMedStorForbokstav = (mottakerRolle: string): string => {
     const rolleLowerCase = mottakerRolle.toLowerCase();
