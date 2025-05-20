d890af3169c7164a02d7b0340e5843abaa75d0dd
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-05-20 08:39:31 +0200

    oversiktKort for å kunne gjenbruke tabellene på revurderFra og personoversikten

--- src/frontend/Sider/Personoversikt/Vedtaksperioderoversikt/VedtaksperioderOversiktBoutgifter.tsx
+++ src/frontend/Sider/Personoversikt/Vedtaksperioderoversikt/VedtaksperioderOversiktBoutgifter.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Table } from '@navikt/ds-react';
 
 import { DetaljertVedtaksperiodeRadBoutgifter } from './DetaljertVedtaksperiodeRadBoutgifter';
-import { OversiktKort } from './OversiktKort';
 import { DetaljertVedtaksperiodeBoutgifter } from '../../../typer/vedtak/vedtaksperiodeOppsummering';
 
 interface Props {
@@ -14,67 +13,65 @@ export const VedtaksperioderOversiktBoutgifter: React.FC<Props> = ({ vedtaksperi
     const inneholderLøpendeUtgifter = vedtaksperioder.some((periode) => periode.erLøpendeUtgift);
 
     return (
-        <OversiktKort tittel={'Boutgifter'}>
-            <Table size={'small'}>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell scope="col">Fra</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Til</Table.HeaderCell>
-                        {inneholderLøpendeUtgifter && (
-                            <Table.HeaderCell scope="col" align="center">
-                                Ant.mnd
-                            </Table.HeaderCell>
-                        )}
-                        <Table.HeaderCell scope="col">Aktivitet</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Målgruppe</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Type</Table.HeaderCell>
-                        <Table.HeaderCell scope="col" align={'right'}>
-                            Utgift
+        <Table size={'small'}>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell scope="col">Fra</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Til</Table.HeaderCell>
+                    {inneholderLøpendeUtgifter && (
+                        <Table.HeaderCell scope="col" align="center">
+                            Ant.mnd
                         </Table.HeaderCell>
-                        <Table.HeaderCell scope="col" align={'right'}>
-                            Stønad
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {vedtaksperioder.map((periode, tabellIndeks) => {
-                        if (periode.utgifterTilOvernatting) {
-                            return periode.utgifterTilOvernatting.map((utgift, utgiftIndeks) => (
-                                <DetaljertVedtaksperiodeRadBoutgifter
-                                    key={utgift.fom}
-                                    fom={utgift.fom}
-                                    tom={utgift.tom}
-                                    skalViseAntallMånederKolonne={false}
-                                    aktivitet={periode.aktivitet}
-                                    målgruppe={periode.målgruppe}
-                                    type="Overnatting"
-                                    totalUtgiftMåned={utgift.utgift}
-                                    stønadsbeløpMnd={utgift.beløpSomDekkes}
-                                    skalHaLinjeUnder={
-                                        utgiftIndeks === periode.utgifterTilOvernatting!.length - 1
-                                    }
-                                    fargetBakgrunn={tabellIndeks % 2 === 0}
-                                />
-                            ));
-                        } else {
-                            return (
-                                <DetaljertVedtaksperiodeRadBoutgifter
-                                    key={periode.fom}
-                                    fom={periode.fom}
-                                    tom={periode.tom}
-                                    antallMåneder={periode.antallMåneder}
-                                    aktivitet={periode.aktivitet}
-                                    målgruppe={periode.målgruppe}
-                                    type="Løpende"
-                                    totalUtgiftMåned={periode.totalUtgiftMåned}
-                                    stønadsbeløpMnd={periode.stønadsbeløpMnd}
-                                    fargetBakgrunn={tabellIndeks % 2 === 0}
-                                />
-                            );
-                        }
-                    })}
-                </Table.Body>
-            </Table>
-        </OversiktKort>
+                    )}
+                    <Table.HeaderCell scope="col">Aktivitet</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Målgruppe</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Type</Table.HeaderCell>
+                    <Table.HeaderCell scope="col" align={'right'}>
+                        Utgift
+                    </Table.HeaderCell>
+                    <Table.HeaderCell scope="col" align={'right'}>
+                        Stønad
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {vedtaksperioder.map((periode, tabellIndeks) => {
+                    if (periode.utgifterTilOvernatting) {
+                        return periode.utgifterTilOvernatting.map((utgift, utgiftIndeks) => (
+                            <DetaljertVedtaksperiodeRadBoutgifter
+                                key={utgift.fom}
+                                fom={utgift.fom}
+                                tom={utgift.tom}
+                                skalViseAntallMånederKolonne={false}
+                                aktivitet={periode.aktivitet}
+                                målgruppe={periode.målgruppe}
+                                type="Overnatting"
+                                totalUtgiftMåned={utgift.utgift}
+                                stønadsbeløpMnd={utgift.beløpSomDekkes}
+                                skalHaLinjeUnder={
+                                    utgiftIndeks === periode.utgifterTilOvernatting!.length - 1
+                                }
+                                fargetBakgrunn={tabellIndeks % 2 === 0}
+                            />
+                        ));
+                    } else {
+                        return (
+                            <DetaljertVedtaksperiodeRadBoutgifter
+                                key={periode.fom}
+                                fom={periode.fom}
+                                tom={periode.tom}
+                                antallMåneder={periode.antallMåneder}
+                                aktivitet={periode.aktivitet}
+                                målgruppe={periode.målgruppe}
+                                type="Løpende"
+                                totalUtgiftMåned={periode.totalUtgiftMåned}
+                                stønadsbeløpMnd={periode.stønadsbeløpMnd}
+                                fargetBakgrunn={tabellIndeks % 2 === 0}
+                            />
+                        );
+                    }
+                })}
+            </Table.Body>
+        </Table>
     );
 };
--- src/frontend/Sider/Personoversikt/Vedtaksperioderoversikt/VedtaksperioderOversiktTilsynBarn.tsx
+++ src/frontend/Sider/Personoversikt/Vedtaksperioderoversikt/VedtaksperioderOversiktTilsynBarn.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Table } from '@navikt/ds-react';
 
-import { OversiktKort } from './OversiktKort';
 import { DetaljertVedtaksperiodeTilsynBarn } from '../../../typer/vedtak/vedtaksperiodeOppsummering';
 import { formaterNullableIsoDato } from '../../../utils/dato';
 import { formaterTallMedTusenSkille } from '../../../utils/fomatering';
@@ -15,45 +14,39 @@ interface Props {
 
 export const VedtaksperioderOversiktTilsynBarn: React.FC<Props> = ({ vedtaksperioder }) => {
     return (
-        <OversiktKort tittel={'Tilsyn Barn'}>
-            <Table size={'small'}>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell scope="col">Fra</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Til</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Aktivitet</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Målgruppe</Table.HeaderCell>
-                        <Table.HeaderCell scope="col">Ant. barn</Table.HeaderCell>
-                        <Table.HeaderCell scope="col" align={'right'}>
-                            Utgifter
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {vedtaksperioder.map((periode) => {
-                        return (
-                            <Table.Row key={periode.fom}>
-                                <Table.DataCell>
-                                    {formaterNullableIsoDato(periode.fom)}
-                                </Table.DataCell>
-                                <Table.DataCell>
-                                    {formaterNullableIsoDato(periode.tom)}
-                                </Table.DataCell>
-                                <Table.DataCell>
-                                    {aktivitetTypeTilTekst(periode.aktivitet)}
-                                </Table.DataCell>
-                                <Table.DataCell>
-                                    {faktiskMålgruppeTilTekst(periode.målgruppe)}
-                                </Table.DataCell>
-                                <Table.DataCell>{periode.antallBarn}</Table.DataCell>
-                                <Table.DataCell align={'right'}>
-                                    {formaterTallMedTusenSkille(periode.totalMånedsUtgift)} kr
-                                </Table.DataCell>
-                            </Table.Row>
-                        );
-                    })}
-                </Table.Body>
-            </Table>
-        </OversiktKort>
+        <Table size={'small'}>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell scope="col">Fra</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Til</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Aktivitet</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Målgruppe</Table.HeaderCell>
+                    <Table.HeaderCell scope="col">Ant. barn</Table.HeaderCell>
+                    <Table.HeaderCell scope="col" align={'right'}>
+                        Utgifter
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {vedtaksperioder.map((periode) => {
+                    return (
+                        <Table.Row key={periode.fom}>
+                            <Table.DataCell>{formaterNullableIsoDato(periode.fom)}</Table.DataCell>
+                            <Table.DataCell>{formaterNullableIsoDato(periode.tom)}</Table.DataCell>
+                            <Table.DataCell>
+                                {aktivitetTypeTilTekst(periode.aktivitet)}
+                            </Table.DataCell>
+                            <Table.DataCell>
+                                {faktiskMålgruppeTilTekst(periode.målgruppe)}
+                            </Table.DataCell>
+                            <Table.DataCell>{periode.antallBarn}</Table.DataCell>
+                            <Table.DataCell align={'right'}>
+                                {formaterTallMedTusenSkille(periode.totalMånedsUtgift)} kr
+                            </Table.DataCell>
+                        </Table.Row>
+                    );
+                })}
+            </Table.Body>
+        </Table>
     );
 };
