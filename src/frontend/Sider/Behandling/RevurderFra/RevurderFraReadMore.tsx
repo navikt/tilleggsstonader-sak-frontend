da7865322717a256e7c0dc930448b28bb417e483
sarahjelle <sara.hjelle@bekk.no>
2025-05-22 15:37:22 +0200

    Oppdater readmore med spesifikk informasjon for hver stønadstype for å gjøre det lettere å sette datoen

--- src/frontend/Sider/Behandling/RevurderFra/RevurderFraReadMore.tsx
+++ src/frontend/Sider/Behandling/RevurderFra/RevurderFraReadMore.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 import { BodyLong, List, ReadMore } from '@navikt/ds-react';
 
-const RevurderFraReadMore = () => (
+import { useBehandling } from '../../../context/BehandlingContext';
+import { Stønadstype } from '../../../typer/behandling/behandlingTema';
+
+export const RevurderFraReadMore = () => (
     <ReadMore header={'Slik setter du revurder fra-dato'} size={'small'} defaultOpen={true}>
         <BodyLong size={'small'}>
             Revurder-fra datoen skal som hovedregel være den første datoen det har skjedd en endring
@@ -13,12 +16,31 @@ const RevurderFraReadMore = () => (
             <List.Item>oppstart på nytt tiltak</List.Item>
             <List.Item>dagen stønaden opphører</List.Item>
         </List>
-        <BodyLong size={'small'}>
-            Unntaket er hvis du skal legge til en ny utgift til pass av barn, da må du sette den 1.
-            i den aktuelle måneden, fordi utgifter til pass av barn alltid skal legges inn for hele
-            måneder
-        </BodyLong>
+        <EkstraInformasjon />
     </ReadMore>
 );
 
-export default RevurderFraReadMore;
+const EkstraInformasjon = () => {
+    const { behandling } = useBehandling();
+
+    switch (behandling.stønadstype) {
+        case Stønadstype.BARNETILSYN:
+            return (
+                <BodyLong size={'small'}>
+                    Unntaket er hvis du skal legge til en ny utgift til pass av barn, da må du sette
+                    den 1. i den aktuelle måneden, fordi utgifter til pass av barn alltid skal
+                    legges inn for hele måneder
+                </BodyLong>
+            );
+        case Stønadstype.BOUTGIFTER:
+            return (
+                <BodyLong size={'small'}>
+                    Unntaket er hvis du skal legge til en ny løpende utgift til bolig, da må du
+                    sette den 1. i den aktuelle måneden, fordi løpende utgifter til bolig alltid
+                    skal legges inn for hele måneder
+                </BodyLong>
+            );
+        default:
+            return null;
+    }
+};
