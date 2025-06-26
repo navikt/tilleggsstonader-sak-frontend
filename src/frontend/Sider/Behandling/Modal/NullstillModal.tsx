af89004d7a6b45e6b7db8bae2f5f6e4957e7ed85
Andreas Strand <andreas.strand@nav.no>
2025-06-26 11:04:58 +0200

    Eksplisitt naviger til inngangsvilkår-fane ved nullstilling av behandling

--- src/frontend/Sider/Behandling/Modal/NullstillModal.tsx
+++ src/frontend/Sider/Behandling/Modal/NullstillModal.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/BehandlingContext';
 import { Feilmelding } from '../../../komponenter/Feil/Feilmelding';
 import { Feil, feiletRessursTilFeilmelding } from '../../../komponenter/Feil/feilmeldingUtils';
 import { ModalWrapper } from '../../../komponenter/Modal/ModalWrapper';
 import { RessursStatus } from '../../../typer/ressurs';
+import { FanePath } from '../faner';
 
 const NullstillModal: React.FC = () => {
     const { request } = useApp();
@@ -15,6 +18,7 @@ const NullstillModal: React.FC = () => {
     const [laster, settLaster] = useState(false);
     const [feilmelding, settFeilmelding] = useState<Feil>();
 
+    const navigate = useNavigate();
     const lukkModal = () => {
         settFeilmelding(undefined);
         settVisNullstillModal(false);
@@ -29,6 +33,7 @@ const NullstillModal: React.FC = () => {
             .then((respons) => {
                 if (respons.status === RessursStatus.SUKSESS) {
                     lukkModal();
+                    navigate(`/behandling/${behandling.id}/${FanePath.INNGANGSVILKÅR}`);
                     // TODO bør kunne løses bedre ved å kun refreshe state
                     window.location.reload();
                 } else {
@@ -54,7 +59,7 @@ const NullstillModal: React.FC = () => {
                 lukkKnapp: { onClick: () => lukkModal(), tekst: 'Avbryt' },
             }}
         >
-            Nullstiller du behandlingen vil alle endringer blit fjernet.
+            Nullstiller du behandlingen vil alle endringer bli fjernet.
             <Feilmelding feil={feilmelding} />
         </ModalWrapper>
     );
