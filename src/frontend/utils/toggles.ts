61bafd4d1575f9f2ba880a1b60833e45a4891340
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-06-27 08:26:13 +0200

    Fjerner bryter for vedtaksperioder

--- src/frontend/utils/toggles.ts
+++ src/frontend/utils/toggles.ts
@@ -21,7 +21,6 @@ export enum Toggle {
      * features-under-utvikling
      */
     TILLATER_NULLVEDAK = `sak.tillater_nullvedtak`,
-    SKAL_VISE_VEDTAKSPERIODER_TAB = `sak.frontend.skal-vise-vedtaksperiode-tab`,
     BOUTGIFTER_TILLAT_HOYERE_UTGIFTER = `sak.boutgifter-tillat-hoyere-utgifter`,
     SKAL_UTLEDE_ENDRINGSDATO_AUTOMATISK = `sak.utled-endringsdato-revurdering`,
     KAN_NULLSTILLE_BEHANDLING = `sak.frontend.kan-nullstille-behandling`,
