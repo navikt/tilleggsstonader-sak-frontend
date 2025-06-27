94de9ce2cf3c81001932e7d4998c61fafcb10841
Sigmund Berbom <sberbom@gmail.com>
2025-06-27 11:02:42 +0200

    💡Legg til TODO på at vi må se over vedtaks-url for daglig reise

--- src/frontend/Sider/Behandling/VedtakOgBeregning/Felles/stønadstypeTilVedtakUrl.ts
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/Felles/stønadstypeTilVedtakUrl.ts
@@ -4,6 +4,7 @@ export const stønadstypeTilVedtakUrl: Record<Stønadstype, string> = {
     [Stønadstype.BARNETILSYN]: 'tilsyn-barn',
     [Stønadstype.LÆREMIDLER]: 'laremidler',
     [Stønadstype.BOUTGIFTER]: 'boutgifter',
-    [Stønadstype.DAGLIG_REISE_TSR]: 'dadlid-reise-tiltaksenheten',
+    //TODO Sjekk at vi ønsker ulike URLer
+    [Stønadstype.DAGLIG_REISE_TSR]: 'daglig-reise-tiltaksenheten',
     [Stønadstype.DAGLIG_REISE_TSO]: 'daglig-reise-nay',
 };
