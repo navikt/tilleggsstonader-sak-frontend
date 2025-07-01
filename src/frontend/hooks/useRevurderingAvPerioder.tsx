931525757c9cdf1944afa3c5cc4e5b1abdce8462
Andreas Strand <andreas.strand@nav.no>
2025-07-01 12:41:43 +0200

    Lov å endre datoer på vilkår og vilkårsperioder i revurdering om feature-toggle for automatisk utleding av endringsdato er på

--- src/frontend/hooks/useRevurderingAvPerioder.tsx
+++ src/frontend/hooks/useRevurderingAvPerioder.tsx
@@ -1,5 +1,8 @@
+import { useFlag } from '@unleash/proxy-client-react';
+
 import { useBehandling } from '../context/BehandlingContext';
 import { dagenFør, datoErIPeriodeInklusivSlutt, erDatoEtterEllerLik, erFør } from '../utils/dato';
+import { Toggle } from '../utils/toggles';
 
 type FelterSomKanEndresIPerioden = 'INGEN' | 'BARE_TOM' | 'ALLE';
 
@@ -28,13 +31,14 @@ export const useRevurderingAvPerioder = ({
     kanSlettePeriode: boolean;
 } => {
     const { behandling } = useBehandling();
+    const skalUtledeEndringsdatoAutomatisk = useFlag(Toggle.SKAL_UTLEDE_ENDRINGSDATO_AUTOMATISK);
     const datoDetRevurderesFra = behandling.revurderFra;
 
     const noenAvDatoeneErUdefinert = !(datoDetRevurderesFra && periodeTom && periodeFom);
 
     // TODO: Gi status bare tom hvis dato er en dag før revurder fra også!
     const bestemFelterSomKanEndres = (): FelterSomKanEndresIPerioden => {
-        if (nyRadLeggesTil || noenAvDatoeneErUdefinert) {
+        if (nyRadLeggesTil || noenAvDatoeneErUdefinert || skalUtledeEndringsdatoAutomatisk) {
             return 'ALLE';
         }
 
