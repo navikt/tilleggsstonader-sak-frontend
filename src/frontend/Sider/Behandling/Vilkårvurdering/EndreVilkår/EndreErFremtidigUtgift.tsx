b67944cb9ea8325b2a41870f1721cefbfa9d0fd0
Sigmund Berbom <sberbom@gmail.com>
2025-08-18 15:53:18 +0200

    🚩 Fjern fremtidig utgift toggle. Dette tillates alltid

--- src/frontend/Sider/Behandling/Vilkårvurdering/EndreVilkår/EndreErFremtidigUtgift.tsx
+++ src/frontend/Sider/Behandling/Vilkårvurdering/EndreVilkår/EndreErFremtidigUtgift.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { useFlag } from '@unleash/proxy-client-react';
 import styled from 'styled-components';
 
 import { Switch } from '@navikt/ds-react';
 
-import { Toggle } from '../../../../utils/toggles';
 import { StønadsvilkårType } from '../../vilkår';
 
 const StyledSwitch = styled(Switch)`
@@ -18,9 +16,6 @@ const EndreErFremtidigUtgift: React.FC<{
     erFremtidigUtgift?: boolean;
     oppdaterErFremtidigUtgift: (verdi: boolean) => void;
 }> = ({ vilkårtype, kanVæreFremtidigUtgift, erFremtidigUtgift, oppdaterErFremtidigUtgift }) => {
-    const tillaterErFremtidigUtgift = useFlag(Toggle.TILLATER_NULLVEDAK);
-    if (!tillaterErFremtidigUtgift) return null;
-
     return (
         <>
             {vilkårtype === StønadsvilkårType.UTGIFTER_OVERNATTING && kanVæreFremtidigUtgift && (
