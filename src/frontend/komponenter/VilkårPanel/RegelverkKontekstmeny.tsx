e07effe1f80822fd0a2b26a65fd939963fa2a9f0
Runar Skagestad <runar.skagestad@nav.no>
2025-08-25 16:31:18 +0200

    ♻️ Lag egen komponent av RegelverkKontekstmeny

---
+++ src/frontend/komponenter/VilkårPanel/RegelverkKontekstmeny.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { GavelSoundBlockIcon } from '@navikt/aksel-icons';
+import { ActionMenu, Button } from '@navikt/ds-react';
+
+import { Lenke } from '../../Sider/Behandling/lenker';
+
+export function RegelverkKontekstmeny({
+    paragraflenker,
+    forskriftlenker,
+    rundskrivlenke,
+}: {
+    paragraflenker: Lenke[];
+    forskriftlenker: Lenke[];
+    rundskrivlenke: Lenke[];
+}) {
+    return (
+        <ActionMenu>
+            <ActionMenu.Trigger>
+                <Button variant={'tertiary'} size={'small'} icon={<GavelSoundBlockIcon />}>
+                    Regelverk
+                </Button>
+            </ActionMenu.Trigger>
+            <ActionMenu.Content>
+                <Gruppe tittel={'Lovbestemmelser'} lenker={paragraflenker} />
+                <Gruppe tittel={'Forskrifter'} lenker={forskriftlenker} />
+                <Gruppe tittel={'Rundskriv'} lenker={rundskrivlenke} />
+            </ActionMenu.Content>
+        </ActionMenu>
+    );
+}
+
+function Gruppe({ tittel, lenker }: { tittel: string; lenker: Lenke[] }) {
+    if (lenker.length === 0) {
+        return null;
+    }
+    return (
+        <>
+            <ActionMenu.Group label={tittel}></ActionMenu.Group>
+            {lenker.map((lenke, indeks) => (
+                <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
+                    {lenke.tekst}
+                </ActionMenu.Item>
+            ))}
+        </>
+    );
+}
--- src/frontend/komponenter/VilkårPanel/VilkårPanel.tsx
+++ src/frontend/komponenter/VilkårPanel/VilkårPanel.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import { GavelSoundBlockIcon } from '@navikt/aksel-icons';
-import { ActionMenu, Button } from '@navikt/ds-react';
-
+import { RegelverkKontekstmeny } from './RegelverkKontekstmeny';
 import { Lenke } from '../../Sider/Behandling/lenker';
 import Panel from '../Panel/Panel';
 
@@ -31,37 +29,11 @@ export const VilkårPanel: React.FC<VilkårpanelProps> = ({
             ikon={ikon}
             ekstraHeading={ekstraHeading}
             kontekstmeny={
-                <ActionMenu>
-                    <ActionMenu.Trigger>
-                        <Button
-                            variant={'tertiary-neutral'}
-                            size={'small'}
-                            icon={<GavelSoundBlockIcon />}
-                        >
-                            Regelverk
-                        </Button>
-                    </ActionMenu.Trigger>
-                    <ActionMenu.Content>
-                        <ActionMenu.Group label="Lovbestemmelser"></ActionMenu.Group>
-                        {paragraflenker.map((lenke, indeks) => (
-                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
-                                {lenke.tekst}
-                            </ActionMenu.Item>
-                        ))}
-                        <ActionMenu.Group label="Forskrifter"></ActionMenu.Group>
-                        {forskriftlenker.map((lenke, indeks) => (
-                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
-                                {lenke.tekst}
-                            </ActionMenu.Item>
-                        ))}
-                        <ActionMenu.Group label="Rundskriv"></ActionMenu.Group>
-                        {rundskrivlenke.map((lenke, indeks) => (
-                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
-                                {lenke.tekst}
-                            </ActionMenu.Item>
-                        ))}
-                    </ActionMenu.Content>
-                </ActionMenu>
+                <RegelverkKontekstmeny
+                    paragraflenker={paragraflenker}
+                    rundskrivlenke={forskriftlenker}
+                    forskriftlenker={rundskrivlenke}
+                />
             }
         >
             {children}
