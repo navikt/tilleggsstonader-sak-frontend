741101107179c2f45fb46964e9214c23507edeb0
Runar Skagestad <runar.skagestad@nav.no>
2025-08-25 16:14:55 +0200

    💄 Bytt fra hamburgermeny til mer beskrivende Regelverk-knapp

--- src/frontend/komponenter/VilkårPanel/VilkårPanel.tsx
+++ src/frontend/komponenter/VilkårPanel/VilkårPanel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { MenuHamburgerIcon } from '@navikt/aksel-icons';
+import { GavelSoundBlockIcon } from '@navikt/aksel-icons';
 import { ActionMenu, Button } from '@navikt/ds-react';
 
 import { Lenke } from '../../Sider/Behandling/lenker';
@@ -29,54 +29,39 @@ export const VilkårPanel: React.FC<VilkårpanelProps> = ({
         <Panel
             tittel={tittel}
             ikon={ikon}
-            ekstraHeading={
-                <>
-                    {ekstraHeading}
-                    <ActionMenu>
-                        <ActionMenu.Trigger>
-                            <Button
-                                variant={'tertiary'}
-                                size={'small'}
-                                icon={<MenuHamburgerIcon />}
-                            />
-                        </ActionMenu.Trigger>
-                        <ActionMenu.Content>
-                            <ActionMenu.Group label="Relevante lovbestemmelser"></ActionMenu.Group>
-                            {paragraflenker.map((lenke, indeks) => (
-                                <ActionMenu.Item
-                                    as={'a'}
-                                    key={indeks}
-                                    href={lenke.url}
-                                    target="_blank"
-                                >
-                                    {lenke.tekst}
-                                </ActionMenu.Item>
-                            ))}
-                            <ActionMenu.Group label="Forskrifter"></ActionMenu.Group>
-                            {forskriftlenker.map((lenke, indeks) => (
-                                <ActionMenu.Item
-                                    as={'a'}
-                                    key={indeks}
-                                    href={lenke.url}
-                                    target="_blank"
-                                >
-                                    {lenke.tekst}
-                                </ActionMenu.Item>
-                            ))}
-                            <ActionMenu.Group label="Rundskriv"></ActionMenu.Group>
-                            {rundskrivlenke.map((lenke, indeks) => (
-                                <ActionMenu.Item
-                                    as={'a'}
-                                    key={indeks}
-                                    href={lenke.url}
-                                    target="_blank"
-                                >
-                                    {lenke.tekst}
-                                </ActionMenu.Item>
-                            ))}
-                        </ActionMenu.Content>
-                    </ActionMenu>
-                </>
+            ekstraHeading={ekstraHeading}
+            kontekstmeny={
+                <ActionMenu>
+                    <ActionMenu.Trigger>
+                        <Button
+                            variant={'tertiary-neutral'}
+                            size={'small'}
+                            icon={<GavelSoundBlockIcon />}
+                        >
+                            Regelverk
+                        </Button>
+                    </ActionMenu.Trigger>
+                    <ActionMenu.Content>
+                        <ActionMenu.Group label="Lovbestemmelser"></ActionMenu.Group>
+                        {paragraflenker.map((lenke, indeks) => (
+                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
+                                {lenke.tekst}
+                            </ActionMenu.Item>
+                        ))}
+                        <ActionMenu.Group label="Forskrifter"></ActionMenu.Group>
+                        {forskriftlenker.map((lenke, indeks) => (
+                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
+                                {lenke.tekst}
+                            </ActionMenu.Item>
+                        ))}
+                        <ActionMenu.Group label="Rundskriv"></ActionMenu.Group>
+                        {rundskrivlenke.map((lenke, indeks) => (
+                            <ActionMenu.Item as={'a'} key={indeks} href={lenke.url} target="_blank">
+                                {lenke.tekst}
+                            </ActionMenu.Item>
+                        ))}
+                    </ActionMenu.Content>
+                </ActionMenu>
             }
         >
             {children}
