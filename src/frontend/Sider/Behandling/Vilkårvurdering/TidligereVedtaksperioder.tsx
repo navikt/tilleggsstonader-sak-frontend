4fea80ae030a177c7b02fdca8738b9fbfb8a9871
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-10-15 14:43:39 +0200

    Tar tilbake dataViewer og early return på VarselVedtakIArena

--- src/frontend/Sider/Behandling/Vilkårvurdering/TidligereVedtaksperioder.tsx
+++ src/frontend/Sider/Behandling/Vilkårvurdering/TidligereVedtaksperioder.tsx
@@ -4,6 +4,7 @@ import { ExpansionCard, Heading } from '@navikt/ds-react';
 import { ALimegreen50 } from '@navikt/ds-tokens/dist/tokens';
 
 import { useHentFullstendigVedtaksOversiktForStønad } from '../../../hooks/useHentFullstendigVedtaksOversikt';
+import DataViewer from '../../../komponenter/DataViewer';
 import { BehandlingFakta } from '../../../typer/behandling/behandlingFakta/behandlingFakta';
 import { Stønadstype } from '../../../typer/behandling/behandlingTema';
 import { formaterDato } from '../../../utils/dato';
@@ -30,44 +31,52 @@ export function TidligereVedtaksperioder({
         return null;
     }
 
-    const vedtaksperioder =
-        vedtaksperioderOversiktForStønad.status === 'SUKSESS'
-            ? vedtaksperioderOversiktForStønad.data
-            : undefined;
+    if (arenaVedtakTom && !forrigeIverksatteBehandlingId) {
+        return <VarselVedtakIArena arenaVedtakTom={arenaVedtakTom} />;
+    }
 
     return (
-        <>
-            {vedtaksperioder && vedtaksperioder.length > 0 && (
-                <ExpansionCard
-                    style={{ backgroundColor: ALimegreen50 }}
-                    size={'small'}
-                    aria-labelledby={'Vedtaksperioder for forrige behandling'}
-                >
-                    <ExpansionCard.Header>
-                        <ExpansionCard.Title>
-                            <Heading size={'xsmall'} as="span">
-                                Søker har vedtak i TS-sak til og med{' '}
-                                {formaterDato(vedtaksperioder[vedtaksperioder.length - 1].tom)}
-                            </Heading>
-                        </ExpansionCard.Title>
-                        {arenaVedtakTom && (
-                            <ExpansionCard.Description>
-                                Det finnes også et vedtak i Arena til og med{' '}
-                                {formaterDato(arenaVedtakTom)}
-                            </ExpansionCard.Description>
+        <DataViewer type={'vedtaksperioder'} response={{ vedtaksperioderOversiktForStønad }}>
+            {({ vedtaksperioderOversiktForStønad }) => {
+                return (
+                    <>
+                        {vedtaksperioderOversiktForStønad.length > 0 && (
+                            <ExpansionCard
+                                style={{ backgroundColor: ALimegreen50 }}
+                                size={'small'}
+                                aria-labelledby={'Vedtaksperioder for forrige behandling'}
+                            >
+                                <ExpansionCard.Header>
+                                    <ExpansionCard.Title>
+                                        <Heading size={'xsmall'} as="span">
+                                            Søker har vedtak i TS-sak til og med{' '}
+                                            {formaterDato(
+                                                vedtaksperioderOversiktForStønad[
+                                                    vedtaksperioderOversiktForStønad.length - 1
+                                                ].tom
+                                            )}
+                                        </Heading>
+                                    </ExpansionCard.Title>
+                                    {arenaVedtakTom && (
+                                        <ExpansionCard.Description>
+                                            Det finnes også et vedtak i Arena til og med{' '}
+                                            {formaterDato(arenaVedtakTom)}
+                                        </ExpansionCard.Description>
+                                    )}
+                                </ExpansionCard.Header>
+                                <ExpansionCard.Content>
+                                    <DetaljerteVedtaksperioderBehandling
+                                        stønadstype={stønadstype}
+                                        vedtaksperioderOversiktForStønad={
+                                            vedtaksperioderOversiktForStønad
+                                        }
+                                    />
+                                </ExpansionCard.Content>
+                            </ExpansionCard>
                         )}
-                    </ExpansionCard.Header>
-                    <ExpansionCard.Content>
-                        <DetaljerteVedtaksperioderBehandling
-                            stønadstype={stønadstype}
-                            vedtaksperioderOversiktForStønad={vedtaksperioder}
-                        />
-                    </ExpansionCard.Content>
-                </ExpansionCard>
-            )}
-            {arenaVedtakTom && !vedtaksperioder?.length && (
-                <VarselVedtakIArena arenaVedtakTom={arenaVedtakTom} />
-            )}
-        </>
+                    </>
+                );
+            }}
+        </DataViewer>
     );
 }
