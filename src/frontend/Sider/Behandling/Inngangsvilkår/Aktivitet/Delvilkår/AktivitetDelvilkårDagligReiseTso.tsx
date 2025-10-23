e3784b25b13bcccb15596c7ab678f5784e562adc
Adnan Vrevic <adnan.vrevic@nav.no>
2025-10-23 13:47:01 +0200

    Legg til spørsmål om utgifter for daglig reise

--- src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/Delvilkår/AktivitetDelvilkårDagligReiseTso.tsx
+++ src/frontend/Sider/Behandling/Inngangsvilkår/Aktivitet/Delvilkår/AktivitetDelvilkårDagligReiseTso.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 
-import styled from 'styled-components';
+import { VStack } from '@navikt/ds-react';
 
 import { JaNeiVurdering } from '../../../Vilkårvurdering/JaNeiVurdering';
 import { SvarJaNei } from '../../typer/vilkårperiode/vilkårperiode';
 import { EndreAktivitetFormDagligReiseTso } from '../EndreAktivitetDagligReiseTso';
 import { skalVurdereLønnet } from '../utilsDagligReiseTso';
-
-const Container = styled.div`
-    display: flex;
-    gap: 2rem;
-`;
+import { HarBrukerUtgifterTilDagligReise } from './HarBrukerUtgifterTilDagligReise';
 
 export const AktivitetDelvilkårDagligReiseTso: React.FC<{
     aktivitetForm: EndreAktivitetFormDagligReiseTso;
     oppdaterLønnet: (svar: SvarJaNei) => void;
-}> = ({ aktivitetForm, oppdaterLønnet }) => {
+    oppdaterHarUtgifter: (svar: SvarJaNei) => void;
+}> = ({ aktivitetForm, oppdaterLønnet, oppdaterHarUtgifter }) => {
     if (aktivitetForm.type === '') return null;
 
     if (!skalVurdereLønnet(aktivitetForm.type)) return null;
 
     return (
-        <Container>
+        <VStack gap={'2'}>
             <JaNeiVurdering
                 label="Mottar bruker ordinær lønn i tiltaket?"
                 svar={aktivitetForm.svarLønnet}
                 oppdaterSvar={oppdaterLønnet}
             />
-        </Container>
+            <HarBrukerUtgifterTilDagligReise
+                aktivitetForm={aktivitetForm}
+                oppdaterSvar={oppdaterHarUtgifter}
+            />
+        </VStack>
     );
 };
