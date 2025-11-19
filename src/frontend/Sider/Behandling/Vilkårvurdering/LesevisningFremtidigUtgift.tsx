805ebb2e24f8d3edb0e671b2f2d83fc9ed252fbf
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-11-19 14:40:39 +0100

    Fjernet duplikat av Redigeringsknapp fra LesevisningFremtidigUtgift siden den allerede ligger i LesevisningVilkår.tsx

--- src/frontend/Sider/Behandling/Vilkårvurdering/LesevisningFremtidigUtgift.tsx
+++ src/frontend/Sider/Behandling/Vilkårvurdering/LesevisningFremtidigUtgift.tsx
@@ -1,26 +1,16 @@
 import React, { FC } from 'react';
 
-import { styled } from 'styled-components';
-
-import { PencilIcon } from '@navikt/aksel-icons';
 import { BodyShort, HGrid, HStack, Label } from '@navikt/ds-react';
 
-import SmallButton from '../../../komponenter/Knapper/SmallButton';
 import { formaterNullablePeriode } from '../../../utils/dato';
 import { formaterTallMedTusenSkilleEllerStrek } from '../../../utils/fomatering';
 import { Vilkår } from '../vilkår';
-
-const Redigeringsknapp = styled(SmallButton)`
-    max-height: 24px;
-    align-self: end;
-    margin-left: auto;
-`;
 
 const LesevisningFremtidigUtgift: FC<{
     vilkår: Vilkår;
     skalViseRedigeringsknapp?: boolean;
     startRedigering?: () => void;
-}> = ({ vilkår, startRedigering, skalViseRedigeringsknapp }) => {
+}> = ({ vilkår }) => {
     const { fom, tom, utgift } = vilkår;
 
     return (
@@ -38,13 +28,6 @@ const LesevisningFremtidigUtgift: FC<{
                     </BodyShort>
                 )}
             </HStack>
-            {skalViseRedigeringsknapp && (
-                <Redigeringsknapp
-                    variant="tertiary"
-                    onClick={startRedigering}
-                    icon={<PencilIcon />}
-                />
-            )}
         </HGrid>
     );
 };
