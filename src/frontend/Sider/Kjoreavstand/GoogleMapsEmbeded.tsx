b2974221501f1732cc2db172d248d83b0e90d098
Sigmund Berbom <sberbom@gmail.com>
2025-11-19 10:50:26 +0100

    🔧 Legg til nøkkel for Embeded maps. Nøkkelen er sikret ved at den kun kan brukes fra Nav domener og tjenesten er gratis

--- src/frontend/Sider/Kjoreavstand/GoogleMapsEmbeded.tsx
+++ src/frontend/Sider/Kjoreavstand/GoogleMapsEmbeded.tsx
@@ -25,7 +25,8 @@ export const GoogleMapsEmbededIFrame: React.FC<Props> = ({
             style={{ border: 0 }}
             loading="lazy"
             allowFullScreen
-            src={`https://www.google.com/maps/embed/v1/directions?origin=${fra},${fraPostkode},${fraPoststed}&destination=${til},${tilPostkode},${tilPoststed}}&mode=transit&mode=walking&mode=driving&key=`}
+            // Nøkkelen ligger i klart tekst da den kun brukes til å identifisere ts-sak på en gratis tjeneste. Nøkkelen er sikret ved at den kun kan brukes fra Nav domener
+            src={`https://www.google.com/maps/embed/v1/directions?origin=${fra},${fraPostkode},${fraPoststed}&destination=${til},${tilPostkode},${tilPoststed}&mode=transit&mode=walking&mode=driving&key=AIzaSyAZO0_xQzIjG6UK0B-kETHv-hV3UqTX9j8`}
         />
     );
 };
