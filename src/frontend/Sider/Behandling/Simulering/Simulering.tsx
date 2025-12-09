5f4cb97e6631b5dc2356dbabaf6c4ca3def74675
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-12-09 14:49:57 +0100

    Flytter nesteknappen inn til simuleringspanelet slik at den dukker opp når ferdig lastet

--- src/frontend/Sider/Behandling/Simulering/Simulering.tsx
+++ src/frontend/Sider/Behandling/Simulering/Simulering.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { Button } from '@navikt/ds-react';
-
 import SimuleringResultatWrapper from './SimuleringResultatWrapper';
-import { useBehandling } from '../../../context/BehandlingContext';
-import { useNavigateUtenSjekkForUlagredeKomponenter } from '../../../hooks/useNavigateUtenSjekkForUlagredeKomponenter';
 import { useVedtak } from '../../../hooks/useVedtak';
 import DataViewer from '../../../komponenter/DataViewer';
-import { BehandlingResultat } from '../../../typer/behandling/behandlingResultat';
-import { BehandlingStatus } from '../../../typer/behandling/behandlingStatus';
-import { FanePath } from '../faner';
 
 const Container = styled.div`
     display: flex;
@@ -22,31 +15,13 @@ const Container = styled.div`
 `;
 
 const Simulering: React.FC = () => {
-    const navigate = useNavigateUtenSjekkForUlagredeKomponenter();
     const { vedtak } = useVedtak();
-    const { behandling } = useBehandling();
-
-    const gåTilNesteSteg = () => {
-        navigate(`/behandling/${behandling.id}/${FanePath.BREV}`);
-    };
 
     return (
         <Container>
             <DataViewer type={'vedtak'} response={{ vedtak }}>
                 {({ vedtak }) => <SimuleringResultatWrapper vedtak={vedtak} />}
             </DataViewer>
-            {behandling.resultat === BehandlingResultat.IKKE_SATT &&
-                behandling.status !== BehandlingStatus.FATTER_VEDTAK && (
-                    <Button
-                        variant="primary"
-                        size="small"
-                        onClick={() => {
-                            gåTilNesteSteg();
-                        }}
-                    >
-                        Neste
-                    </Button>
-                )}
         </Container>
     );
 };
--- src/frontend/Sider/Behandling/Simulering/SimuleringResultatWrapper.tsx
+++ src/frontend/Sider/Behandling/Simulering/SimuleringResultatWrapper.tsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
-import { Alert, Heading } from '@navikt/ds-react';
+import { Alert, Button, Heading } from '@navikt/ds-react';
 
 import Oppsumering from './Oppsumering';
 import SimuleringTabell from './SimuleringTabell';
 import { SimuleringResponse } from './simuleringTyper';
 import { useApp } from '../../../context/AppContext';
 import { useBehandling } from '../../../context/BehandlingContext';
+import { useNavigateUtenSjekkForUlagredeKomponenter } from '../../../hooks/useNavigateUtenSjekkForUlagredeKomponenter';
 import DataViewer from '../../../komponenter/DataViewer';
 import Panel from '../../../komponenter/Panel/Panel';
-import { byggHenterRessurs, byggTomRessurs, Ressurs } from '../../../typer/ressurs';
+import { BehandlingResultat } from '../../../typer/behandling/behandlingResultat';
+import { BehandlingStatus } from '../../../typer/behandling/behandlingStatus';
+import {
+    byggHenterRessurs,
+    byggTomRessurs,
+    harNoenRessursMedStatus,
+    Ressurs,
+    RessursStatus,
+} from '../../../typer/ressurs';
 import { VedtakResponse } from '../../../typer/vedtak/vedtak';
 import { formaterÅrFullMåned } from '../../../utils/dato';
+import { FanePath } from '../faner';
 
 const SimuleringResultatWrapper: React.FC<{ vedtak: VedtakResponse }> = ({ vedtak }) => {
     const { behandling, hentBehandling } = useBehandling();
     const { request } = useApp();
 
-    const [simuleringsresultat, settSimuleringsresultat] =
+    const [simuleringsresultatState, settSimuleringsresultat] =
         useState<Ressurs<SimuleringResponse | null>>(byggTomRessurs());
 
     useEffect(() => {
@@ -28,6 +38,12 @@ const SimuleringResultatWrapper: React.FC<{ vedtak: VedtakResponse }> = ({ vedta
                 hentBehandling.rerun(); // Må hente behandling på nytt for å oppdatere behandling med ritkig steg
             });
     }, [request, settSimuleringsresultat, behandling.id, hentBehandling]);
+
+    const navigate = useNavigateUtenSjekkForUlagredeKomponenter();
+
+    const gåTilNesteSteg = () => {
+        navigate(`/behandling/${behandling.id}/${FanePath.BREV}`);
+    };
 
     const utledBeskrivelseIngenSimulering = (simuleringsresultat: SimuleringResponse | null) => {
         if (vedtak.type === 'AVSLAG') {
@@ -40,28 +56,46 @@ const SimuleringResultatWrapper: React.FC<{ vedtak: VedtakResponse }> = ({ vedta
     };
 
     return (
-        <DataViewer type={'simuleringsresultat'} response={{ simuleringsresultat }}>
-            {({ simuleringsresultat }) => {
+        <DataViewer type={'simuleringsresultat'} response={{ simuleringsresultatState }}>
+            {({ simuleringsresultatState: simuleringsresultat }) => {
                 const { perioder, oppsummering } = simuleringsresultat || {};
 
                 return perioder && oppsummering ? (
-                    <Panel
-                        tittel={`Simulering for perioden ${formaterÅrFullMåned(oppsummering.fom)} - ${formaterÅrFullMåned(oppsummering.tom)}`}
-                    >
-                        <Oppsumering oppsummering={oppsummering} />
-                        <SimuleringTabell perioder={perioder} />
-                        {oppsummering.feilutbetaling > 0 && (
-                            <Alert variant={'info'}>
-                                <Heading spacing size="small" level="3">
-                                    Feilutbetaling
-                                </Heading>
-                                Behandlingen har resultert i en feilutbetaling. Det er foreløpig
-                                ikke systemstøtte for å opprette en tilbakekrevingsbehandling. Du
-                                kan behandle ferdig denne revurderingen. Det vil komme nærmere
-                                informasjon om hvordan feilutbetalinger skal behandles.
-                            </Alert>
-                        )}
-                    </Panel>
+                    <>
+                        <Panel
+                            tittel={`Simulering for perioden ${formaterÅrFullMåned(oppsummering.fom)} - ${formaterÅrFullMåned(oppsummering.tom)}`}
+                        >
+                            <Oppsumering oppsummering={oppsummering} />
+                            <SimuleringTabell perioder={perioder} />
+                            {oppsummering.feilutbetaling > 0 && (
+                                <Alert variant={'info'}>
+                                    <Heading spacing size="small" level="3">
+                                        Feilutbetaling
+                                    </Heading>
+                                    Behandlingen har resultert i en feilutbetaling. Det er foreløpig
+                                    ikke systemstøtte for å opprette en tilbakekrevingsbehandling.
+                                    Du kan behandle ferdig denne revurderingen. Det vil komme
+                                    nærmere informasjon om hvordan feilutbetalinger skal behandles.
+                                </Alert>
+                            )}
+                        </Panel>
+                        {behandling.resultat === BehandlingResultat.IKKE_SATT &&
+                            behandling.status !== BehandlingStatus.FATTER_VEDTAK && (
+                                <Button
+                                    variant="primary"
+                                    size="small"
+                                    loading={harNoenRessursMedStatus(
+                                        [simuleringsresultatState],
+                                        RessursStatus.HENTER
+                                    )}
+                                    onClick={() => {
+                                        gåTilNesteSteg();
+                                    }}
+                                >
+                                    Neste
+                                </Button>
+                            )}
+                    </>
                 ) : (
                     <Alert variant={'info'} inline>
                         {utledBeskrivelseIngenSimulering(simuleringsresultat)}
