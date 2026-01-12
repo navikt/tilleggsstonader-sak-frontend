7fc6101b9b169c7b9753db7af8c12d81aede4a12
sarahjelle <sara.hjelle@bekk.no>
2026-01-12 16:28:54 +0100

    Toggle nye faner så de ikke vises før ny flyt er klar

--- src/frontend/Sider/Behandling/VedtakOgBeregning/DagligReise/innvilgeRammevedtak/KjørelisteDagligReise.tsx
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/DagligReise/innvilgeRammevedtak/KjørelisteDagligReise.tsx
@@ -1,47 +1,15 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 
-import { useApp } from '../../../../../context/AppContext';
-import { useBehandling } from '../../../../../context/BehandlingContext';
-import DataViewer from '../../../../../komponenter/DataViewer';
-import Panel from '../../../../../komponenter/Panel/Panel';
+import { BodyShort } from '@navikt/ds-react';
+
 import { StegKnapp } from '../../../../../komponenter/Stegflyt/StegKnapp';
 import { Steg } from '../../../../../typer/behandling/steg';
-import { byggHenterRessurs, byggTomRessurs } from '../../../../../typer/ressurs';
-import { BeregningsresultatPrivatBil } from '../../../../../typer/vedtak/vedtakDagligReise';
 import { FanePath } from '../../../faner';
-import { RammevedtakPrivatBil } from '../innvilgeVedtak/RammevedtakPrivatBil';
-import styles from '../VedtakOgBeregningDagligReise.module.css';
 
 export const KjørelisteDagligReise: FC = () => {
-    const { request } = useApp();
-    const { behandling } = useBehandling();
-
-    const [beregningsresultat, settBeregningsresultat] =
-        useState(byggTomRessurs<BeregningsresultatPrivatBil>());
-
-    const hentberegningsresultat = () => {
-        settBeregningsresultat(byggHenterRessurs());
-        request<BeregningsresultatPrivatBil, BeregningsresultatPrivatBil>(
-            `/api/sak/vedtak/daglig-reise/${behandling.id}/rammevedtak`,
-            'GET'
-        ).then((result) => settBeregningsresultat(result));
-    };
-
-    useEffect(() => {
-        hentberegningsresultat();
-    });
-
     return (
         <>
-            <DataViewer type={'beregningsresultat'} response={{ beregningsresultat }}>
-                {({ beregningsresultat }) => (
-                    <div className={styles.container}>
-                        <Panel tittel="Rammevedtak">
-                            <RammevedtakPrivatBil beregningsresultat={beregningsresultat} />
-                        </Panel>
-                    </div>
-                )}
-            </DataViewer>
+            <BodyShort size="small">Her kommer det informasjon om kjøring med privat bil</BodyShort>
             <StegKnapp
                 nesteFane={FanePath.BREV}
                 steg={Steg.KJØRELISTE}
