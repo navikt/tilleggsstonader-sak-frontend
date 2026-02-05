4d4393087de3e042838e02610aa53e81a2410d36
Sigmund Berbom <sberbom@gmail.com>
2026-02-05 12:41:25 +0100

    🏷️ Legg til egen type for `VedtaksperiodeTsrDto`

--- src/frontend/Sider/Behandling/VedtakOgBeregning/DagligReise/innvilgeVedtak/innvilgeDagligReiseUtils.ts
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/DagligReise/innvilgeVedtak/innvilgeDagligReiseUtils.ts
@@ -1,10 +1,12 @@
 import { Stønadstype } from '../../../../../typer/behandling/behandlingTema';
 import { Vedtaksperiode } from '../../../../../typer/vedtak/vedtakperiode';
 
+export type VedtaksperiodeTsrDto = Omit<Vedtaksperiode, 'målgruppeType' | 'aktivitetType'>;
+
 export const tilVedtaksperioderDto = (
     perioder: Vedtaksperiode[],
     stønadstype: Stønadstype
-): Vedtaksperiode[] | Omit<Vedtaksperiode, 'målgruppeType' | 'aktivitetType'>[] => {
+): Vedtaksperiode[] | VedtaksperiodeTsrDto[] => {
     if (stønadstype === Stønadstype.DAGLIG_REISE_TSR) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         return perioder.map(({ målgruppeType, aktivitetType, ...rest }) => rest);
