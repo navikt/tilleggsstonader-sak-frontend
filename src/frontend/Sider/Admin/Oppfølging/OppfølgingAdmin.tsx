bdf1050a1f384c4eb85b4a23a7eb509c6f666caa
Adnan Vrevic <adnan.vrevic@nav.no>
2026-02-18 11:28:00 +0100

    ✨ Legg til enhetvalg for henting av behandlinger i oppfølging

--- src/frontend/Sider/Admin/Oppfølging/OppfølgingAdmin.tsx
+++ src/frontend/Sider/Admin/Oppfølging/OppfølgingAdmin.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Heading, VStack } from '@navikt/ds-react';
+import { Heading, HStack, Select, VStack } from '@navikt/ds-react';
 
 import { InformasjonOppfølging } from './InformasjonOppfølging';
 import styles from './OppfølgingAdmin.module.css';
@@ -10,33 +10,59 @@ import { useApp } from '../../../context/AppContext';
 import DataViewer from '../../../komponenter/DataViewer';
 import SmallButton from '../../../komponenter/Knapper/SmallButton';
 import { byggHenterRessurs, Ressurs } from '../../../typer/ressurs';
+import { erProd } from '../../../utils/miljø';
+import {
+    Enheter,
+    enhetTilTekst,
+    hentEnheterSaksbehandlerHarTilgangTil,
+} from '../../Oppgavebenk/typer/enhet';
 
 export const OppølgingAdmin = () => {
-    const { request } = useApp();
+    const { request, saksbehandler, appEnv } = useApp();
 
     const [oppfølginger, settOppføginger] = useState<Ressurs<Oppfølging[]>>(byggHenterRessurs());
+    const gyldigeEnheterForSaksbehandler = hentEnheterSaksbehandlerHarTilgangTil(
+        appEnv,
+        saksbehandler
+    );
+
+    const [enhet, settEnhet] = useState<Enheter>(gyldigeEnheterForSaksbehandler[0]);
 
     const hentBehandlingerForOppfølging = () => {
         settOppføginger(byggHenterRessurs());
-        request(`/api/sak/oppfolging/start`, 'POST');
+        request(`/api/sak/oppfolging/start/${enhet}`, 'POST');
         setTimeout(() => {
-            request<Oppfølging[], null>(`/api/sak/oppfolging`).then(settOppføginger);
+            request<Oppfølging[], null>(`/api/sak/oppfolging/${enhet}`).then(settOppføginger);
         }, 3000);
-    };
+    }; // TODO Denne delayen bør gjøres på en bedre måte hvis vi skal gjøre dette tilgjengelig i prod
 
     useEffect(() => {
-        request<Oppfølging[], null>(`/api/sak/oppfolging`).then(settOppføginger);
-    }, [request]);
+        request<Oppfølging[], null>(`/api/sak/oppfolging/${enhet}`).then(settOppføginger);
+    }, [request, enhet]);
 
     return (
         <VStack gap={'4'} className={styles.container}>
             <Heading size={'medium'}>[Admin] Oppfølging</Heading>
             <InformasjonOppfølging />
-            <div>
-                <SmallButton onClick={hentBehandlingerForOppfølging}>
-                    {'Hent behandlinger for oppfølging'}
-                </SmallButton>
-            </div>
+            {!erProd() && (
+                <HStack gap={'4'} className={styles.formContainer}>
+                    <Select
+                        value={enhet}
+                        label="Enhet"
+                        onChange={(e) => settEnhet(e.target.value as Enheter)}
+                        size="small"
+                    >
+                        {gyldigeEnheterForSaksbehandler.map((enhet) => (
+                            <option key={enhet} value={enhet}>
+                                {enhetTilTekst[enhet]}
+                            </option>
+                        ))}
+                    </Select>
+                    <SmallButton onClick={hentBehandlingerForOppfølging}>
+                        {'Hent behandlinger for oppfølging'}
+                    </SmallButton>
+                </HStack>
+            )}
             <DataViewer type={'oppfølginger'} response={{ oppfølginger }}>
                 {({ oppfølginger }) => <OppfølgingTabell oppfølgingerInit={oppfølginger} />}
             </DataViewer>
