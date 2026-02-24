cb5452343350bad174fc48ca08b66a8da82ed039
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-02-24 12:17:37 +0100

    merge main

--- src/frontend/utils/toggles.ts
+++ src/frontend/utils/toggles.ts
@@ -27,7 +27,6 @@ export enum Toggle {
      * features-under-utvikling
      */
     KAN_HA_FLERE_BEHANDLINGER_PÅ_SAMME_FAGSAK = 'sak.kan-ha-flere-behandlinger-pa-samme-fagsak',
-    VIS_KARTSIDE = 'sak.vis-kartside',
     TILGANGSSTYRE_PÅ_TILORDNET_OPPGAVE = 'sak.tilgangsstyring-tildelt-oppgave',
     BRUK_DYNAMISK_KART = 'sak.frontend.bruk-dynamisk-kart',
     KAN_BEHANDLE_PRIVAT_BIL = 'sak.daglig-reise-privat-bil',
