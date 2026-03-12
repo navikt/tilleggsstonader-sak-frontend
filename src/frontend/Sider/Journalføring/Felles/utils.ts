ec46331d86be4f99052b50107f403732c4c3827f
Runar Skagestad <runar.skagestad@nav.no>
2026-03-12 12:20:58 +0100

    ➖ Migrer bort fra familie-form-elements (#1078)

--- src/frontend/Sider/Journalføring/Felles/utils.ts
+++ src/frontend/Sider/Journalføring/Felles/utils.ts
@@ -1,15 +1,10 @@
-import { ISelectOption, MultiValue, PropsValue, SingleValue } from '@navikt/familie-form-elements';
-
 import { Journalføringsaksjon } from '../../../hooks/useJournalføringState';
 import { BehandlingForJournalføring } from '../../../typer/behandling/behandling';
 import { BehandlingResultat } from '../../../typer/behandling/behandlingResultat';
 import { BehandlingStatus } from '../../../typer/behandling/behandlingStatus';
 import { BehandlingType } from '../../../typer/behandling/behandlingType';
-import { LogiskVedlegg } from '../../../typer/dokument';
 import { JournalpostResponse } from '../../../typer/journalpost';
 import { Journalføringsårsak } from '../typer/journalføringsårsak';
-
-export type MultiSelectValue = { label: string; value: string };
 
 export const dokumentTitler: string[] = [
     'Anke',
@@ -47,33 +42,6 @@ export const dokumentTitler: string[] = [
     'Søknad om tilleggsstønad',
     'Uttalelse',
 ];
-
-export const dokumentTitlerMultiSelect: ISelectOption[] = dokumentTitler.map((tittel) => {
-    return { value: tittel, label: tittel };
-});
-
-export const mapDokumentTittelTilMultiselectValue = (tittel: string) => {
-    return { value: tittel, label: tittel };
-};
-
-export const mapLogiskeVedleggTilMultiselectValue = (
-    logiskeVedlegg: LogiskVedlegg[]
-): PropsValue<{ label: string; value: string }> => {
-    return logiskeVedlegg.map((vedlegg) => {
-        return { label: vedlegg.tittel, value: vedlegg.tittel };
-    });
-};
-
-export const mapMultiselectValueTilLogiskeVedlegg = (
-    values: MultiValue<MultiSelectValue> | SingleValue<MultiSelectValue>
-) => {
-    if ((values as MultiValue<MultiSelectValue>).length !== undefined) {
-        return (values as MultiValue<MultiSelectValue>).map((value) => value.value);
-    } else {
-        const value = values as SingleValue<MultiSelectValue>;
-        return [value === null ? '' : value.value];
-    }
-};
 
 export const journalføringGjelderKlage = (journalføringsårsak: Journalføringsårsak) =>
     journalføringsårsak === Journalføringsårsak.KLAGE ||
