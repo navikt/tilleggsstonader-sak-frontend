fdd50d3962359002a987638849a5c6480bbd138a
Nibedita.Pahari <nibedita.pahari@nav.no>
2026-05-06 21:55:50 +0200

    handlet Personopplysninger hvis det er null

--- src/frontend/utils/adressebeskyttelse.ts
+++ src/frontend/utils/adressebeskyttelse.ts
@@ -1,7 +1,9 @@
+import logger from '../../backend/logger';
 import { Adressebeskyttelse, Personopplysninger } from '../typer/personopplysninger';
 
 export function søkerHarStrengtFortroligAdresse(personopplysninger: Personopplysninger) {
     if (!personopplysninger) {
+        logger.warn('Personopplysninger mangler');
         return false;
     }
 
