a2cbd3242b03837b7764d35c51a6d7b80437b3e4
sarahjelle <sara.hjelle@bekk.no>
2026-05-11 15:26:24 +0200

    Fjern unødvendig div rundt container

--- src/frontend/komponenter/ResultatOgStatusKort/ResultatOgStatusKort.tsx
+++ src/frontend/komponenter/ResultatOgStatusKort/ResultatOgStatusKort.tsx
@@ -32,32 +32,28 @@ export const ResultatOgStatusKort: React.FC<{
     const kildeId = periode && erAktivitet(periode) ? periode.kildeId : undefined;
 
     return (
-        <div>
-            <div className={styles.container}>
-                {skalViseStatus && <Statusbånd status={periode.status} />}
-                <div
-                    className={classNames([
-                        styles.resultatMarg,
-                        utledFarge(periode?.resultat, redigeres),
-                    ])}
-                >
-                    {periode && !redigeres && (
-                        <HvittVilkårsresultatIkon vilkårsresultat={periode?.resultat} />
-                    )}
-                </div>
-                <div>
-                    <div
-                        className={`${styles.innholdContainer} ${redigeres ? styles.redigeres : ''}`}
-                    >
-                        {children}
-                    </div>
-                    {footer}
-                </div>
-                <HStack gap="space-16" className={styles.redigeringsknappContainer}>
-                    {kildeId && <Detail>ID: {kildeId}</Detail>}
-                    {redigeringKnapp}
-                </HStack>
+        <div className={styles.container}>
+            {skalViseStatus && <Statusbånd status={periode.status} />}
+            <div
+                className={classNames([
+                    styles.resultatMarg,
+                    utledFarge(periode?.resultat, redigeres),
+                ])}
+            >
+                {periode && !redigeres && (
+                    <HvittVilkårsresultatIkon vilkårsresultat={periode?.resultat} />
+                )}
             </div>
+            <div>
+                <div className={`${styles.innholdContainer} ${redigeres ? styles.redigeres : ''}`}>
+                    {children}
+                </div>
+                {footer}
+            </div>
+            <HStack gap="space-16" className={styles.redigeringsknappContainer}>
+                {kildeId && <Detail>ID: {kildeId}</Detail>}
+                {redigeringKnapp}
+            </HStack>
         </div>
     );
 };
