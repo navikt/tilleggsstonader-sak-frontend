1dc32df491ab90563013504f5163a37f0edef7b8
sarahjelle <sara.hjelle@bekk.no>
2026-05-12 15:45:21 +0200

    Skal få frem om uke er ny, endret eller utbetalt. Samt om den er behandlet automatisk eller manuelt, evt. har avvik. Mål: gjøre det enklere å se hvilke uker som må behandles nå, er med fra tidligere og hva som evt. er endret av det eksisterende

--- src/frontend/Sider/Behandling/Kjøreliste/Reisevurdering/UkeInnhold.tsx
+++ src/frontend/Sider/Behandling/Kjøreliste/Reisevurdering/UkeInnhold.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react';
 
 import { PencilIcon } from '@navikt/aksel-icons';
-import { Button, HStack, InlineMessage, Label, VStack } from '@navikt/ds-react';
+import { BodyShort, Button, HStack, InlineMessage, Label, VStack } from '@navikt/ds-react';
 
 import { RedigerAvklartDag } from './Dag/RedigerAvklartDag';
 import styles from './UkeInnhold.module.css';
@@ -29,8 +29,8 @@ import { RammeForReiseMedPrivatBilDelperiode } from '../../../../typer/vedtak/ve
 export const UkeInnhold: FC<{
     uke: UkeVurdering;
     oppdaterUke: (uke: UkeVurdering) => void;
-    delperioder: RammeForReiseMedPrivatBilDelperiode[];
-}> = ({ uke, oppdaterUke, delperioder }) => {
+    delperiodeForUke: RammeForReiseMedPrivatBilDelperiode;
+}> = ({ uke, oppdaterUke, delperiodeForUke }) => {
     const { request } = useApp();
     const { behandling } = useBehandling();
     const { erStegRedigerbart } = useSteg();
@@ -55,7 +55,7 @@ export const UkeInnhold: FC<{
         settValideringsfeilForDager(feil);
 
         const erAntallGodkjenteDagerInnenforRammevedtak =
-            validerAntallReisedagerInnenforRammevedtak(redigerbareDager, uke, delperioder);
+            validerAntallReisedagerInnenforRammevedtak(redigerbareDager, delperiodeForUke);
 
         settValideringsfeilForUke(
             erAntallGodkjenteDagerInnenforRammevedtak
@@ -102,7 +102,10 @@ export const UkeInnhold: FC<{
     const kanRedigereUke = erStegRedigerbart && !!uke.kjørelisteInnsendtDato && !!uke.avklartUkeId;
 
     return (
-        <VStack gap="space-16">
+        <VStack gap="space-16" data-color="neutral">
+            <BodyShort size="small">
+                Antall reisedager fra rammevedtak: {delperiodeForUke.reisedagerPerUke}
+            </BodyShort>
             <div>
                 <div className={`${styles.borderToppBunn} ${styles.wrapper}`}>
                     <div className={styles.venstreGrid}>
