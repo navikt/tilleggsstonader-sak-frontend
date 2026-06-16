e524f4a40fec43840890d158805978bc9bf0705b
Sigmund Berbom <44610513+sberbom@users.noreply.github.com>
2026-06-16 13:00:51 +0200

    ✨Legg til tag som viser status på avklarte dager ved en revudrering (#1196)
    For at det skal bli enklere å se status på en dag ved revudering og opphør vises en tag med status på dagen. Ved sletting settes det en strek gjennom dataen SB har lagt inn.
    
    Fjerner også muligheten til å redigere kjørelisteuker hvis for opphør.
    
    Mål: Opphør daglig reise privat bil
    
    Commits:
    
    * ✨Legg til tag som viser status på avklarte dager ved en revudrering. Ved sletting vises line-through
    
    * 🐛 Ikke ha line-through på fallback "-"-verdi
    
    * ✨ Ikke vis redigeringsknapp for kjørelister hvis opphør
    
    * ♻️ Bedre variabelnavn for å gjøre koden mer lesbar

--- src/frontend/Sider/Behandling/Kjøreliste/Reisevurdering/Dag/AvklartDagLesevisning.tsx
+++ src/frontend/Sider/Behandling/Kjøreliste/Reisevurdering/Dag/AvklartDagLesevisning.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
 
-import { BodyShort, HStack } from '@navikt/ds-react';
+import { BodyShort, HStack, Tag } from '@navikt/ds-react';
 
-import { AvklartDag } from '../../../../../typer/kjøreliste';
+import { AvklartDag, AvklartKjørtDagStatus } from '../../../../../typer/kjøreliste';
 import { kronerEllerStrek } from '../../../../../utils/tekstformatering';
 import { godkjentGjennomførtKjøringTilTekst } from '../../utils';
 import styles from '../UkeInnhold.module.css';
@@ -10,16 +10,54 @@ import styles from '../UkeInnhold.module.css';
 export const AvklartDagLesevisning: FC<{
     avklartDag: AvklartDag | undefined;
 }> = ({ avklartDag }) => {
+    const erSlettet = avklartDag?.avklartKjørtDagStatus === AvklartKjørtDagStatus.SLETTET;
+    const finnClassname = (harVerdi: boolean) =>
+        erSlettet && harVerdi ? styles.slettet : undefined;
+
     return (
         <div className={styles.høyreGrid}>
-            <HStack gap="space-4">
+            <HStack gap="space-4" className={finnClassname(avklartDag !== undefined)}>
                 <BodyShort size="small">
                     {avklartDag &&
                         godkjentGjennomførtKjøringTilTekst[avklartDag.godkjentGjennomførtKjøring]}
                 </BodyShort>
             </HStack>
-            <BodyShort size="small">{kronerEllerStrek(avklartDag?.parkeringsutgift)}</BodyShort>
-            <BodyShort size="small">{avklartDag?.begrunnelse || '-'}</BodyShort>
+            <BodyShort size="small" className={finnClassname(!!avklartDag?.parkeringsutgift)}>
+                {kronerEllerStrek(avklartDag?.parkeringsutgift)}
+            </BodyShort>
+            <BodyShort size="small" className={finnClassname(!!avklartDag?.begrunnelse)}>
+                {avklartDag?.begrunnelse || '-'}
+            </BodyShort>
+            <AvklartKjørtDagStatusTag avklartKjørtDagStatus={avklartDag?.avklartKjørtDagStatus} />
         </div>
     );
+};
+
+const AvklartKjørtDagStatusTag: FC<{
+    avklartKjørtDagStatus: AvklartKjørtDagStatus | undefined;
+}> = ({ avklartKjørtDagStatus }) => {
+    switch (avklartKjørtDagStatus) {
+        case AvklartKjørtDagStatus.NY:
+            return (
+                <Tag size="small" data-color="success">
+                    Ny
+                </Tag>
+            );
+        case AvklartKjørtDagStatus.ENDRET:
+            return (
+                <Tag size="small" data-color="warning">
+                    Endret
+                </Tag>
+            );
+        case AvklartKjørtDagStatus.UENDRET:
+            return <BodyShort size="small">Uendret</BodyShort>;
+        case AvklartKjørtDagStatus.SLETTET:
+            return (
+                <Tag size="small" data-color="danger">
+                    Slettet
+                </Tag>
+            );
+        default:
+            return null;
+    }
 };
