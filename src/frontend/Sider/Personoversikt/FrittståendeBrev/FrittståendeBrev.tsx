fdf96ebb8457f4b6ee45709159be9301a3559f75
Filip Larsen <32879828+Filipcl@users.noreply.github.com>
2026-06-17 12:29:50 +0200

    Felles klagebrev (#1197)
    * Legger til støtte for å hente ut felles brev i personoversikten

--- src/frontend/Sider/Personoversikt/FrittståendeBrev/FrittståendeBrev.tsx
+++ src/frontend/Sider/Personoversikt/FrittståendeBrev/FrittståendeBrev.tsx
@@ -35,7 +35,7 @@ export const FrittståendeBrev: React.FC<{
         fil,
         settFil,
         hentMalStruktur,
-        hentBrevmaler,
+        hentBrevmalerPersonoversikt,
     } = useBrev(valgtStønadstype);
 
     const { mellomlagretBrev } = useMellomlagringFrittståendeBrev(fagsakId);
@@ -47,8 +47,8 @@ export const FrittståendeBrev: React.FC<{
     }, [mellomlagretBrev, settBrevmal]);
 
     useEffect(() => {
-        hentBrevmaler([BrevmalResultat.FRITTSTAENDE]);
-    }, [hentBrevmaler]);
+        hentBrevmalerPersonoversikt([BrevmalResultat.FRITTSTAENDE]);
+    }, [hentBrevmalerPersonoversikt]);
 
     useEffect(() => {
         if (brevmal) {
