6bbe6aa4dc2aec4cd7c9ce5926fddd58f9ae5922
Nibeditanav <nibedita.pahari@nav.no>
2026-07-13 10:31:07 +0200

    Beregning reise til smling offentlig transport (#1220)
    * Lagt til vedtak og beregning fane for reise til samling
    ---------
    
    Signed-off-by: Nibeditanav <nibedita.pahari@nav.no>

---
+++ src/frontend/Sider/Behandling/VedtakOgBeregning/ReiseTilSamling/innvilgeVedtak/Beregningsresultat/OffentligTransport.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+
+import { Heading, HStack, Table } from '@navikt/ds-react';
+
+import { BeregningsresultatOffentligTransportForSamling } from '../../../../../../typer/vedtak/vedtakReiseTilSamling';
+import { formaterIsoDato } from '../../../../../../utils/dato';
+
+interface Props {
+    beregningsresultat: BeregningsresultatOffentligTransportForSamling[];
+}
+
+export const BeregningOffentligTransport: FC<Props> = ({ beregningsresultat }) => {
+    return (
+        <div>
+            <HStack justify="space-between">
+                <Heading spacing size="xsmall" level="4">
+                    Beregningsresultat
+                </Heading>
+            </HStack>
+
+            <Table size="small">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Adresse</Table.HeaderCell>
+                        <Table.HeaderCell>F.o.m.</Table.HeaderCell>
+                        <Table.HeaderCell>T.o.m.</Table.HeaderCell>
+                        <Table.HeaderCell align="right">Stønadsbeløp</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {beregningsresultat.map((samling) => (
+                        <Table.Row key={`${samling.reiseId}-${samling.fom}`}>
+                            <Table.DataCell>{samling.adresse ?? '-'}</Table.DataCell>
+                            <Table.DataCell>{formaterIsoDato(samling.fom)}</Table.DataCell>
+                            <Table.DataCell>{formaterIsoDato(samling.tom)}</Table.DataCell>
+                            <Table.DataCell align="right">{samling.beløp ?? 0}</Table.DataCell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
+        </div>
+    );
+};
