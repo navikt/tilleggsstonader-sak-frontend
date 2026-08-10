f8f4722eafedfd435cac845d333a2e77abd1b706
Sara Hjelle <46678893+sarahjelle@users.noreply.github.com>
2026-08-10 10:06:26 +0200

    Legg til knapp for å slette manuell kjøreliste (#1253)
    - knapp for å slette manuell kjøreliste

--- src/frontend/Sider/Behandling/Kjøreliste/RegistrerKjørelisteManuelt/LagredeKjørelister/LagretKjøreliste.tsx
+++ src/frontend/Sider/Behandling/Kjøreliste/RegistrerKjørelisteManuelt/LagredeKjørelister/LagretKjøreliste.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { BodyShort, Box, Heading, HStack, Label, Table, VStack } from '@navikt/ds-react';
+import { TrashIcon } from '@navikt/aksel-icons';
+import { BodyShort, Box, Button, Heading, HStack, Label, Table, VStack } from '@navikt/ds-react';
 import { BorderNeutralSubtle } from '@navikt/ds-tokens/js';
 
 import { UkeInnholdLagretKjøreliste } from './UkeInnholdLagretKjøreliste';
+import { useRegistrerKjøreliste } from '../../../../../context/RegistrerKjørelisteContext/RegistrerKjørelisteContext';
+import { useSteg } from '../../../../../context/StegContext';
+import { Feilmelding } from '../../../../../komponenter/Feil/Feilmelding';
+import {
+    Feil,
+    feiletRessursTilFeilmelding,
+} from '../../../../../komponenter/Feil/feilmeldingUtils';
 import { Skillelinje } from '../../../../../komponenter/Skillelinje';
 import { TableHeaderCellSmall } from '../../../../../komponenter/TabellSmall';
 import { formaterIsoPeriode } from '../../../../../utils/dato';
@@ -12,6 +20,19 @@ import { ManueltInnsendtKjørelisteUke } from '../typer';
 export const OppsummertLagretKjøreliste: React.FC<{
     kjøreliste: ManueltInnsendtKjørelisteUke;
 }> = ({ kjøreliste }) => {
+    const { slettKjøreliste } = useRegistrerKjøreliste();
+    const { erStegRedigerbart } = useSteg();
+
+    const [feilmelding, settFeilmelding] = React.useState<Feil | undefined>(undefined);
+
+    const slett = () => {
+        slettKjøreliste(kjøreliste.id).then((respons) => {
+            if (respons.status === 'FEILET') {
+                settFeilmelding(feiletRessursTilFeilmelding(respons));
+            }
+        });
+    };
+
     return (
         <Box background="default" borderColor="neutral-subtle" borderRadius="12" borderWidth="1">
             <VStack gap="space-8" paddingBlock="space-16 space-8" paddingInline="space-24">
@@ -24,14 +45,29 @@ export const OppsummertLagretKjøreliste: React.FC<{
                         Journalpost ID: {kjøreliste.journalpostId}
                     </BodyShort>
                 </HStack>
-                <div>
-                    <Label size="small">Begrunnelse for manuell registrering: </Label>
-                    <BodyShort size="small">
-                        {kjøreliste.begrunnelse
-                            ? kjøreliste.begrunnelse
-                            : 'Ingen begrunnselse lagt ved'}
-                    </BodyShort>
-                </div>
+                <HStack justify="space-between" align="end">
+                    <div>
+                        <Label size="small">Begrunnelse for manuell registrering: </Label>
+                        <BodyShort size="small">
+                            {kjøreliste.begrunnelse
+                                ? kjøreliste.begrunnelse
+                                : 'Ingen begrunnselse lagt ved'}
+                        </BodyShort>
+                    </div>
+                    {erStegRedigerbart && (
+                        <Button
+                            size="small"
+                            variant="tertiary"
+                            icon={<TrashIcon />}
+                            iconPosition="right"
+                            data-color="danger"
+                            onClick={slett}
+                        >
+                            Slett
+                        </Button>
+                    )}
+                </HStack>
+                <Feilmelding feil={feilmelding} />
             </VStack>
 
             <Skillelinje borderColor={BorderNeutralSubtle} />
