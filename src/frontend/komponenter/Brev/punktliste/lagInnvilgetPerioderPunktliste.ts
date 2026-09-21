198b592c1d792507bcefae3bea349eccd8693397
Sigmund Berbom <sberbom@gmail.com>
2026-09-21 17:37:53 +0200

    ✨ Legg til punktliste med samlinger i brev

--- src/frontend/komponenter/Brev/punktliste/lagInnvilgetPerioderPunktliste.ts
+++ src/frontend/komponenter/Brev/punktliste/lagInnvilgetPerioderPunktliste.ts
@@ -2,6 +2,7 @@ import { Behandling } from '../../../typer/behandling/behandling';
 import { Stønadstype } from '../../../typer/behandling/behandlingTema';
 import { VedtakResponse } from '../../../typer/vedtak/vedtak';
 import { BeregningsresultatBoutgifter } from '../../../typer/vedtak/vedtakBoutgifter';
+import { BeregningResultatReiseTilSamling } from '../../../typer/vedtak/vedtakReiseTilSamling';
 import { formaterTilTekstligDato } from '../../../utils/dato';
 
 export const lagInnvilgetPerioderPunktliste = (
@@ -20,25 +21,63 @@ export const lagInnvilgetPerioderPunktliste = (
             return lagPunktlisteInnvilgedePerioderForBoutgifter(
                 vedtak.beregningsresultat as BeregningsresultatBoutgifter
             );
+        case Stønadstype.REISE_TIL_SAMLING_TSO:
+        case Stønadstype.REISE_TIL_SAMLING_TSR:
+            return lagPunktlisteInnvilgedePerioderForReiseTilSamling(
+                vedtak.beregningsresultat as BeregningResultatReiseTilSamling
+            );
         default:
             return '';
     }
 };
 
-const lagPunktlisteInnvilgedePerioderForBoutgifter = (
-    beregningsresultat: BeregningsresultatBoutgifter
-): string => {
+type PeriodeMedFomTom = {
+    fom: string;
+    tom: string;
+};
+
+const lagPunktlisteHtml = (perioder: PeriodeMedFomTom[]): string => {
     return `<ul style="margin: 0; padding-top: 0">
-    ${beregningsresultat.perioder
-        .map((periode) =>
-            periode.utgifter
-                .filter((utgift) => !utgift.erFørTidligsteEndring)
-                .map(
-                    (utgift) =>
-                        `<li style="margin: 0;">fra og med ${formaterTilTekstligDato(utgift.fom)} til og med ${formaterTilTekstligDato(utgift.tom)}</li>`
-                )
-                .join('')
+    ${perioder
+        .map(
+            (periode) =>
+                `<li style="margin: 0;">fra og med ${formaterTilTekstligDato(periode.fom)} til og med ${formaterTilTekstligDato(periode.tom)}</li>`
         )
         .join('')}
     </ul>`;
+};
+
+const lagPunktlisteInnvilgedePerioderForBoutgifter = (
+    beregningsresultat: BeregningsresultatBoutgifter
+): string => {
+    const perioder = beregningsresultat.perioder.flatMap((periode) =>
+        periode.utgifter.filter((utgift) => !utgift.erFørTidligsteEndring)
+    );
+    return lagPunktlisteHtml(perioder);
+};
+
+/**
+ * Slår sammen offentlig transport og privat bil til én liste med samlinger,
+ * fjerner samlinger med identisk fom/tom og sorterer synkende på fom (nyeste først).
+ */
+const lagPunktlisteInnvilgedePerioderForReiseTilSamling = (
+    beregningsresultat: BeregningResultatReiseTilSamling
+): string => {
+    const perioder = [
+        ...(beregningsresultat.offentligTransport ?? []),
+        ...(beregningsresultat.privatBil ?? []),
+    ];
+
+    const unikePerioder = fjernDuplikatePerioder(perioder);
+    const sortertePerioder = unikePerioder.sort((a, b) => b.fom.localeCompare(a.fom));
+
+    return lagPunktlisteHtml(sortertePerioder);
+};
+
+const fjernDuplikatePerioder = <T extends PeriodeMedFomTom>(perioder: T[]): T[] => {
+    const unikePerioder = new Map<string, T>();
+    perioder.forEach((periode) => {
+        unikePerioder.set(`${periode.fom}_${periode.tom}`, periode);
+    });
+    return Array.from(unikePerioder.values());
 };
