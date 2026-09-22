ec83d822acdeb5908981aa755f228070b3945461
Sigmund Berbom <44610513+sberbom@users.noreply.github.com>
2026-09-22 14:36:30 +0200

    Reise til samling - punktliste brev (#1342)
    Legg til punktliste over samlingene som bruker får godkjent i brevet. Flere reiser med samme `fom` og `tom` slåes sammen til en reise.
    
    * ✨ Legg til punktliste med samlinger i brev
    
    * ♻️ Ta i bruk eksiterende periode type
    
    * 🐛 Fiks feil ved undefined beregningsresultat
    
    * ✨ Sørg for at punktlisten for boutgifter også er sortert
    
    * ♻️ Legg til generisk funksjon for å fjerne duplikater fra liste

--- src/frontend/komponenter/Brev/punktliste/lagInnvilgetPerioderPunktliste.ts
+++ src/frontend/komponenter/Brev/punktliste/lagInnvilgetPerioderPunktliste.ts
@@ -2,7 +2,10 @@ import { Behandling } from '../../../typer/behandling/behandling';
 import { Stønadstype } from '../../../typer/behandling/behandlingTema';
 import { VedtakResponse } from '../../../typer/vedtak/vedtak';
 import { BeregningsresultatBoutgifter } from '../../../typer/vedtak/vedtakBoutgifter';
+import { BeregningResultatReiseTilSamling } from '../../../typer/vedtak/vedtakReiseTilSamling';
 import { formaterTilTekstligDato } from '../../../utils/dato';
+import { Periode } from '../../../utils/periode';
+import { fjernDuplikater } from '../../../utils/utils';
 
 export const lagInnvilgetPerioderPunktliste = (
     behandling: Behandling | undefined,
@@ -20,25 +23,53 @@ export const lagInnvilgetPerioderPunktliste = (
             return lagPunktlisteInnvilgedePerioderForBoutgifter(
                 vedtak.beregningsresultat as BeregningsresultatBoutgifter
             );
+        case Stønadstype.REISE_TIL_SAMLING_TSO:
+        case Stønadstype.REISE_TIL_SAMLING_TSR:
+            return lagPunktlisteInnvilgedePerioderForReiseTilSamling(
+                vedtak.beregningsresultat as BeregningResultatReiseTilSamling | undefined
+            );
         default:
             return '';
     }
 };
 
-const lagPunktlisteInnvilgedePerioderForBoutgifter = (
-    beregningsresultat: BeregningsresultatBoutgifter
-): string => {
+const lagPunktlisteHtml = (perioder: Periode[]): string => {
+    const sortertePerioder = perioder.sort((a, b) => b.fom.localeCompare(a.fom));
     return `<ul style="margin: 0; padding-top: 0">
-    ${beregningsresultat.perioder
-        .map((periode) =>
-            periode.utgifter
-                .filter((utgift) => !utgift.erFørTidligsteEndring)
-                .map(
-                    (utgift) =>
-                        `<li style="margin: 0;">fra og med ${formaterTilTekstligDato(utgift.fom)} til og med ${formaterTilTekstligDato(utgift.tom)}</li>`
-                )
-                .join('')
+    ${sortertePerioder
+        .map(
+            (periode) =>
+                `<li style="margin: 0;">fra og med ${formaterTilTekstligDato(periode.fom)} til og med ${formaterTilTekstligDato(periode.tom)}</li>`
         )
         .join('')}
     </ul>`;
 };
+
+const lagPunktlisteInnvilgedePerioderForBoutgifter = (
+    beregningsresultat: BeregningsresultatBoutgifter
+): string => {
+    const perioder = beregningsresultat.perioder.flatMap((periode) =>
+        periode.utgifter.filter((utgift) => !utgift.erFørTidligsteEndring)
+    );
+    return lagPunktlisteHtml(perioder);
+};
+
+/**
+ * Slår sammen offentlig transport og privat bil til én liste med samlinger,
+ * og fjerner samlinger med identisk fom/tom. lagPunktlisteHtml sorterer perioder.
+ */
+const lagPunktlisteInnvilgedePerioderForReiseTilSamling = (
+    beregningsresultat: BeregningResultatReiseTilSamling | undefined
+): string => {
+    const perioder = [
+        ...(beregningsresultat?.offentligTransport ?? []),
+        ...(beregningsresultat?.privatBil ?? []),
+    ];
+
+    const unikePerioder = fjernDuplikater(perioder, erPerioderLike);
+
+    return lagPunktlisteHtml(unikePerioder);
+};
+
+const erPerioderLike = (periodeA: Periode, periodeB: Periode) =>
+    periodeA.fom === periodeB.fom && periodeA.tom === periodeB.tom;
